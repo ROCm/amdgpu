e1cdecb583b8051dcc53c117f6ffd560fe61f494
Le.Ma <Le.Ma@amd.com>
2017-09-14 15:53:13 +0800

    drm/amdgpu: [hybrid] add AMDGPU VERSION
    Signed-off-by: Le.Ma <Le.Ma@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -131,6 +131,7 @@
 #define KMS_DRIVER_MINOR	64
 #define KMS_DRIVER_PATCHLEVEL	0
 
+#define AMDGPU_VERSION		"19.10.9.418"
 /*
  * amdgpu.debug module options. Are all disabled by default
  */
@@ -3157,6 +3158,10 @@ static int __init amdgpu_init(void)
 		goto error_fence;
 
 	DRM_INFO("amdgpu kernel modesetting enabled.\n");
+
+	DRM_INFO("amdgpu version: %s\n", AMDGPU_VERSION);
+	DRM_INFO("OS DRM version: %d.%d.%d\n", DRM_VER, DRM_PATCH, DRM_SUB);
+
 	amdgpu_register_atpx_handler();
 	amdgpu_acpi_detect();
 
@@ -3197,3 +3202,4 @@ module_exit(amdgpu_exit);
 MODULE_AUTHOR(DRIVER_AUTHOR);
 MODULE_DESCRIPTION(DRIVER_DESC);
 MODULE_LICENSE("GPL and additional rights");
+MODULE_VERSION(AMDGPU_VERSION);
