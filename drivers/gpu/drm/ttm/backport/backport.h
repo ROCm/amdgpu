5114871b7b92d69d0c3676f5554d70db2e612c77
Kevin Wang <Kevin1.Wang@amd.com>
2018-01-23 15:33:23 +0800

    drm/amdkcl: check whether __GFP_RETRY_MAYFAIL is available
    kernel 4.13: rename __GFP_REPEAT to __GFP_RETRY_MAYFAIL
    
    Reviewed-by: Le Ma <Le.Ma@amd.com>
    Reviewed-by: Roger He <Hongbo.He@amd.com>
    Signed-Off-by: Kevin Wang <Kevin1.Wang@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/ttm/backport/backport.h
+++ drivers/gpu/drm/ttm/backport/backport.h
@@ -3,6 +3,7 @@
 #define AMDTTM_BACKPORT_H
 
 #include <linux/version.h>
+#include <kcl/kcl_kernel.h>
 #include <kcl/kcl_dma_mapping.h>
 #include <kcl/kcl_list.h>
 #include <kcl/backport/kcl_fence_backport.h>
