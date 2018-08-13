db7691292abad4158231df6a31a91d8affef7fd2
changzhu <Changfeng.Zhu@amd.com>
2018-08-13 17:21:10 +0800

    drm/amdkcl: [4.16] fix drm .last code, .output_poll_changed conflict
    .last_close and .output_poll_changed helpers are introduced in the same
    commit v4.14-rc3-576-g304a4f6accac
    
    drm_fb_helper_output_poll_changed & drm_fb_helper_lastclose can't be in
    amdkcl like other nonexistent symbols because there's no way to get
    fb_helper pointer from drm_device. amdgpu_device is required to get
    fb_helper pointer in this case.
    drm_device->fb_helper is introduced in v4.14-rc3-575-g29ad20b22c8f.
    and the 2 helper are introduced in v4.14-rc3-576-g304a4f6accac.
    
    Squash of
    9d340fac6ea4 drm/amdkcl: add drm_fb_helper_output_poll_changed & drm_fb_helper_lastclose
    686ea5e4ffe8 drm/amdkcl: refactor check for HAVE_DRM_FB_HELPER_LASTCLOSE
    9c3af6d1cc70 drm/amdkcl: [4.16] fix drm .last code, .output_poll_changed conflict
    ae16f355fd44 drm/amdkcl: Test whether drm_fb_helper_lastclose is available
    394828020901 drm/amdkcl: fix license for kcl backport part
    
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>
    
    Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
    
    Signed-off-by: changzhu <Changfeng.Zhu@amd.com>
    Reviewed-by: Rui Teng <rui.teng@amd.com>
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Acked-by: Feifei Xu <Feifei.Xu@amd.com>
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

--- drivers/gpu/drm/amd/backport/backport.h
+++ drivers/gpu/drm/amd/backport/backport.h
@@ -45,5 +45,6 @@
 #include <kcl/backport/kcl_drm_atomic_helper_backport.h>
 #include <kcl/kcl_drm_hdcp.h>
 #include <kcl/kcl_acpi_table.h>
+#include "kcl/kcl_amdgpu_drm_fb_helper.h"
 
 #endif /* AMDGPU_BACKPORT_H */
