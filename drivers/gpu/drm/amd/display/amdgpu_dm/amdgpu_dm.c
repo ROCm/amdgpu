d29ce6ea419906aa1f30f6ef6bc74ddf7c158f38
Chengming Gui <Jack.Gui@amd.com>
2019-09-20 09:27:01 +0800

    drm/amdkcl: Test whether drm_connector_list_iter_begin is available
    Introduced by kernel v4.11-rc1~83^2~48^2~54
    
    It's a squash of
    drm/amdkcl: refactor guard for drm_modeset_{lock,unlock}_all
        Signed-off-by: Flora Cui <flora.cui@amd.com>
        Acked-by: Rodrigo Siqueira <Rodrigo.Siqueira@amd.com>
    drm/amdkcl: [4.14] fix drm_modeset_lock_all
        Signed-off-by: Evan Quan <evan.quan@amd.com>
        Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>
        Signed-off-by: Jack Gui <Jack.Gui@amd.com>
        Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>
    drm/amdkcl: rework kcl for amdgpu_pmops_runtime_idle
        Signed-off-by: Flora Cui <flora.cui@amd.com>
        Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>
    drm/amdkcl: add kcl for amdgpu_pmops_runtime_idle
        Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    drm/amdkcl: fix drm_connector_list_iter
        Signed-off-by: Flora Cui <flora.cui@amd.com>
    
    Change-Id: Iacbf79c46b7be3146e0b318c67769e10ff5b73a4
    Signed-off-by: Chengming Gui <Jack.Gui@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Feifei Xu <Feifei Xu@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    Signed-off-by: Yang Xiong <Yang.Xiong@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -1039,7 +1039,9 @@ static int amdgpu_dm_audio_component_get_eld(struct device *kdev, int port,
 	struct drm_device *dev = dev_get_drvdata(kdev);
 	struct amdgpu_device *adev = drm_to_adev(dev);
 	struct drm_connector *connector;
+#ifdef HAVE_DRM_CONNECTOR_LIST_ITER_BEGIN
 	struct drm_connector_list_iter conn_iter;
+#endif
 	struct amdgpu_dm_connector *aconnector;
 	int ret = 0;
 
@@ -1047,9 +1049,12 @@ static int amdgpu_dm_audio_component_get_eld(struct device *kdev, int port,
 
 	mutex_lock(&adev->dm.audio_lock);
 
+#ifdef HAVE_DRM_CONNECTOR_LIST_ITER_BEGIN
 	drm_connector_list_iter_begin(dev, &conn_iter);
 	drm_for_each_connector_iter(connector, &conn_iter) {
-
+#else
+	drm_for_each_connector(connector, dev) {
+#endif
 		if (connector->connector_type == DRM_MODE_CONNECTOR_WRITEBACK)
 			continue;
 
@@ -1065,7 +1070,9 @@ static int amdgpu_dm_audio_component_get_eld(struct device *kdev, int port,
 
 		break;
 	}
+#ifdef HAVE_DRM_CONNECTOR_LIST_ITER_BEGIN
 	drm_connector_list_iter_end(&conn_iter);
+#endif
 
 	mutex_unlock(&adev->dm.audio_lock);
 
@@ -2717,12 +2724,17 @@ static int detect_mst_link_for_all_connectors(struct drm_device *dev)
 {
 	struct amdgpu_dm_connector *aconnector;
 	struct drm_connector *connector;
+#ifdef HAVE_DRM_CONNECTOR_LIST_ITER_BEGIN
 	struct drm_connector_list_iter iter;
+#endif
 	int ret = 0;
 
+#ifdef HAVE_DRM_CONNECTOR_LIST_ITER_BEGIN
 	drm_connector_list_iter_begin(dev, &iter);
 	drm_for_each_connector_iter(connector, &iter) {
-
+#else
+	drm_for_each_connector(connector, dev) {
+#endif
 		if (connector->connector_type == DRM_MODE_CONNECTOR_WRITEBACK)
 			continue;
 
@@ -2744,7 +2756,9 @@ static int detect_mst_link_for_all_connectors(struct drm_device *dev)
 			}
 		}
 	}
+#ifdef HAVE_DRM_CONNECTOR_LIST_ITER_BEGIN
 	drm_connector_list_iter_end(&iter);
+#endif
 
 	return ret;
 }
@@ -2892,12 +2906,17 @@ static void s3_handle_mst(struct drm_device *dev, bool suspend)
 {
 	struct amdgpu_dm_connector *aconnector;
 	struct drm_connector *connector;
+#ifdef HAVE_DRM_CONNECTOR_LIST_ITER_BEGIN
 	struct drm_connector_list_iter iter;
+#endif
 	struct drm_dp_mst_topology_mgr *mgr;
 
+#ifdef HAVE_DRM_CONNECTOR_LIST_ITER_BEGIN
 	drm_connector_list_iter_begin(dev, &iter);
 	drm_for_each_connector_iter(connector, &iter) {
-
+#else
+	drm_for_each_connector(connector, dev) {
+#endif
 		if (connector->connector_type == DRM_MODE_CONNECTOR_WRITEBACK)
 			continue;
 
@@ -2927,7 +2946,10 @@ static void s3_handle_mst(struct drm_device *dev, bool suspend)
 			resume_mst_branch_status(mgr);
 		}
 	}
+#ifdef HAVE_DRM_CONNECTOR_LIST_ITER_BEGIN
 	drm_connector_list_iter_end(&iter);
+#endif
+
 }
 
 static int amdgpu_dm_smu_write_watermarks_table(struct amdgpu_device *adev)
@@ -3392,7 +3414,9 @@ static int dm_resume(struct amdgpu_ip_block *ip_block)
 	struct amdgpu_display_manager *dm = &adev->dm;
 	struct amdgpu_dm_connector *aconnector;
 	struct drm_connector *connector;
+#ifdef HAVE_DRM_CONNECTOR_LIST_ITER_BEGIN
 	struct drm_connector_list_iter iter;
+#endif
 	struct drm_crtc *crtc;
 	struct drm_crtc_state *new_crtc_state;
 	struct dm_crtc_state *dm_new_crtc_state;
