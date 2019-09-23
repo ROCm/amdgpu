de07f04fed9a026cefef74ae5e747015aa2924e4
Rui Teng <rui.teng@amd.com>
2019-09-23 19:29:57 +0800

    drm/amdkcl: check whether DEFINE_DEBUGFS_ATTRIBUTE is available
    DEFINE_DEBUGFS_ATTRIBUTE and debugfs_create_file_unsafe is introduced by kernel 4.7
    - commit c64688081490321f2d23a292ef24e60bb321f3f1
    - Author: Nicolai Stange <nicstange@gmail.com>
    - debugfs: add support for self-protecting attribute file fops
    
    Change-Id: Ia4c9ddf96d50a5eea76a28c6e8f3a012332f1f69
    Signed-off-by: Rui Teng <rui.teng@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    drm/amdkcl: fix compile error if DEFINE_DEBUGFS_ATTRIBUTE not defined
    Change-Id: I23277d6b9c298fb4325cc54b1ca7379e45b68d99
    Signed-off-by: Stanley.Yang <Stanley.Yang@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_debugfs.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_debugfs.c
@@ -3152,8 +3152,10 @@ static int force_yuv420_output_get(void *data, u64 *val)
 	return 0;
 }
 
+#ifdef DEFINE_DEBUGFS_ATTRIBUTE
 DEFINE_DEBUGFS_ATTRIBUTE(force_yuv420_output_fops, force_yuv420_output_get,
 			 force_yuv420_output_set, "%llu\n");
+#endif
 
 /*
  *  Read Replay state
@@ -3415,6 +3417,7 @@ static int dmcub_trace_event_state_get(void *data, u64 *val)
 	return 0;
 }
 
+#ifdef DEFINE_DEBUGFS_ATTRIBUTE
 DEFINE_DEBUGFS_ATTRIBUTE(dmcub_trace_event_state_fops, dmcub_trace_event_state_get,
 			 dmcub_trace_event_state_set, "%llu\n");
 
@@ -3432,6 +3435,7 @@ DEFINE_DEBUGFS_ATTRIBUTE(allow_edp_hotplug_detection_fops,
 DEFINE_DEBUGFS_ATTRIBUTE(disallow_edp_enter_psr_fops,
 			disallow_edp_enter_psr_get,
 			disallow_edp_enter_psr_set, "%llu\n");
+#endif
 
 DEFINE_DEBUGFS_ATTRIBUTE(ips_residency_cntl_fops, ips_residency_cntl_get,
 			   ips_residency_cntl_set, "%llu\n");
@@ -3444,7 +3448,9 @@ static const struct {
 	char *name;
 	const struct file_operations *fops;
 } connector_debugfs_entries[] = {
+#ifdef DEFINE_DEBUGFS_ATTRIBUTE
 		{"force_yuv420_output", &force_yuv420_output_fops},
+#endif
 		{"trigger_hotplug", &trigger_hotplug_debugfs_fops},
 		{"internal_display", &internal_display_fops},
 		{"odm_combine_segments", &odm_combine_segments_fops}
@@ -3615,6 +3621,8 @@ void connector_debugfs_init(struct amdgpu_dm_connector *connector)
 					    dp_debugfs_entries[i].fops);
 		}
 	}
+
+#ifdef DEFINE_DEBUGFS_ATTRIBUTE
 	if (connector->base.connector_type == DRM_MODE_CONNECTOR_eDP) {
 		debugfs_create_file("replay_capability", 0444, dir, connector,
 					&replay_capability_fops);
@@ -3636,6 +3644,7 @@ void connector_debugfs_init(struct amdgpu_dm_connector *connector)
 		debugfs_create_file("disallow_edp_enter_psr", 0644, dir, connector,
 					&disallow_edp_enter_psr_fops);
 	}
+#endif
 
 	for (i = 0; i < ARRAY_SIZE(connector_debugfs_entries); i++) {
 		debugfs_create_file(connector_debugfs_entries[i].name,
@@ -4105,8 +4114,10 @@ static int force_timing_sync_get(void *data, u64 *val)
 	return 0;
 }
 
+#ifdef DEFINE_DEBUGFS_ATTRIBUTE
 DEFINE_DEBUGFS_ATTRIBUTE(force_timing_sync_ops, force_timing_sync_get,
 			 force_timing_sync_set, "%llu\n");
+#endif
 
 
 /*
@@ -4244,8 +4255,10 @@ static int visual_confirm_get(void *data, u64 *val)
 }
 
 DEFINE_SHOW_ATTRIBUTE(mst_topo);
+#ifdef DEFINE_DEBUGFS_ATTRIBUTE
 DEFINE_DEBUGFS_ATTRIBUTE(visual_confirm_fops, visual_confirm_get,
 			 visual_confirm_set, "%llu\n");
+#endif
 
 
 /*
@@ -4371,6 +4384,7 @@ void dtn_debugfs_init(struct amdgpu_device *adev)
 	debugfs_create_file("amdgpu_dm_dp_ignore_cable_id", 0644, root, adev,
 				&dp_ignore_cable_id_ops);
 
+#ifdef DEFINE_DEBUGFS_ATTRIBUTE
 	debugfs_create_file_unsafe("amdgpu_dm_visual_confirm", 0644, root, adev,
 				   &visual_confirm_fops);
 
@@ -4411,4 +4425,5 @@ void dtn_debugfs_init(struct amdgpu_device *adev)
 		debugfs_create_file_unsafe("amdgpu_dm_ips_residency", 0644, root, adev,
 					   &ips_residency_fops);
 	}
+#endif
 }
