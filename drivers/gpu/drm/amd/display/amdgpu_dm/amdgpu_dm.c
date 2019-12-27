f230f2039fb0c46a64c5ac61a09be68c6b1c7ecd
Stanley.Yang <Stanley.Yang@amd.com>
2019-12-27 15:20:06 +0800

    drm/amdkcl: Test whether drm_dp_mst_add_affected_dsc_crtcs() is available
    Change-Id: Icefa820b7602cfd943cbb4d49caac6fa71083263
    Signed-off-by: Stanley.Yang <Stanley.Yang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -12765,6 +12765,7 @@ static inline struct __drm_planes_state *__get_next_zpos(
 			     (old_plane_state) = __i->old_state,		\
 			     (new_plane_state) = __i->new_state, 1))
 
+#if defined(HAVE_DRM_DP_MST_ADD_AFFECTED_DSC_CRTCS)
 static int add_affected_mst_dsc_crtcs(struct drm_atomic_commit *state, struct drm_crtc *crtc)
 {
 	struct drm_connector *connector;
@@ -12794,6 +12795,7 @@ static int add_affected_mst_dsc_crtcs(struct drm_atomic_commit *state, struct dr
 
 	return drm_dp_mst_add_affected_dsc_crtcs(state, &aconnector->mst_root->mst_mgr);
 }
+#endif
 
 /**
  * DOC: Cursor Modes - Native vs Overlay
@@ -13102,6 +13104,7 @@ static int amdgpu_dm_atomic_check(struct drm_device *dev,
 			new_crtc_state->connectors_changed = true;
 	}
 
+#if defined(HAVE_DRM_DP_MST_ADD_AFFECTED_DSC_CRTCS)
 	if (dc_resource_is_dsc_encoding_supported(dc)) {
 		for_each_oldnew_crtc_in_state(state, crtc, old_crtc_state, new_crtc_state, i) {
 			dm_new_crtc_state = to_dm_crtc_state(new_crtc_state);
@@ -13118,6 +13121,7 @@ static int amdgpu_dm_atomic_check(struct drm_device *dev,
 			}
 		}
 	}
+#endif
 
 	for_each_oldnew_crtc_in_state(state, crtc, old_crtc_state, new_crtc_state, i) {
 		dm_old_crtc_state = to_dm_crtc_state(old_crtc_state);
