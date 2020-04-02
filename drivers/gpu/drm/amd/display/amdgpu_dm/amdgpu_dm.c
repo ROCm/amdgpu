b9034656d7e91e2010d98b8f7bee9a77eaa9bd60
Emily Deng <Emily.Deng@amd.com>
2020-04-02 15:06:51 +0800

    amd/amdkcl: Fix the assert report
    Signed-off-by: Emily Deng <Emily.Deng@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Change-Id: If2f264ccc7b2f0e73952fd88b45a23174d22ea61
    Signed-off-by: Yifan Zhang <Yifan1.Zhang@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -1083,7 +1083,7 @@ static int amdgpu_dm_audio_component_get_eld(struct device *kdev, int port,
 	drm_connector_list_iter_begin(dev, &conn_iter);
 	drm_for_each_connector_iter(connector, &conn_iter) {
 #else
-	drm_for_each_connector(connector, dev) {
+	list_for_each_entry(connector, &(dev)->mode_config.connector_list, head) {
 #endif
 		if (connector->connector_type == DRM_MODE_CONNECTOR_WRITEBACK)
 			continue;
@@ -2644,7 +2644,7 @@ static int detect_mst_link_for_all_connectors(struct drm_device *dev)
 	drm_connector_list_iter_begin(dev, &iter);
 	drm_for_each_connector_iter(connector, &iter) {
 #else
-	drm_for_each_connector(connector, dev) {
+	list_for_each_entry(connector, &(dev)->mode_config.connector_list, head) {
 #endif
 		if (connector->connector_type == DRM_MODE_CONNECTOR_WRITEBACK)
 			continue;
@@ -2826,7 +2826,7 @@ static void s3_handle_mst(struct drm_device *dev, bool suspend)
 	drm_connector_list_iter_begin(dev, &iter);
 	drm_for_each_connector_iter(connector, &iter) {
 #else
-	drm_for_each_connector(connector, dev) {
+	list_for_each_entry(connector, &(dev)->mode_config.connector_list, head) {
 #endif
 		if (connector->connector_type == DRM_MODE_CONNECTOR_WRITEBACK)
 			continue;
