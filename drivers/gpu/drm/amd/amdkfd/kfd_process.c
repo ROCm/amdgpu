c824d067e98b791437ffe060fd553c703c60534f
Felix Kuehling <Felix.Kuehling@amd.com>
2020-04-23 03:14:27 -0400

    drm/amdkfd: Proof of concept for KFD traces
    Add tracing to a few KFD functions as a proof of concept.
    
    Signed-off-by: Felix Kuehling <Felix.Kuehling@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_process.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_process.c
@@ -43,6 +43,7 @@ struct mm_struct;
 #include "kfd_priv.h"
 #include "kfd_device_queue_manager.h"
 #include "kfd_svm.h"
+#include "kfd_trace.h"
 #include "kfd_smi_events.h"
 #include "kfd_debug.h"
 
@@ -2303,6 +2304,7 @@ static void evict_process_worker(struct work_struct *work)
 	 * lifetime of this thread, kfd_process p will be valid
 	 */
 	p = container_of(dwork, struct kfd_process, eviction_work);
+	trace_kfd_evict_process_worker_start(p);
 
 	p->last_evict_timestamp = get_jiffies_64();
 
@@ -2321,6 +2323,7 @@ static void evict_process_worker(struct work_struct *work)
 		pr_debug("Finished evicting process pid %d\n", p->lead_thread->pid);
 	} else
 		pr_err("Failed to evict queues of process pid %d\n", p->lead_thread->pid);
+	trace_kfd_evict_process_worker_end(p, ret ? "Failed" : "Success");
 }
 
 static int restore_process_helper(struct kfd_process *p)
@@ -2336,6 +2339,7 @@ static int restore_process_helper(struct kfd_process *p)
 	}
 
 	ret = kfd_process_restore_queues(p);
+	trace_kfd_restore_process_worker_end(p,	ret ? "Failed" : "Success");
 	if (!ret)
 		pr_debug("Finished restoring process pid %d\n",
 			p->lead_thread->pid);
@@ -2359,6 +2363,7 @@ static void restore_process_worker(struct work_struct *work)
 	 */
 	p = container_of(dwork, struct kfd_process, restore_work);
 	pr_debug("Started restoring process pasid %d\n", (int)p->lead_thread->pid);
+	trace_kfd_restore_process_worker_start(p);
 
 	/* Setting last_restore_timestamp before successful restoration.
 	 * Otherwise this would have to be set by KGD (restore_process_bos)
@@ -2379,6 +2384,11 @@ static void restore_process_worker(struct work_struct *work)
 		if (mod_delayed_work(kfd_restore_wq, &p->restore_work,
 				     msecs_to_jiffies(PROCESS_RESTORE_TIME_MS)))
 			kfd_process_restore_queues(p);
+		trace_kfd_restore_process_worker_end(p, ret ?
+					"Rescheduled restore" :
+					"Failed to reschedule restore");
+	} else {
+		trace_kfd_restore_process_worker_end(p, "Success");
 	}
 }
 
