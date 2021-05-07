d53706b6f8341488ac8d552073afbac5e71e1065
Shiwu Zhang <shiwu.zhang@amd.com>
2021-05-07 18:07:00 +0800

    drm/amdkcl: export the symbol of pxm_to_node
    This is caused by "add ACPI SRAT parsing for topology"
    
    v2: dummy pxm_to_node() in case the symbol cannot be found by
    kallsyms_lookup_name()
    
    Signed-off-by: Shiwu Zhang <shiwu.zhang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/amdkcl/main.c
+++ drivers/gpu/drm/amd/amdkcl/main.c
@@ -10,6 +10,7 @@ extern void amdkcl_io_init(void);
 extern void amdkcl_mm_init(void);
 extern void amdkcl_pci_init(void);
 extern void amdkcl_suspend_init(void);
+extern void amdkcl_numa_init(void);
 
 int __init amdkcl_init(void)
 {
@@ -21,6 +22,7 @@ int __init amdkcl_init(void)
 	amdkcl_mm_init();
 	amdkcl_pci_init();
 	amdkcl_suspend_init();
+	amdkcl_numa_init();
 
 	return 0;
 }
