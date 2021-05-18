3fe7297f8c4ee6a7982bd3c7a99c8177457d5062
Leslie Shi <Yuliang.Shi@amd.com>
2021-05-18 15:53:33 +0800

    drm/amdkcl: adapt code for legacy os
    This is caused by following commits:
    d9e1c5963c97 drm/amd/amdgpu: fix refcount leak
    9faf262c32d3 drm/amdgpu: Add DMA mapping of GTT BOs
    ac89ed0789f8 drm/amdgpu: Move kfd_mem_attach outside reservation
    
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Guchun Chen <guchun.chen@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gpuvm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gpuvm.c
@@ -818,6 +818,7 @@ static int kfd_mem_export_dmabuf(struct kgd_mem *mem)
 	return 0;
 }
 
+#ifdef AMDKCL_AMDGPU_DMABUF_OPS
 static int
 kfd_mem_attach_dmabuf(struct amdgpu_device *adev, struct kgd_mem *mem,
 		      struct amdgpu_bo **bo)
@@ -838,6 +839,7 @@ kfd_mem_attach_dmabuf(struct amdgpu_device *adev, struct kgd_mem *mem,
 
 	return 0;
 }
+#endif
 
 /* kfd_mem_attach - Add a BO to a VM
  *
@@ -928,6 +930,7 @@ static int kfd_mem_attach(struct amdgpu_device *adev, struct kgd_mem *mem,
 			ret = create_dmamap_sg_bo(adev, mem, &bo[i]);
 			if (ret)
 				goto unwind;
+#ifdef AMDKCL_AMDGPU_DMABUF_OPS
 		/* Enable acces to GTT and VRAM BOs of peer devices */
 		} else if (mem->domain == AMDGPU_GEM_DOMAIN_GTT ||
 			   mem->domain == AMDGPU_GEM_DOMAIN_VRAM) {
@@ -936,6 +939,7 @@ static int kfd_mem_attach(struct amdgpu_device *adev, struct kgd_mem *mem,
 			if (ret)
 				goto unwind;
 			pr_debug("Employ DMABUF mechanism to enable peer GPU access\n");
+#endif
 		} else {
 			WARN_ONCE(true, "Handling invalid ATTACH request");
 			ret = -EINVAL;
