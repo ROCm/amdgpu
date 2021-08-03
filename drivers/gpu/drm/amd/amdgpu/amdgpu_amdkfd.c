721d519c8d2300fd278927089e7c43eca9059075
Eric Huang <jinhuieric.huang@amd.com>
2021-08-03 14:44:31 -0400

    drm/amdkfd: add parameter force in kfd_process_evict_queues
    It is to differenciate case scenario for proper behavior when
    calling evict queues, such as GPU reset doesn't need to roll
    back restoring partial evicted queues.
    
    Signed-off-by: Eric Huang <jinhuieric.huang@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd.c
@@ -260,7 +260,7 @@ void amdgpu_amdkfd_suspend(struct amdgpu_device *adev, bool suspend_proc)
 		if (adev->in_s0ix)
 			kgd2kfd_stop_sched_all_nodes(adev->kfd.dev);
 		else
-			kgd2kfd_suspend(adev->kfd.dev, suspend_proc);
+			kgd2kfd_suspend(adev->kfd.dev, suspend_proc, true);
 	}
 }
 
@@ -281,7 +281,7 @@ int amdgpu_amdkfd_resume(struct amdgpu_device *adev, bool resume_proc)
 void amdgpu_amdkfd_suspend_process(struct amdgpu_device *adev)
 {
 	if (adev->kfd.dev)
-		kgd2kfd_suspend_process(adev->kfd.dev);
+		kgd2kfd_suspend_process(adev->kfd.dev, true);
 }
 
 int amdgpu_amdkfd_resume_process(struct amdgpu_device *adev)
