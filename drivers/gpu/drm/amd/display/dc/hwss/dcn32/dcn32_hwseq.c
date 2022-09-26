da0103addcb23d8a1765872967118777efdef606
Asher Song <Asher.Song@amd.com>
2022-09-26 15:44:12 +0800

    drm/amdkcl: wrap code under macro CONFIG_DRM_AMD_DC_DSC_SUPPORT
    It's caused by ac2e8b34b24ac3966df28d289fc2c03825bc8f1f
    drm/amd/display: Fix various dynamic ODM transitions on DCN32
    
    Signed-off-by: Asher Song <Asher.Song@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

--- drivers/gpu/drm/amd/display/dc/hwss/dcn32/dcn32_hwseq.c
+++ drivers/gpu/drm/amd/display/dc/hwss/dcn32/dcn32_hwseq.c
@@ -1537,6 +1537,7 @@ bool dcn32_dsc_pg_status(
 	return pwr_status == 0;
 }
 
+#ifdef CONFIG_DRM_AMD_DC_DSC_SUPPORT
 void dcn32_update_dsc_pg(struct dc *dc,
 		struct dc_state *context,
 		bool safe_to_disable)
@@ -1559,6 +1560,7 @@ void dcn32_update_dsc_pg(struct dc *dc,
 		}
 	}
 }
+#endif
 
 void dcn32_disable_phantom_streams(struct dc *dc, struct dc_state *context)
 {
