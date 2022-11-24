61181691ea9ff48eaea32f94cb05033bcb634bfb
Ma Jun <Jun.Ma2@amd.com>
2022-11-24 13:15:48 +0800

    drm/amdkcl: Check if drm_dp_mst_root_conn_atomic_check() is available
    Check if drm_dp_mst_root_conn_atomic_check() is available
    
    Signed-off-by: Ma Jun <Jun.Ma2@amd.com>
    Change-Id: I3b91f33e2fd780e3b2c952aa2c5c95bcaae78a52

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -8707,7 +8707,9 @@ amdgpu_dm_connector_atomic_check(struct drm_connector *conn,
 		drm_atomic_get_old_connector_state(state, conn);
 	struct drm_crtc *crtc = new_con_state->crtc;
 	struct drm_crtc_state *new_crtc_state;
+#ifdef HAVE_DRM_DP_MST_ROOT_CONN_ATOMIC_CHECK
 	struct amdgpu_dm_connector *aconn = to_amdgpu_dm_connector(conn);
+#endif
 	int ret;
 
 	if (WARN_ON(unlikely(!old_con_state || !new_con_state)))
@@ -8716,9 +8718,11 @@ amdgpu_dm_connector_atomic_check(struct drm_connector *conn,
 	trace_amdgpu_dm_connector_atomic_check(new_con_state);
 
 	if (conn->connector_type == DRM_MODE_CONNECTOR_DisplayPort) {
+#ifdef HAVE_DRM_DP_MST_ROOT_CONN_ATOMIC_CHECK
 		ret = drm_dp_mst_root_conn_atomic_check(new_con_state, &aconn->mst_mgr);
 		if (ret < 0)
 			return ret;
+#endif
 	}
 
 	if (!crtc)
@@ -8778,6 +8782,7 @@ amdgpu_dm_connector_atomic_check(struct drm_connector *conn,
 	return 0;
 }
 #endif
+
 static struct drm_encoder *amdgpu_dm_connector_to_encoder(struct drm_connector *connector)
 {
 #ifdef HAVE_DRM_CONNECTOR_FOR_EACH_POSSIBLE_ENCODER_2ARGS
