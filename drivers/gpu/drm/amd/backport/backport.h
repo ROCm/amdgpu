a558de7265470108f6f3cf3d49e0d38de610c5b5
Leslie Shi <Yuliang.Shi@amd.com>
2023-02-20 11:58:45 +0800

    drm/amdkcl: kcl-cleanup HAVE_DRM_MM_INSERT_MODE
    This patch also removes macro HAVE_DRM_MM_INSERT_NODE_THREE_PARAMETERS
    
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Reviewed-by: Ma Jun <Jun.Ma2@amd.com>

--- drivers/gpu/drm/amd/backport/backport.h
+++ drivers/gpu/drm/amd/backport/backport.h
@@ -61,7 +61,6 @@
 #include <kcl/kcl_drm_fourcc.h>
 #include <kcl/kcl_sysfs_emit.h>
 #include <kcl/kcl_bitops.h>
-#include <kcl/backport/kcl_drm_mm_backport.h>
 #include <kcl/kcl_sched_mm.h>
 #include <kcl/backport/kcl_numa_backport.h>
 #include "kcl/kcl_amdgpu_drm_fb_helper.h"
