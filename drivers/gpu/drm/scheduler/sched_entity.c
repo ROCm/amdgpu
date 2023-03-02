5412853a4de8c8fcafb166c19519dc500f58af84
bobzhou <bob.zhou@amd.com>
2023-03-02 13:19:09 +0800

    drm/amdkcl: rename dependency callback into prepare_job
    It's caused by a25fe9259b68c90033850033ad1b0f20b41e8980
    "drm/scheduler: rename dependency callback into prepare_job"
    
    Signed-off-by: bobzhou <bob.zhou@amd.com>
    Reviewed-by: Leslie Shi <Yuliang.Shi@amd.com>

--- drivers/gpu/drm/scheduler/sched_entity.c
+++ drivers/gpu/drm/scheduler/sched_entity.c
@@ -348,7 +348,7 @@ static void drm_sched_entity_kill_jobs(struct drm_sched_entity *entity)
 		struct drm_sched_fence *s_fence = job->s_fence;
 
 		/* Wait for all dependencies to avoid data corruptions */
-		while ((f = job->sched->ops->dependency(job, entity))) {
+		while ((f = job->sched->ops->prepare_job(job, entity))) {
 			dma_fence_wait(f, false);
 			dma_fence_put(f);
 		}
@@ -570,7 +570,7 @@ struct drm_sched_job *drm_sched_entity_pop_job(struct drm_sched_entity *entity)
 #ifdef HAVE_STRUCT_XARRAY
 			drm_sched_job_dependency(sched_job, entity))) {
 #else
-			sched->ops->dependency(sched_job, entity))) {
+			sched->ops->prepare_job(sched_job, entity))) {
 #endif
 		if (drm_sched_entity_add_dependency_cb(entity, sched_job))
 			return NULL;
