bb6d3fd273fe5948759cc8eb700f9b8384f321dd
Bob Zhou <bob.zhou@amd.com>
2023-04-23 11:29:55 +0800

    drm/amdkcl: test whether drm_connector->edid_override is available
    It's caused by 4596e8af5f3d520dcd2edf009aa785ad4cdc50e8
    "drm/amd/display: implement force function in amdgpu_dm_connector_funcs"
    
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>
    Reviewed-by: Asher Song <Asher.Song@amd.com>

--- drivers/gpu/drm/amd/dkms/config/config.h
+++ drivers/gpu/drm/amd/dkms/config/config.h
@@ -831,8 +831,11 @@
 /* memalloc_noreclaim_save() is available */
 #define HAVE_MEMALLOC_NORECLAIM_SAVE 1
 
-/* mem_encrypt_active() is available */
-#define HAVE_MEM_ENCRYPT_ACTIVE 1
+/* drm_connector->edid_override is available */
+#define HAVE_DRM_CONNECTOR_EDID_OVERRIDE 1
+
+/* struct migrate_vma has fault_page */
+#define HAVE_MIGRATE_VMA_FAULT_PAGE 1
 
 /* migrate_vma->pgmap_owner is available */
 #define HAVE_MIGRATE_VMA_PGMAP_OWNER 1
