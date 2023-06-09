91edc96755e4b12d1d46e3164e1375ecf239878b
Bob Zhou <bob.zhou@amd.com>
2023-06-09 13:39:20 +0800

    drm/amdkcl: fake drm_mode_create_colorspace_property functions
    It's cauesd by a9fa9b21c98f7ebeb76897c1f9c6796508aed5a5
    "drm/amd/display: Register Colorspace property for DP and HDMI"
    
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>

--- drivers/gpu/drm/amd/dkms/config/config.h
+++ drivers/gpu/drm/amd/dkms/config/config.h
@@ -487,6 +487,21 @@
 /* drm_dsc_compute_rc_parameters() is available */
 #define HAVE_DRM_DSC_COMPUTE_RC_PARAMETERS 1
 
+/* drm_connector_attach_colorspace_property() is available */
+#define HAVE_DRM_CONNECT_ATTACH_COLORSPACE_PROPERTY 1
+
+/* drm_mode_create_hdmi_colorspace_property() has 2 args */
+#define HAVE_DRM_MODE_CREATE_HDMI_COLORSPACE_PROPERTY_2ARGS 1
+
+/* drm_mode_create_dp_colorspace_property() has 2 args */
+#define HAVE_DRM_MODE_CREATE_DP_COLORSPACE_PROPERTY_2ARGS 1
+
+/* struct drm_dsc_config has member simple_422 */
+#define HAVE_DRM_DSC_CONFIG_SIMPLE_422 1
+
+/* drm_dsc_pps_payload_pack() is available */
+#define HAVE_DRM_DSC_PPS_PAYLOAD_PACK 1
+
 /* drm_fb_helper_single_add_all_connectors() &&
    drm_fb_helper_remove_one_connector() are symbol */
 /* #undef HAVE_DRM_FB_HELPER_ADD_REMOVE_CONNECTORS */
