d385461cf70f64e7d834000eef5e923e868b301f
Bob Zhou <bob.zhou@amd.com>
2023-07-18 15:32:09 +0800

    drm/amdkcl: test whether drm_dp_mst_hpd_irq_handle_event() is available
    It's caused by 55970ce5015265eb0985f518995aa8fc4b3fa384
    "drm/dp_mst: Clear MSG_RDY flag before sending new message"
    
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>
    Reviewed-by: Leslie Shi <Yuliang.Shi@amd.com>

--- drivers/gpu/drm/amd/dkms/config/config.h
+++ drivers/gpu/drm/amd/dkms/config/config.h
@@ -367,6 +367,12 @@
 /* drm_dp_mst_{get,put}_port_malloc() is available */
 #define HAVE_DRM_DP_MST_GET_PUT_PORT_MALLOC 1
 
+/* drm_dp_mst_hpd_irq_handle_event() is available */
+#define HAVE_DRM_DP_MST_HPD_IRQ_HANDLE_EVENT 1
+
+/* drm_dp_mst_port struct has full_pbn member */
+#define HAVE_DRM_DP_MST_PORT_FULL_PBN 1
+
 /* struct drm_dp_mst_port has passthrough_aux member */
 /* #undef HAVE_DRM_DP_MST_PORT_PASSTHROUGH_AUX */
 
