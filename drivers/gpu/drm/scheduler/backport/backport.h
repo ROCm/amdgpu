12266bb6dbc4a60662dabbcd715295172d822e10
Ma Jun <Jun.Ma2@amd.com>
2023-08-24 17:59:02 +0800

    drm/amdkcl: Include kcl_rbtree.h in the backport.h
    Include kcl_rbtree.h in the backport.h instead of
    gpu_scheduler.h
    
    Signed-off-by: Ma Jun <Jun.Ma2@amd.com>
    Reviewed-by: Flora Cui <Flora.Cui@amd.com>

--- drivers/gpu/drm/scheduler/backport/backport.h
+++ drivers/gpu/drm/scheduler/backport/backport.h
@@ -6,6 +6,6 @@
 #include <kcl/kcl_compiler_attributes.h>
 #include <kcl/backport/kcl_fence_backport.h>
 #include <kcl/backport/kcl_drm_print.h>
-#include <kcl/kcl_kref.h>
+#include <kcl/kcl_rbtree.h>
 
 #endif
