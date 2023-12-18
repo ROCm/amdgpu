f79080f1534d87426d928ef6020116590574c6d7
Bob Zhou <bob.zhou@amd.com>
2023-12-18 13:54:33 +0800

    drm/amdkcl: test whether struct drm_color_ctm_3x4 is available
    It's caused by 2d4457c2d03ed0e2fcf4206a10c0bdfcab4fd03f
    "drm/amd/display: Add 3x4 CTM support for plane CTM"
    
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>
    Reviewed-by: Leslie Shi <Yuliang.Shi@amd.com>

--- include/uapi/drm/amdgpu_drm.h
+++ include/uapi/drm/amdgpu_drm.h
@@ -1670,6 +1670,7 @@ struct drm_amdgpu_info_uq_metadata {
 #define AMDGPU_FAMILY_GC_11_5_0			150 /* GC 11.5.0 */
 #define AMDGPU_FAMILY_GC_12_0_0			152 /* GC 12.0.0 */
 
+#ifndef HAVE_DRM_COLOR_CTM_3X4
 /* FIXME wrong namespace! */
 struct drm_color_ctm_3x4 {
 	/*
@@ -1678,6 +1679,7 @@ struct drm_color_ctm_3x4 {
 	 */
 	__u64 matrix[12];
 };
+#endif
 
 /**
  *  Definition of System Unified Address (SUA) apertures
