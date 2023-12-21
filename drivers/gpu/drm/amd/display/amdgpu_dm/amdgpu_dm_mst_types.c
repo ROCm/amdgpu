8e04c799418be659a33aada53f7920e32fac8e89
Asher Song <Asher.Song@amd.com>
2023-12-21 16:14:20 +0800

    drm/amdkcl: test struct drm_dp_mst_topology_state whether has union member pbn_div
    It's caused by v6.6-rc2-733-g191dc43935d1
    drm/dp_mst: Store the MST PBN divider value in fixed point format
    
    Signed-off-by: Asher Song <Asher.Song@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
@@ -1101,7 +1101,7 @@ static int increase_dsc_bpp(struct drm_atomic_state *state,
 	int min_initial_slack;
 	int next_index;
 	int remaining_to_increase = 0;
-#ifndef HAVE_DRM_DP_MST_TOPOLOGY_STATE_PBN_DIV
+#if !defined(HAVE_DRM_DP_MST_TOPOLOGY_STATE_PBN_DIV_INT) && !defined(HAVE_DRM_DP_MST_TOPOLOGY_STATE_PBN_DIV_UNION)
 	int pbn_per_timeslot;
 #endif
 	int link_timeslots_used;
@@ -1109,7 +1109,7 @@ static int increase_dsc_bpp(struct drm_atomic_state *state,
 	int ret = 0;
 	uint16_t fec_overhead_multiplier_x1000 = get_fec_overhead_multiplier(dc_link);
 
-#ifndef HAVE_DRM_DP_MST_TOPOLOGY_STATE_PBN_DIV
+#if !defined(HAVE_DRM_DP_MST_TOPOLOGY_STATE_PBN_DIV_INT) && !defined(HAVE_DRM_DP_MST_TOPOLOGY_STATE_PBN_DIV_UNION)
 	pbn_per_timeslot = dm_mst_get_pbn_divider(dc_link);
 #endif
 
@@ -1144,8 +1144,10 @@ static int increase_dsc_bpp(struct drm_atomic_state *state,
 
 		for (i = 0; i < count; i++)
 			link_timeslots_used += DIV_ROUND_UP(vars[i + k].pbn, 
-#ifdef HAVE_DRM_DP_MST_TOPOLOGY_STATE_PBN_DIV
+#ifdef HAVE_DRM_DP_MST_TOPOLOGY_STATE_PBN_DIV_UNION
 							    dfixed_trunc(mst_state->pbn_div)
+#elif HAVE_DRM_DP_MST_TOPOLOGY_STATE_PBN_DIV_INT
+							    mst_state->pbn_div
 #else
 							    pbn_per_timeslot
 #endif
@@ -1153,8 +1155,10 @@ static int increase_dsc_bpp(struct drm_atomic_state *state,
 
 		fair_pbn_alloc =
 			(63 - link_timeslots_used) / remaining_to_increase *
-#ifdef HAVE_DRM_DP_MST_TOPOLOGY_STATE_PBN_DIV
+#ifdef HAVE_DRM_DP_MST_TOPOLOGY_STATE_PBN_DIV_UNION
 			dfixed_trunc(mst_state->pbn_div);
+#elif HAVE_DRM_DP_MST_TOPOLOGY_STATE_PBN_DIV_INT
+			mst_state->pbn_div;
 #else
 			pbn_per_timeslot;
 #endif
