55ff0278dd1239b0bb379d7e0dffa0b16aca6306
Bob Zhou <bob.zhou@amd.com>
2024-01-17 14:07:56 +0800

    drm/amdgpu: Synchronize after mapping into a compute VM
    Compute VMs use user mode queues for command submission. They
    cannot use a CS ioctl to synchronize with pending PTE updates
    and flush TLBs. Do this synchronization in amdgpu_gem_va_ioctl
    for compute VMs.
    
    Signed-off-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_gem.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_gem.c
@@ -883,11 +883,12 @@ out:
  */
 static struct dma_fence *
 amdgpu_gem_va_update_vm(struct amdgpu_device *adev,
-			struct amdgpu_vm *vm,
+			struct amdgpu_fpriv *fpriv,
 			struct amdgpu_bo_va *bo_va,
 			uint32_t operation)
 {
 	struct dma_fence *fence;
+	struct amdgpu_vm *vm = &fpriv->vm;
 	int r = 0;
 
 	/* If the VM is not ready return only a stub. */
@@ -918,6 +919,23 @@ amdgpu_gem_va_update_vm(struct amdgpu_device *adev,
 	r = amdgpu_vm_update_pdes(adev, vm, false);
 	if (r)
 		goto error;
+
+	if (vm->is_compute_context) {
+		if (bo_va->last_pt_update)
+			r = dma_fence_wait(bo_va->last_pt_update, true);
+		if (!r && vm->last_update)
+			r = dma_fence_wait(vm->last_update, true);
+		if (!r) {
+			uint32_t xcc_mask = (!adev->xcp_mgr ||
+					     fpriv->xcp_id == ~0) ? 1 :
+				adev->xcp_mgr->xcp[fpriv->xcp_id]
+					.ip[AMDGPU_XCP_GFX].inst_mask;
+
+			r = amdgpu_vm_flush_compute_tlb(adev, vm,
+							TLB_FLUSH_HEAVYWEIGHT,
+							xcc_mask);
+		}
+	}
 
 	if ((operation == AMDGPU_VA_OP_MAP ||
 	     operation == AMDGPU_VA_OP_REPLACE) &&
@@ -1115,7 +1133,7 @@ int amdgpu_gem_va_ioctl(struct drm_device *dev, void *data,
 	    (!adev->debug_vm || timeline_syncobj)) {
 		struct dma_fence *fence;
 
-		fence = amdgpu_gem_va_update_vm(adev, &fpriv->vm, bo_va,
+		fence = amdgpu_gem_va_update_vm(adev, fpriv, bo_va,
 						args->operation);
 		if (timeline_syncobj) {
 			if (!args->vm_timeline_point) {
