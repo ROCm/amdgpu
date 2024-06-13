81e0952e5da1bbce5d8f09585460edf0020b0c22
Bob Zhou <bob.zhou@amd.com>
2024-06-13 13:39:28 +0800

    drm/amdkcl: Test drm_atomic_plane_enabling() is available
    It's caused by 1d0c9ded451944ef616149be5fe4a7d9a9d428da
    "drm/amd/display: Introduce overlay cursor mode"
    
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>
    Reviewed-by: Asher Song <Asher.Song@amd.com>

--- drivers/gpu/drm/amd/dkms/config/config.h
+++ drivers/gpu/drm/amd/dkms/config/config.h
@@ -183,6 +183,9 @@
 /* drm_atomic_helper_calc_timestamping_constants() is available */
 #define HAVE_DRM_ATOMIC_HELPER_CALC_TIMESTAMPING_CONSTANTS 1
 
+/* drm_atomic_plane_enabling() is available */
+#define HAVE_DRM_ATOMIC_PLANE_ENABLING 1
+
 /* drm_client_register() is available */
 #define HAVE_DRM_CLIENT_REGISTER 1
 
