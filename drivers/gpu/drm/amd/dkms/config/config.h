dac681eae7796979938a597aa6a9ebb44aad1287
Bob Zhou <bob.zhou@amd.com>
2024-09-23 13:22:28 +0800

    drm/amdkcl: test drm_gem_prime_handle_to_dmabuf() is available
    It's caused by eeab2428df5a886d40c08f9847ea4d2c2fbce7e0
    "drm/amdgpu: fix a race in kfd_mem_export_dmabuf()"
    
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>
    Reviewed-by: Asher Song <Asher.Song@amd.com>

--- drivers/gpu/drm/amd/dkms/config/config.h
+++ drivers/gpu/drm/amd/dkms/config/config.h
@@ -558,6 +558,9 @@
 /* drm_gen_fb_init_with_funcs() is available */
 #define HAVE_DRM_GEN_FB_INIT_WITH_FUNCS 1
 
+/* drm_gem_prime_handle_to_dmabuf() is available */
+#define HAVE_DRM_GEM_PRIME_HANDLE_TO_DMABUF 1
+
 /* drm_gem_prime_handle_to_fd() is available */
 #define HAVE_DRM_GEM_PRIME_HANDLE_TO_FD 1
 
