af4d276ec7a313c8cc25a115ec23e8f77d2db8ca
James Zhu <James.Zhu@amd.com>
2024-10-11 13:40:42 -0400

    drm/amdkfd: refactor rlc/gfx spm
    for later adding multiple xcc support.
    
    Signed-off-by: James Zhu <James.Zhu@amd.com>
    Reviewed-by: Bing Ma <Bing.Ma@amd.com>
    Reviewed-by: Gang Ba <gaba@amd.com>

--- drivers/gpu/drm/amd/amdgpu/gfx_v9_0.c
+++ drivers/gpu/drm/amd/amdgpu/gfx_v9_0.c
@@ -4806,7 +4806,7 @@ fail:
 	return r;
 }
 
-static void gfx_v9_0_spm_start(struct amdgpu_device *adev)
+static void gfx_v9_0_spm_start(struct amdgpu_device *adev, int xcc_id)
 {
 	struct amdgpu_ring *kiq_ring = &adev->gfx.kiq[0].ring;
 	uint32_t data = 0;
@@ -4831,7 +4831,7 @@ static void gfx_v9_0_spm_start(struct amdgpu_device *adev)
 			SOC15_REG_OFFSET(GC, 0, mmRLC_SPM_INT_CNTL), 1);
 }
 
-static void gfx_v9_0_spm_stop(struct amdgpu_device *adev)
+static void gfx_v9_0_spm_stop(struct amdgpu_device *adev, int xcc_id)
 {
 	struct amdgpu_ring *kiq_ring = &adev->gfx.kiq[0].ring;
 	uint32_t data = 0;
@@ -4853,7 +4853,7 @@ static void gfx_v9_0_spm_stop(struct amdgpu_device *adev)
 			SOC15_REG_OFFSET(GC, 0, mmRLC_SPM_RING_RDPTR), 0);
 }
 
-static void gfx_v9_0_spm_set_rdptr(struct amdgpu_device *adev,  u32 rptr)
+static void gfx_v9_0_spm_set_rdptr(struct amdgpu_device *adev, int xcc_id,  u32 rptr)
 {
 	struct amdgpu_ring *kiq_ring = &adev->gfx.kiq[0].ring;
 
@@ -4861,7 +4861,8 @@ static void gfx_v9_0_spm_set_rdptr(struct amdgpu_device *adev,  u32 rptr)
 			SOC15_REG_OFFSET(GC, 0, mmRLC_SPM_RING_RDPTR), rptr);
 }
 
-static void gfx_v9_0_set_spm_perfmon_ring_buf(struct amdgpu_device *adev, u64 gpu_addr, u32 size)
+static void gfx_v9_0_set_spm_perfmon_ring_buf(struct amdgpu_device *adev,
+					    int xcc_id, u64 gpu_addr, u32 size)
 {
 	struct amdgpu_ring *kiq_ring = &adev->gfx.kiq[0].ring;
 
@@ -5281,7 +5282,8 @@ static void gfx_v9_0_update_spm_vmid_internal(struct amdgpu_device *adev,
 		WREG32_SOC15(GC, 0, mmRLC_SPM_MC_CNTL, data);
 }
 
-static void gfx_v9_0_update_spm_vmid(struct amdgpu_device *adev, struct amdgpu_ring *ring, unsigned int vmid)
+static void gfx_v9_0_update_spm_vmid(struct amdgpu_device *adev, int xcc_id,
+		struct amdgpu_ring *ring, unsigned int vmid)
 {
 	amdgpu_gfx_off_ctrl(adev, false);
 
@@ -7231,7 +7233,7 @@ static int gfx_v9_0_spm_irq(struct amdgpu_device *adev,
 			     struct amdgpu_irq_src *source,
 			     struct amdgpu_iv_entry *entry)
 {
-	amdgpu_amdkfd_rlc_spm_interrupt(adev);
+	amdgpu_amdkfd_rlc_spm_interrupt(adev, 0);
 	return 0;
 }
 
