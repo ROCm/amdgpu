61c1da32648b7a4aebabebae1ec7f65d491b742b
Flora Cui <flora.cui@amd.com>
2024-11-07 14:36:23 +0800

    drm/amdkcl: move reservation_ww_class check to compile-time
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Bob Zhou <bob.zhou@amd.com>

--- drivers/gpu/drm/amd/amdkcl/main.c
+++ drivers/gpu/drm/amd/amdkcl/main.c
@@ -5,7 +5,6 @@
 extern void amdkcl_symbol_init(void);
 extern void amdkcl_dev_cgroup_init(void);
 extern void amdkcl_fence_init(void);
-extern void amdkcl_reservation_init(void);
 extern void amdkcl_io_init(void);
 extern void amdkcl_mm_init(void);
 extern void amdkcl_suspend_init(void);
@@ -18,7 +17,6 @@ int __init amdkcl_init(void)
 	amdkcl_symbol_init();
 	amdkcl_dev_cgroup_init();
 	amdkcl_fence_init();
-	amdkcl_reservation_init();
 	amdkcl_io_init();
 	amdkcl_mm_init();
 	amdkcl_suspend_init();
