b0f7256af3470708b96dfe2cd9a46ea31c4d487d
chengjya <chengjya@amd.com>
2024-11-28 17:32:37 +0800

    drm/amdkcl: test __sg_alloc_table_from_pages() has 9 args
    It's caused by follow commit:166ea4a47bdfec1a789c98a6a5bab70274d58502
    "drm/amdgpu: Use SG helper for peer-direct sgtable"
    
    Signed-off-by: chengjya <chengjya@amd.com>
    Reviewed-by: Bob Zhou <Bob.Zhou@amd.com>

--- drivers/gpu/drm/amd/dkms/config/config.h
+++ drivers/gpu/drm/amd/dkms/config/config.h
@@ -1159,20 +1159,8 @@
 /* __kthread_should_park() is available */
 /* #undef HAVE___KTHREAD_SHOULD_PARK */
 
-/* __print_array is available */
-#define HAVE___PRINT_ARRAY 1
-
-/* kobj_type->default_groups is available */
-#define HAVE_DEFAULT_GROUP_IN_KOBJ_TYPE 1
-
-/* close_fd() is available */
-#define HAVE_KERNEL_CLOSE_FD 1
-
-/* ksys_close() is available */
-#define HAVE_KSYS_CLOSE_FD 1
-
-/* pm_suspend_target_state is available */
-#define HAVE_PM_SUSPEND_TARGET_STATE 1
+/* __sg_alloc_table_from_pages() has 9 args */
+/* #undef HAVE___SG_ALLOC_TABLE_FROM_PAGES_9ARGS */
 
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT ""
