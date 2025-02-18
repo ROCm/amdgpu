72a3b647281c5686656a2b3c79e62d96a605aea6
Alex Deucher <alexander.deucher@amd.com>
2025-02-18 12:07:48 -0500

    drm/amdgpu/gfx: add generic handling for disable_kq
    Add proper checks for disable_kq functionality in
    gfx helper functions.  Add special logic for families
    that require the clear state setup.
    
    v2: use ring count as per Felix suggestion
    v3: fix num_gfx_rings handling in amdgpu_gfx_graphics_queue_acquire()
    
    Reviewed-by: Prike Liang <Prike.Liang@amd.com>
    Reviewed-by: Sunil Khatri <sunil.khatri@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_gfx.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_gfx.c
@@ -259,8 +259,9 @@ void amdgpu_gfx_graphics_queue_acquire(struct amdgpu_device *adev)
 	}
 
 	/* update the number of active graphics rings */
-	adev->gfx.num_gfx_rings =
-		bitmap_weight(adev->gfx.me.queue_bitmap, AMDGPU_MAX_GFX_QUEUES);
+	if (adev->gfx.num_gfx_rings)
+		adev->gfx.num_gfx_rings =
+			bitmap_weight(adev->gfx.me.queue_bitmap, AMDGPU_MAX_GFX_QUEUES);
 }
 
 static int amdgpu_gfx_kiq_acquire(struct amdgpu_device *adev,
@@ -1544,6 +1545,9 @@ static ssize_t amdgpu_gfx_set_run_cleaner_shader(struct device *dev,
 		return -EPERM;
 	if (adev->in_suspend && !adev->in_runpm)
 		return -EPERM;
+
+	if (adev->gfx.disable_kq)
+		return -ENOTSUPP;
 
 	ret = kstrtol(buf, 0, &value);
 
