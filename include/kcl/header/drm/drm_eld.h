8351463f491ca1b6cbb2388cf61b989b797eef08
Bob Zhou <bob.zhou@amd.com>
2025-02-21 15:26:24 +0800

    drm/amdkcl: fix macro _KCL_HEADER_DRM_ELD_H_H_
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>
    Reviewed-by: Chengjun Yao <Chengjun.Yao@amd.com>

--- include/kcl/header/drm/drm_eld.h
+++ include/kcl/header/drm/drm_eld.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: MIT */
 #ifndef _KCL_HEADER_DRM_ELD_H_
-#define _KCL_HEADER_DRM_ELD_H_H_
+#define _KCL_HEADER_DRM_ELD_H_
 
 #ifdef HAVE_DRM_DRM_ELD_H
 #include_next <drm/drm_eld.h>
