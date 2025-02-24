0604f1951db80e56a4cf60fcd4c8fb6bb18215ca
chengjya <chengjya@amd.com>
2025-02-24 11:25:39 +0800

    drm/amdkcl: fix the redefinetion about the struct dc_link *dc_link
    It's caused by the following commit: 69b01ad9e
    "drm/amd/display: restore edid reading from a given i2c adapter"
    It re-defines 'dc_link' , will conflicts with its definition in KCL.
    
    Signed-off-by: chengjya <chengjya@amd.com>
    Reviewed-by: Bob Zhou <bob.zhou@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -7678,10 +7678,10 @@ static void create_eml_sink(struct amdgpu_dm_connector *aconnector)
 			.link = aconnector->dc_link,
 			.sink_signal = SIGNAL_TYPE_VIRTUAL
 	};
+	struct i2c_adapter *ddc;
 #ifdef HAVE_DRM_DP_MST_EDID_READ
 	const struct drm_edid *drm_edid;
 	const struct edid *edid;
-	struct i2c_adapter *ddc;
 
 	if (dc_link && dc_link->aux_mode)
 		ddc = &aconnector->dm_dp_aux.aux.ddc;
@@ -7692,9 +7692,7 @@ static void create_eml_sink(struct amdgpu_dm_connector *aconnector)
 	drm_edid_connector_update(connector, drm_edid);
 	if (!drm_edid) {
 #else
-	struct dc_link *dc_link = aconnector->dc_link;
 	struct edid *edid;
-	struct i2c_adapter *ddc;
 
 	if (dc_link->aux_mode)
 		ddc = &aconnector->dm_dp_aux.aux.ddc;
