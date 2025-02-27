6f455980005751cfd5742662ffa1f43aa8f8a42b
chengjya <chengjya@amd.com>
2025-02-27 18:18:26 +0800

    drm/amdkcl : Corrected the usage of an incorrect variable name
    It's caused by the following commit: 3529abc32
    "drm/amdgpu: [hybrid] disable dGMA when iommu is enabled and not in PT mode"
    
    Signed-off-by: chengjya <chengjya@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.c
@@ -2117,7 +2117,7 @@ static int amdgpu_direct_gma_init(struct amdgpu_device *adev)
 
 	if (amdgpu_direct_gma_size > 0) {
 		struct iommu_domain *dom = iommu_get_domain_for_dev(adev->dev);
-		if (dom && domain->type != IOMMU_DOMAIN_IDENTITY) {
+		if (dom && dom->type != IOMMU_DOMAIN_IDENTITY) {
 			DRM_INFO("IOMMU is enabled and not in pass through mode, disable direct GMA\n");
 			amdgpu_direct_gma_size = 0;
 		}
