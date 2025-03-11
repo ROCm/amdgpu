77598eaf7c1437bd8a594949b4f172962e60ace0
Asher Song <Asher.Song@amd.com>
2025-03-11 13:48:19 +0800

    drm/amdkcl: test the argument in attribut callback of bin_is_visible wether is constant
    It's caused by v6.12-rc6-16-gb626816fdd7f
    sysfs: treewide: constify attribute callback of bin_is_visible()
    
    Signed-off-by: Asher Song <Asher.Song@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_psp.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_psp.c
@@ -4304,7 +4304,10 @@ static umode_t amdgpu_flash_attr_is_visible(struct kobject *kobj, struct attribu
 
 #ifdef HAVE_ATTRIBUTE_GROUP_IS_BIN_VISIBLE
 static umode_t amdgpu_bin_flash_attr_is_visible(struct kobject *kobj,
-						const struct bin_attribute *attr,
+#ifdef HAVE_CONSTANT_ARGUMENT_IN_IS_BIN_VISIBLE
+						const
+#endif
+						struct bin_attribute *attr,
 						int idx)
 {
 	struct device *dev = kobj_to_dev(kobj);
