d0633dcceb1407222f5b1016fb99bee3d826cae5
Asher Song <Asher.Song@amd.com>
2025-03-24 18:08:42 +0800

    drm/amdkcl: test whether drm_driver->date exists
    It's caused by
    commit v6.13-rc1-117-gcb2e1c2136f7
    drm: remove driver date from struct drm_driver and all drivers
    
    v4.9-rc4-834-g85e634bce01a
    drm: Extract drm_drv.h
    
    Signed-off-by: Asher Song <Asher.Song@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -3098,6 +3098,9 @@ static struct drm_driver amdgpu_kms_driver = {
 
 	.name = DRIVER_NAME,
 	.desc = DRIVER_DESC,
+#ifdef HAVE_DRM_DRIVER_DATE
+	.date = DRIVER_DATE,
+#endif
 	.major = KMS_DRIVER_MAJOR,
 	.minor = KMS_DRIVER_MINOR,
 	.patchlevel = KMS_DRIVER_PATCHLEVEL,
@@ -3131,6 +3134,9 @@ const struct drm_driver amdgpu_partition_driver = {
 
 	.name = DRIVER_NAME,
 	.desc = DRIVER_DESC,
+#ifdef HAVE_DRM_DRIVER_DATE
+	.date = DRIVER_DATE,
+#endif
 	.major = KMS_DRIVER_MAJOR,
 	.minor = KMS_DRIVER_MINOR,
 	.patchlevel = KMS_DRIVER_PATCHLEVEL,
