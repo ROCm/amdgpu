780638f53d2630230d909cdc8c0ae8aaf2d6f7fc
Bob Zhou <bob.zhou@amd.com>
2025-03-25 16:16:25 +0800

    drm/amdkcl: wrap code under amdkcl_gem_resvp
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>

---
+++ drivers/gpu/drm/amd/dkms/backport_drm_exec.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _KCL_BACKPORT_DRM_EXEC_H
+#define _KCL_BACKPORT_DRM_EXEC_H
+
+#include <kcl/kcl_mm.h>
+#include <kcl/kcl_mmzone.h>
+#include <drm/ttm/ttm_bo.h>
+#include <kcl/kcl_drm_gem.h>
+
+#endif
