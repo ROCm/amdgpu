c6acdb50fb536e3f9db0acc194fd2d4c4d85f062
chengjya <chengjya@amd.com>
2025-03-31 17:12:48 +0800

    drm/amdkcl: fake __must_be_cstr macro defintion
    It's caused by the commit:bdbabfe4
    "drm/amdgpu: Replace deprecated function strcpy() with strscpy()"
    
    Signed-off-by: chengjya <chengjya@amd.com>
    Reviewed-by: Bob Zhou <Bob.Zhou@amd.com>

--- drivers/gpu/drm/amd/dkms/config/config.h
+++ drivers/gpu/drm/amd/dkms/config/config.h
@@ -831,6 +831,9 @@
 /* Define to 1 if you have the <linux/cleanup.h> header file. */
 #define HAVE_LINUX_CLEANUP_H 1
 
+/* Define to 1 if you have the <linux/compiler.h> header file. */
+#define HAVE_LINUX_COMPILER_H 1
+
 /* Define to 1 if you have the <linux/compiler_attributes.h> header file. */
 #define HAVE_LINUX_COMPILER_ATTRIBUTES_H 1
 
