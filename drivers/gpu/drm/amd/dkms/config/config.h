af1d0606ae0709742b34f3ed520a484d06ae1d24
chengjya <chengjya@amd.com>
2025-03-31 16:48:19 +0800

    drm/amdkcl: fake the __annotated macro defintion
    It's caused by the commit:bdbabfe4
    "drm/amdgpu: Replace deprecated function strcpy() with strscpy()"
    
    Signed-off-by: chengjya <chengjya@amd.com>
    Reviewed-by: Bob Zhou <Bob.Zhou@amd.com>

--- drivers/gpu/drm/amd/dkms/config/config.h
+++ drivers/gpu/drm/amd/dkms/config/config.h
@@ -834,6 +834,9 @@
 /* Define to 1 if you have the <linux/compiler_attributes.h> header file. */
 #define HAVE_LINUX_COMPILER_ATTRIBUTES_H 1
 
+/* Define to 1 if you have the <linux/compiler_types.h> header file. */
+#define HAVE_LINUX_COMPILER_TYPES_H 1
+
 /* Define to 1 if you have the <linux/container_of.h> header file. */
 #define HAVE_LINUX_CONTAINER_OF_H 1
 
