9d47ed7a914aaac726525ddd1b4cca0988e98c61
chengjya <chengjya@amd.com>
2025-03-31 16:22:40 +0800

    drm/amdkcl: fake COUNT_ARGS() and CONCATENATE() macro definition
    It's caused by the commit:bdbabfe4
    "drm/amdgpu: Replace deprecated function strcpy() with strscpy()"
    
    Signed-off-by: chengjya <chengjya@amd.com>
    Reviewed-by: Bob Zhou <Bob.Zhou@amd.com>

--- drivers/gpu/drm/amd/dkms/config/config.h
+++ drivers/gpu/drm/amd/dkms/config/config.h
@@ -813,6 +813,12 @@
 /* Define to 1 if you have the <linux/apple-gmux.h> header file. */
 #define HAVE_LINUX_APPLE_GMUX_H 1
 
+/* Define to 1 if you have the <linux/args.h> header file. */
+#define HAVE_LINUX_ARGS_H 1
+
+/* atomic_long_try_cmpxchg() is available */
+#define HAVE_LINUX_ATOMIC_LONG_TRY_CMPXCHG 1
+
 /* Define to 1 if you have the <linux/bits.h> header file. */
 #define HAVE_LINUX_BITS_H 1
 
