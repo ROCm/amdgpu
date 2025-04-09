bb42bfb556e0d10da309100894558628e554b0b0
chengjya <chengjya@amd.com>
2025-04-09 12:10:44 +0800

    drm/amdkcl: wrap code under the HAVE_PM_SUSPEND_TARGET_STATE
    It's caused by the commit: fe01a75b042f
    "drm/amd: Forbid suspending into non-default suspend states"
    
    Signed-off-by: chengjya <chengjya@amd.com>
    Reviewed-by: Bob Zhou <Bob.Zhou@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -2661,6 +2661,7 @@ static int amdgpu_pmops_suspend(struct device *dev)
 	else if (amdgpu_acpi_is_s3_active(adev))
 		adev->in_s3 = true;
 	if (!adev->in_s0ix && !adev->in_s3) {
+#ifdef HAVE_PM_SUSPEND_TARGET_STATE
 		/* don't allow going deep first time followed by s2idle the next time */
 		if (adev->last_suspend_state != PM_SUSPEND_ON &&
 		    adev->last_suspend_state != pm_suspend_target_state) {
@@ -2668,11 +2669,14 @@ static int amdgpu_pmops_suspend(struct device *dev)
 				     pm_suspend_target_state);
 			return -EINVAL;
 		}
+#endif
 		return 0;
 	}
 
+#ifdef HAVE_PM_SUSPEND_TARGET_STATE
 	/* cache the state last used for suspend */
 	adev->last_suspend_state = pm_suspend_target_state;
+#endif
 
 	return amdgpu_device_suspend(drm_dev, true);
 }
