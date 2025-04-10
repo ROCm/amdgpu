079765c8e41a9fd10f934b9d0f22366612da3dc0
Chengjun Yao <Chengjun.Yao@amd.com>
2025-04-10 11:18:03 +0800

    drm/amdkcl: fake macro INTEL_ALDERLAKE & INTEL_ROCKETLAKE
    It's caused by the commit:f4f55ab3
    "drm/amd/amdgpu: disable ASPM in some situations"
    
    Signed-off-by: Chengjun Yao <Chengjun.Yao@amd.com>
    Reviewed-by: Bob Zhou <Bob.Zhou@amd.com>

--- include/kcl/kcl_intel_family.h
+++ include/kcl/kcl_intel_family.h
@@ -5,6 +5,7 @@
 #ifdef CONFIG_X86
 
 #include <asm/intel-family.h>
+#include <kcl/kcl_cpu_device_id.h>
 /* Copied froma asm/intel-family.h*/
 #ifndef INTEL_FAM6_ROCKETLAKE
 #define INTEL_FAM6_ROCKETLAKE       0xA7
@@ -12,6 +13,21 @@
 
 #ifndef INTEL_FAM6_ALDERLAKE
 #define INTEL_FAM6_ALDERLAKE		0x97	/* Golden Cove / Gracemont */
+#endif
+
+#ifndef IFM
+#define IFM(_fam, _model)	VFM_MAKE(X86_VENDOR_INTEL, _fam, _model)
+#endif
+
+#ifndef INTEL_ALDERLAKE
+#define INTEL_ALDERLAKE			IFM(6, 0x97) /* Golden Cove / Gracemont */
+#define INTEL_ALDERLAKE_L		IFM(6, 0x9A) /* Golden Cove / Gracemont */
+#endif
+
+#ifndef INTEL_ROCKETLAKE
+#define INTEL_RAPTORLAKE		IFM(6, 0xB7) /* Raptor Cove / Enhanced Gracemont */
+#define INTEL_RAPTORLAKE_P		IFM(6, 0xBA)
+#define INTEL_RAPTORLAKE_S		IFM(6, 0xBF)
 #endif
 
 #endif /* CONFIG_X86 */
