3b85de763fd97bec83a273638864adaf9021557b
Chengjun Yao <Chengjun.Yao@amd.com>
2025-04-23 15:45:02 +0800

    Revert "drm/amd/display: Add error check for avi and vendor infoframe setup function"
    Temporarily reverts commit 8ffd2a2ffb7affbc8d5e82aba1c6e6730b906d9c.
    The reverted patch causes a jira issue SWDEV-528342
    
    Signed-off-by: chengjya <chengjya@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -6622,7 +6622,6 @@ static void fill_stream_properties_from_drm_display_mode(
 	struct amdgpu_dm_connector *aconnector = NULL;
 	struct hdmi_vendor_infoframe hv_frame;
 	struct hdmi_avi_infoframe avi_frame;
-	ssize_t err;
 
 	if (connector->connector_type != DRM_MODE_CONNECTOR_WRITEBACK)
 		aconnector = to_amdgpu_dm_connector(connector);
@@ -6670,22 +6669,14 @@ static void fill_stream_properties_from_drm_display_mode(
 
 	if (stream->signal == SIGNAL_TYPE_HDMI_TYPE_A) {
 #if defined(HAVE_DRM_HDMI_AVI_INFOFRAME_FROM_DISPLAY_MODE_P_P_P)
-		err = drm_hdmi_avi_infoframe_from_display_mode(&avi_frame,
-							       (struct drm_connector *)connector,
-							       mode_in);
-		if (err < 0)
-			drm_err(connector->dev, "Failed to setup avi infoframe: %zd\n", err);
+		drm_hdmi_avi_infoframe_from_display_mode(&avi_frame, (struct drm_connector *)connector, mode_in);
 #elif defined(HAVE_DRM_HDMI_AVI_INFOFRAME_FROM_DISPLAY_MODE_P_P_B)
 		drm_hdmi_avi_infoframe_from_display_mode(&avi_frame, mode_in, false);
 #else
 		drm_hdmi_avi_infoframe_from_display_mode(&avi_frame, mode_in);
 #endif /* HAVE_DRM_HDMI_AVI_INFOFRAME_FROM_DISPLAY_MODE_P_P_P */
 		timing_out->vic = avi_frame.video_code;
-		err = drm_hdmi_vendor_infoframe_from_display_mode(&hv_frame,
-								  (struct drm_connector *)connector,
-								  mode_in);
-		if (err < 0)
-			drm_err(connector->dev, "Failed to setup vendor infoframe: %zd\n", err);
+		drm_hdmi_vendor_infoframe_from_display_mode(&hv_frame, (struct drm_connector *)connector, mode_in);
 		timing_out->hdmi_vic = hv_frame.vic;
 	}
 
