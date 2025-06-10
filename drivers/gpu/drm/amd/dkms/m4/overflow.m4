ROCm/amdgpu

commit f73fa446f6edea101c59af2e6c657a80f002b626

drm/amdkcl: test whether size_mul() exist
It's caused by c72e473642e23d
"drm/amdgpu: Fix integer overflow issues in amdgpu_userq_fence.c"

Signed-off-by: Yang Su <Yang.Su2@amd.com>

drivers/gpu/drm/amd/dkms/m4/overflow.m4
@@ -1,0 +1,16 @@
+dnl #
+dnl # v5.17-rc3-2-ge1be43d9b5d0
+dnl # overflow: Implement size_t saturating arithmetic helpers
+dnl #
+AC_DEFUN([AC_AMDGPU_SIZE_MUL], [
+	AC_KERNEL_DO_BACKGROUND([
+		AC_KERNEL_TRY_COMPILE([
+			#include <linux/overflow.h>
+		], [
+			size_mul(NULL, NULL);
+		], [
+			AC_DEFINE(HAVE_SIZE_MUL, 1,
+				[size_mul() is available])
+		])
+	])
+])

drivers/gpu/drm/amd/backport/backport.h
@@ -149,5 +149,6 @@
 #include <kcl/kcl_cpu_device_id.h>
 #include <kcl/kcl_dma_fence_array.h>
 #include <kcl/kcl_dma_fence_unwrap.h>
+#include <kcl/kcl_overflow.h>
 
 #endif /* AMDGPU_BACKPORT_H */

drivers/gpu/drm/amd/dkms/config/config.h
@@ -1098,6 +1098,9 @@
 /* smca_get_bank_type(x) is available */
 /* #undef HAVE_SMCA_GET_BANK_TYPE_WITH_ONE_ARGUMENT */
 
+/* size_mul() is available */
+#define HAVE_SIZE_MUL 1
+
 /* whether smca_get_bank_type(x, x) is available */
 #define HAVE_SMCA_GET_BANK_TYPE_WITH_TWO_ARGUMENTS 1
 

drivers/gpu/drm/amd/dkms/m4/kernel.m4
@@ -275,6 +275,7 @@ AC_DEFUN([AC_CONFIG_KERNEL], [
 	AC_AMDGPU_DMA_FENCE_DEDUP_ARRAY
 	AC_AMDGPU_DRM_FILE_ERR
 	AC_AMDGPU_DRM_FILE_CLIENT_NAME
+	AC_AMDGPU_SIZE_MUL
 	
 	AC_KERNEL_WAIT
 	AS_IF([test "$LINUX_OBJ" != "$LINUX"], [

include/kcl/kcl_overflow.h
@@ -1,0 +1,9 @@
+#ifndef __KCL_LINUX_OVERFLOW_H
+#define __KCL_LINUX_OVERFLOW_H
+
+#include <linux/overflow.h>
+
+#ifndef HAVE_SIZE_MUL
+#define size_mul array_size
+#endif
+#endif

commit 9d1ea028c1820804dec79bed467df8a51d9a4b57

drm/amdkcl: fix size_mul() unused cause m4 detect fail
size_mul is declared with __attribute__((warn_unused_result)), but it's not being used.
use a size_t variable to store the size_mul() return value.

It's caused by f73fa446f6e
"drm/amdkcl: test whether size_mul() exist"

Signed-off-by: Yang Su <Yang.Su2@amd.com>
Reviewed-by: Bob Zhou <bob.zhou@amd.com>

drivers/gpu/drm/amd/dkms/m4/overflow.m4
@@ -7,7 +7,8 @@ AC_DEFUN([AC_AMDGPU_SIZE_MUL], [
 		AC_KERNEL_TRY_COMPILE([
 			#include <linux/overflow.h>
 		], [
-			size_mul(NULL, NULL);
+			size_t offset;
+			offset = size_mul(0, 0);
 		], [
 			AC_DEFINE(HAVE_SIZE_MUL, 1,
 				[size_mul() is available])