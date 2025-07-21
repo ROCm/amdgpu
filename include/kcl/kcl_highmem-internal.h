6068071524fca4cbd52e24ccab0e53be3c9837c8
Bob Zhou <bob.zhou@amd.com>
2025-07-21 17:39:12 +0800

    drm/amdkcl: test if kmap_local_page_try_from_panic() is available
    It's caused by 718370ff283284f191155a5eb9d4f376aaf93bb8
    "drm/ttm: Add ttm_bo_kmap_try_from_panic()"
    
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>

--- include/kcl/kcl_highmem-internal.h
+++ include/kcl/kcl_highmem-internal.h
@@ -36,6 +36,23 @@ do {                                                            \
 } while (0)
 #endif /* kunmap_local */
 
-
+#ifdef CONFIG_HIGHMEM
+#ifndef HAVE_KMAP_LOCAL_PAGE_TRY_FROM_PANIC
+static inline void *kmap_local_page_try_from_panic(struct page *page)
+{
+	if (!PageHighMem(page))
+		return page_address(page);
+	/* If the page is in HighMem, it's not safe to kmap it.*/
+	return NULL;
+}
+#endif
+#else
+#ifndef HAVE_KMAP_LOCAL_PAGE_TRY_FROM_PANIC
+static inline void *kmap_local_page_try_from_panic(struct page *page)
+{
+	return page_address(page);
+}
+#endif
+#endif
 
 #endif
