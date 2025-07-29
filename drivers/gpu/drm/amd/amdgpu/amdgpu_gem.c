ROCm/ROCK-Kernel-Driver

drm/amdgpu:[hybrid] unpin bo per exit
Signed-off-by: Flora Cui <flora.cui@amd.com>
Reviewed-by: Guchun Chen <guchun.chen@amd.com>

drivers/gpu/drm/amd/amdgpu/amdgpu_gem.c
@@ -51,12 +51,20 @@ static void amdgpu_gem_object_free(struct drm_gem_object *gobj)
 	struct amdgpu_device *adev = amdgpu_ttm_adev(robj->tbo.bdev);
 
 	if (robj) {
+		if (robj->flags & AMDGPU_GEM_CREATE_NO_EVICT) {
+			if (!amdgpu_bo_reserve(robj, false)) {
+				amdgpu_bo_unpin(robj);
+				amdgpu_bo_unreserve(robj);
+			}
+		}
+
 		if (robj->tbo.mem.mem_type == AMDGPU_PL_DGMA)
 			atomic64_sub(amdgpu_bo_size(robj),
 				     &adev->direct_gma.vram_usage);
 		else if (robj->tbo.mem.mem_type == AMDGPU_PL_DGMA_IMPORT)
 			atomic64_sub(amdgpu_bo_size(robj),
 				     &adev->direct_gma.gart_usage);
+
 		amdgpu_mn_unregister(robj);
 		amdgpu_bo_unref(&robj);
 	}

ROCm/amdgpu

drivers/gpu/drm/amd/amdgpu/amdgpu_gem.c
@@ -197,6 +197,13 @@ static void amdgpu_gem_object_free(struct drm_gem_object *gobj)
 {
 	struct amdgpu_bo *aobj = gem_to_amdgpu_bo(gobj);
 
+	if (aobj->flags & AMDGPU_GEM_CREATE_NO_EVICT) {
+		if (!amdgpu_bo_reserve(aobj, false)) {
+			amdgpu_bo_unpin(aobj);
+			amdgpu_bo_unreserve(aobj);
+		}
+	}
+
 	amdgpu_hmm_unregister(aobj);
 	ttm_bo_put(&aobj->tbo);
 }