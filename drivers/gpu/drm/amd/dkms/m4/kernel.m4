torvalds/linux

commit fe75adffac33eeb167b378861d80c805b234495c

ttm: Call shmem_writeout() from ttm_backup_backup_page()
The ->writepage operation is being removed.  Since this function
exclusively deals with shmem folios, we can call shmem_writeout()
to write it.

Signed-off-by: "Matthew Wilcox (Oracle)" <willy@infradead.org>
Link: https://lore.kernel.org/r/20250402150005.2309458-8-willy@infradead.org
Signed-off-by: Christian Brauner <brauner@kernel.org>

drivers/gpu/drm/ttm/ttm_backup.c
@@ -136,13 +136,13 @@ ttm_backup_backup_page(struct ttm_backup *backup, struct page *page,
 			.for_reclaim = 1,
 		};
 		folio_set_reclaim(to_folio);
-		ret = mapping->a_ops->writepage(folio_file_page(to_folio, idx), &wbc);
+		ret = shmem_writeout(to_folio, &wbc);
 		if (!folio_test_writeback(to_folio))
 			folio_clear_reclaim(to_folio);
 		/*
-		 * If writepage succeeds, it unlocks the folio.
-		 * writepage() errors are otherwise dropped, since writepage()
-		 * is only best effort here.
+		 * If writeout succeeds, it unlocks the folio.	errors
+		 * are otherwise dropped, since writeout is only best
+		 * effort here.
 		 */
 		if (ret)
 			folio_unlock(to_folio);

ROCm/amdgpu

commit be43991914e91cdef72550040fee34c83236802b

amd/amdkcl: test shmem_writeout() is available
It's caused by fe75adffac33eeb167b378861d80c805b234495c
"ttm: Call shmem_writeout() from ttm_backup_backup_page()"

Signed-off-by: Bob Zhou <bob.zhou@amd.com>

drivers/gpu/drm/amd/dkms/m4/kernel.m4
@@ -283,6 +283,7 @@ AC_DEFUN([AC_CONFIG_KERNEL], [
 	AC_AMDGPU_RATELIMIT_STATE_RESET_INTERVAL
 	AC_AMDGPU_TIMER_DELETE
 	AC_AMDGPU_IRQ_DOMAIN_CREATE_LINEAR
+	AC_AMDGPU_SHMEM_WRITEOUT
 	
 	AC_KERNEL_WAIT
 	AS_IF([test "$LINUX_OBJ" != "$LINUX"], [

drivers/gpu/drm/amd/dkms/m4/shmem_writeout.m4
@@ -1,0 +1,16 @@
+dnl #
+dnl # commit v6.15-rc1-5-g7b73c12c6ebf
+dnl # shmem: Add shmem_writeout()
+dnl #
+AC_DEFUN([AC_AMDGPU_SHMEM_WRITEOUT], [
+	AC_KERNEL_DO_BACKGROUND([
+		AC_KERNEL_TRY_COMPILE_SYMBOL([
+			#include <linux/shmem_fs.h>
+		], [
+			shmem_writeout(NULL, NULL);
+		],[shmem_writeout], [mm/shmem.c],[
+			AC_DEFINE(HAVE_SHMEM_WRITEOUT, 1,
+				[shmem_writeout() is available])
+		])
+	])
+])

drivers/gpu/drm/ttm/ttm_backup.c
@@ -122,7 +122,11 @@ ttm_backup_backup_page(struct file *backup, struct page *page,
 			.for_reclaim = 1,
 		};
 		folio_set_reclaim(to_folio);
+#ifdef HAVE_SHMEM_WRITEOUT
 		ret = shmem_writeout(to_folio, &wbc);
+#else
+		ret = mapping->a_ops->writepage(folio_file_page(to_folio, idx), &wbc);
+#endif
 		if (!folio_test_writeback(to_folio))
 			folio_clear_reclaim(to_folio);
 		/*