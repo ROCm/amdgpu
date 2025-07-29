ROCm/ROCK-Kernel-Driver

drm/amdkcl: fake drm_helper_mode_fill_fb_struct()
drm_helper_mode_fill_fb_struct() prototype change in commit
v4.9-rc8-1643-ga3f913ca9892

Signed-off-by: Flora Cui <flora.cui@amd.com>
Reviewed-by: Yang Xiong <Yang.Xiong@amd.com>

drivers/gpu/drm/amd/dkms/m4/drm_helper_mode_fill_fb_struct.m4
@@ -1,0 +1,17 @@
+dnl #
+dnl # v4.9-rc8-1647-g95bce7601581 drm: Populate fb->dev from drm_helper_mode_fill_fb_struct()
+dnl # v4.9-rc8-1643-ga3f913ca9892 drm: Pass 'dev' to drm_helper_mode_fill_fb_struct()
+dnl #
+AC_DEFUN([AC_AMDGPU_DRM_HELPER_MODE_FILL_FB_STRUCT], [
+	AC_KERNEL_DO_BACKGROUND([
+		AC_KERNEL_TRY_COMPILE([
+			#include <linux/stddef.h>
+			#include <drm/drm_modeset_helper.h>
+		], [
+			drm_helper_mode_fill_fb_struct(NULL, NULL, NULL);
+		], [
+			AC_DEFINE(HAVE_DRM_HELPER_MODE_FILL_FB_STRUCT_DEV, 1,
+				[drm_helper_mode_fill_fb_struct() wants dev arg])
+		])
+	])
+])

ROCm/amdgpu

drivers/gpu/drm/amd/dkms/m4/drm_helper_mode_fill_fb_struct.m4
@@ -1,0 +1,16 @@
+dnl #
+dnl # v4.9-rc8-1647-g95bce7601581 drm: Populate fb->dev from drm_helper_mode_fill_fb_struct()
+dnl # v4.9-rc8-1643-ga3f913ca9892 drm: Pass 'dev' to drm_helper_mode_fill_fb_struct()
+dnl #
+AC_DEFUN([AC_AMDGPU_DRM_HELPER_MODE_FILL_FB_STRUCT], [
+	AC_KERNEL_DO_BACKGROUND([
+		AC_KERNEL_TRY_COMPILE([
+			#include <drm/drm_crtc_helper.h>
+		], [
+			drm_helper_mode_fill_fb_struct(NULL, NULL, NULL);
+		], [
+			AC_DEFINE(HAVE_DRM_HELPER_MODE_FILL_FB_STRUCT_DEV, 1,
+				[drm_helper_mode_fill_fb_struct() wants dev arg])
+		])
+	])
+])