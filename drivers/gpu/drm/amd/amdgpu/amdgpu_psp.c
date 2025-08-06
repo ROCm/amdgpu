104e4db964f40ecac0a52b7f77a7ce0c91c8622a
Bob Zhou <bob.zhou@amd.com>
2025-08-06 13:27:04 +0800

    drm/amdkcl: wrap code under HAVE_ATTRIBUTE_GROUP_BIN_ATTRS_NEW
    It's caused by 2d0f5001b61c4831d413d12c10caed0e99d73b25
    "drm/amdgpu: Constify 'struct bin_attribute'"
    
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_psp.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_psp.c
@@ -4294,7 +4294,11 @@ rel_buf:
  * Writing to this file will stage an IFWI for update. Reading from this file
  * will trigger the update process.
  */
+#ifdef HAVE_ATTRIBUTE_GROUP_BIN_ATTRS_NEW
 static const struct bin_attribute psp_vbflash_bin_attr = {
+#else
+static struct bin_attribute psp_vbflash_bin_attr = {
+#endif
 	.attr = {.name = "psp_vbflash", .mode = 0660},
 	.size = 0,
 	.write = amdgpu_psp_vbflash_write,
