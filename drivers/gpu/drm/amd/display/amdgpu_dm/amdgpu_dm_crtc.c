bdbc6d5986c791978afb7811b8a1cfdff8be4587
Yang Su <Yang.Su2@amd.com>
2025-08-20 17:36:45 +0800

    drm/amdkcl: wrap code under HAVE_DRM_VBLANK_CRTC_STRUCT_CONFIG
    in the old kernel version, struct drm_vblank_crtc doesn't have
    member "config". so bypass in the code.
    it's cuased by 142fc1c2ce2684
    "drm/amd/display: Fix Xorg desktop unresponsive on Replay panel"
    
    Signed-off-by: Yang Su <Yang.Su2@amd.com>
    Reviewed-by: Bob Zhou <bob.zhou@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_crtc.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_crtc.c
@@ -299,6 +299,7 @@ static inline int amdgpu_dm_crtc_set_vblank(struct drm_crtc *crtc, bool enable)
 	irq_type = amdgpu_display_crtc_idx_to_irq_type(adev, acrtc->crtc_id);
 
 	if (enable) {
+#ifdef HAVE_DRM_VBLANK_CRTC_STRUCT_CONFIG
 		struct dc *dc = adev->dm.dc;
 		struct drm_vblank_crtc *vblank = drm_crtc_vblank_crtc(crtc);
 		struct psr_settings *psr = &acrtc_state->stream->link->psr_settings;
@@ -317,7 +318,7 @@ static inline int amdgpu_dm_crtc_set_vblank(struct drm_crtc *crtc, bool enable)
 			dc->config.disable_ips != DMUB_IPS_DISABLE_ALL &&
 			sr_supported && vblank->config.disable_immediate)
 			drm_crtc_vblank_restore(crtc);
-
+#endif
 		/* vblank irq on -> Only need vupdate irq in vrr mode */
 		if (amdgpu_dm_crtc_vrr_active(acrtc_state))
 			rc = amdgpu_dm_crtc_set_vupdate_irq(crtc, true);
--- drivers/gpu/drm/amd/dkms/config/config.h
+++ drivers/gpu/drm/amd/dkms/config/config.h
@@ -720,6 +720,9 @@
 /* drm_vblank_crtc_config is available */
 #define HAVE_DRM_VBLANK_CRTC_CONFIG 1
 
+/* struct vblank->config is available */
+#define HAVE_DRM_VBLANK_CRTC_STRUCT_CONFIG 1
+
 /* struct drm_vma_offset_node has readonly field */
 /* #undef HAVE_DRM_VMA_OFFSET_NODE_READONLY_FIELD */
 
