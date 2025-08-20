79bae3af8766cdb05f1ef00e543f3621eaded4da
Srinivasan Shanmugam <srinivasan.shanmugam@amd.com>
2025-08-20 12:33:29 +0530

    drm/amdgpu/uapi: Introduce AMDGPU_GEM_DOMAIN_MMIO_REMAP
    Add a new GEM domain bit AMDGPU_GEM_DOMAIN_MMIO_REMAP to allow
    userspace to request the MMIO remap (HDP flush) page via GEM_CREATE.
    
    - include/uapi/drm/amdgpu_drm.h:
      * define AMDGPU_GEM_DOMAIN_MMIO_REMAP
      * include the bit in AMDGPU_GEM_DOMAIN_MASK
    
    v2: Add early reject in amdgpu_gem_create_ioctl() (Alex).
    
    Cc: Christian König <christian.koenig@amd.com>
    Suggested-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Srinivasan Shanmugam <srinivasan.shanmugam@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

--- include/uapi/drm/amdgpu_drm.h
+++ include/uapi/drm/amdgpu_drm.h
@@ -112,6 +112,8 @@ extern "C" {
  *
  * %AMDGPU_GEM_DOMAIN_DOORBELL	Doorbell. It is an MMIO region for
  * signalling user mode queues.
+ *
+ * %AMDGPU_GEM_DOMAIN_MMIO_REMAP	MMIO remap page (special mapping for HDP flushing).
  */
 /* hybrid specific ioctls */
 #define DRM_IOCTL_AMDGPU_SEM		DRM_IOWR(DRM_COMMAND_BASE + DRM_AMDGPU_SEM, union drm_amdgpu_sem)
@@ -123,8 +125,10 @@ extern "C" {
 #define AMDGPU_GEM_DOMAIN_GWS		0x10
 #define AMDGPU_GEM_DOMAIN_OA		0x20
 #define AMDGPU_GEM_DOMAIN_DOORBELL	0x40
+#define AMDGPU_GEM_DOMAIN_MMIO_REMAP	0x80
 #define AMDGPU_GEM_DOMAIN_DGMA		0x400
 #define AMDGPU_GEM_DOMAIN_DGMA_IMPORT	0x800
+
 #define AMDGPU_GEM_DOMAIN_MASK		(AMDGPU_GEM_DOMAIN_CPU | \
 					 AMDGPU_GEM_DOMAIN_GTT | \
 					 AMDGPU_GEM_DOMAIN_VRAM | \
@@ -132,6 +136,7 @@ extern "C" {
 					 AMDGPU_GEM_DOMAIN_GWS | \
 					 AMDGPU_GEM_DOMAIN_OA |\
 					 AMDGPU_GEM_DOMAIN_DOORBELL |\
+					 AMDGPU_GEM_DOMAIN_MMIO_REMAP |\
 					 AMDGPU_GEM_DOMAIN_DGMA |\
 					 AMDGPU_GEM_DOMAIN_DGMA_IMPORT)
 
