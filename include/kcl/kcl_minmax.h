1cafc0e9aa13b0c274485637e346cfe45ead1f7b
Yang Su <Yang.Su2@amd.com>
2025-08-21 11:13:31 +0800

    drm/amdkcl: test whether MIN() exist
    it's caused by 3a02acd7ed124c9
    "drm/amd/display: Adjust DCE 8-10 clock, don't overclock by 15%"
    Signed-off-by: Yang Su <Yang.Su2@amd.com>

--- include/kcl/kcl_minmax.h
+++ include/kcl/kcl_minmax.h
@@ -9,4 +9,12 @@
 	min((x) + 0u + 0ul + 0ull, (y) + 0u + 0ul + 0ull)
 #endif
 
+#ifndef MIN
+#define __cmp_op_min <
+
+#define __cmp(op, x, y)	((x) __cmp_op_##op (y) ? (x) : (y))
+
+#define MIN(a, b) __cmp(min, a, b)
+#endif
+
 #endif /* _KCL_MINMAX_H */
