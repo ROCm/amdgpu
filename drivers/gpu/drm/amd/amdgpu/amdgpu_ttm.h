7ab36c0d4d12eb80b821f53590601abbeab756d4
David Yat Sin <David.YatSin@amd.com>
2025-09-03 20:03:52 -0400

    amdgpu/amdkfd: peerdirect support address lookup
    Add look-up for the BO by address. This is required for memory
    allocations where the virtual address are reserved outside of
    KFD and then later mapped to GPU memory.
    
    Signed-off-by: David Yat Sin <David.YatSin@amd.com>
    Reviewed-by: Felix Kuehling <felix.kuehling@amd.com>'

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.h
@@ -186,6 +186,8 @@ int amdgpu_ttm_init(struct amdgpu_device *adev);
 void amdgpu_ttm_fini(struct amdgpu_device *adev);
 void amdgpu_ttm_set_buffer_funcs_status(struct amdgpu_device *adev,
 					bool enable);
+bool amdgpu_vma_is_amdgpu_bo(struct vm_area_struct *vma);
+
 int amdgpu_copy_buffer(struct amdgpu_device *adev,
 		       struct amdgpu_ttm_buffer_entity *entity,
 		       uint64_t src_offset,
