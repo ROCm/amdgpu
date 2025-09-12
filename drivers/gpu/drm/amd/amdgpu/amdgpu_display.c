2c728ae89ef054a9f8f8cc418d0eeb75666faf50
Chengjun Yao <Chengjun.Yao@amd.com>
2025-09-12 17:09:43 +0800

    drm/amdkcl: test drm_helper_mode_fill_fb_struct() wants 4 arguments
    Signed-off-by: Chengjun Yao <Chengjun.Yao@amd.com>
    Reviewed-by: Bob Zhou <bob.zhou@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_display.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_display.c
@@ -1209,7 +1209,12 @@ int amdgpu_display_gem_fb_init(struct drm_device *dev,
 {
 	int ret;
 	rfb->base.obj[0] = obj;
+
+#ifdef HAVE_DRM_HELPER_MODE_FILL_FB_STRUCT_4_ARGS
+	drm_helper_mode_fill_fb_struct(dev, &rfb->base, NULL, mode_cmd);
+#else
 	drm_helper_mode_fill_fb_struct(dev, &rfb->base, mode_cmd);
+#endif
 
 	ret = amdgpu_display_framebuffer_init(dev, rfb, mode_cmd, obj);
 	if (ret)
@@ -1236,7 +1241,14 @@ static int amdgpu_display_gem_fb_verify_and_init(struct drm_device *dev,
 	int ret;
 
 	rfb->base.obj[0] = obj;
+
+#ifdef HAVE_DRM_HELPER_MODE_FILL_FB_STRUCT_4_ARGS
 	drm_helper_mode_fill_fb_struct(dev, &rfb->base, info, mode_cmd);
+#else
+	drm_helper_mode_fill_fb_struct(dev, &rfb->base, mode_cmd);
+#endif
+
+
 #ifdef HAVE_DRM_FORMAT_INFO_MODIFIER_SUPPORTED
 	/* Verify that the modifier is supported. */
 	if (!drm_any_plane_has_format(dev, mode_cmd->pixel_format,
