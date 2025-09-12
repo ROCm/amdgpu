24ce4218221411e476891f6df0f9dcfd122851ec
Chengjun Yao <Chengjun.Yao@amd.com>
2025-09-12 17:32:56 +0800

    drm/amdkcl: test whether fb_create has const struct drm_format_info * parameter
    Signed-off-by: Chengjun Yao <Chengjun.Yao@amd.com>
    Reviewed-by: Bob Zhou <bob.zhou@amd.com>

--- drivers/gpu/drm/amd/dkms/config/config.h
+++ drivers/gpu/drm/amd/dkms/config/config.h
@@ -561,8 +561,8 @@
 /* drm_edid_valid() is available */
 #define HAVE_DRM_EDID_VALID 1
 
-/* drm_exec() has 3 arguments */
-#define HAVE_DRM_EXEC_INIT_3_ARGUMENTS 1
+/* fb_create has const struct drm_format_info * parameter */
+#define HAVE_DRM_FB_CREATE_FORMAT_INFO 1
 
 /* drm_fb_helper_fill_info() is available */
 #define HAVE_DRM_FB_HELPER_FILL_INFO 1
