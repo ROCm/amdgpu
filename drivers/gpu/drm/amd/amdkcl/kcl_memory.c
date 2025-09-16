3c480ead5a4889c459f02cd3e21deaa1297b8b41
Chengjun Yao <Chengjun.Yao@amd.com>
2025-09-16 14:47:27 +0800

    drm/amdkcl: remove unnecessary vm_insert_mixed & vmf_insert_mixed_prot() check
    Signed-off-by: Chengjun Yao <Chengjun.Yao@amd.com>
    Reviewed-by: Bob Zhou <bob.zhou@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_memory.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_memory.c
@@ -31,20 +31,6 @@
 #include <kcl/kcl_mm.h>
 #include <linux/vmalloc.h>
 
-/* Copied from drivers/gpu/drm/ttm/ttm_bo_vm.c and modified for KCL */
-#ifndef HAVE_VMF_INSERT_MIXED_PROT
-vm_fault_t _kcl_vmf_insert_mixed_prot(struct vm_area_struct *vma, unsigned long addr,
-			pfn_t pfn, pgprot_t pgprot)
-{
-	struct vm_area_struct cvma = *vma;
-
-	cvma.vm_page_prot = pgprot;
-
-	return vmf_insert_mixed(&cvma, addr, pfn);
-}
-EXPORT_SYMBOL(_kcl_vmf_insert_mixed_prot);
-#endif
-
 #ifndef HAVE_VMF_INSERT_PFN_PROT
 #ifndef HAVE_VM_INSERT_PFN_PROT
 int vm_insert_pfn_prot(struct vm_area_struct *vma, unsigned long addr,
