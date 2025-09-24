18a1dd1cbc90ed085d9c92da4d9863cd20381558
Chengjun Yao <Chengjun.Yao@amd.com>
2025-09-24 13:36:49 +0800

    drm/amdkcl：Add kcl_ prefix to fake drm_suballoc_* functions for avoid symbol conflict
    KCL's fake drm_suballoc_* implementations may conflict with upstream DRM symbols,causing module loading issues
    
    Signed-off-by: Chengjun Yao <Chengjun.Yao@amd.com>
    Reviewed-by: Bob Zhou <Bob.Zhou@amd.com>

--- include/kcl/kcl_drm_suballoc.h
+++ include/kcl/kcl_drm_suballoc.h
@@ -52,16 +52,21 @@ struct drm_suballoc {
 	struct dma_fence *fence;
 };
 
-void drm_suballoc_manager_init(struct drm_suballoc_manager *sa_manager,
+#ifndef HAVE_DRM_SUBALLOC_MANAGER_INIT
+void kcl_drm_suballoc_manager_init(struct drm_suballoc_manager *sa_manager,
 			       size_t size, size_t align);
+#define drm_suballoc_manager_init kcl_drm_suballoc_manager_init
 
-void drm_suballoc_manager_fini(struct drm_suballoc_manager *sa_manager);
+void kcl_drm_suballoc_manager_fini(struct drm_suballoc_manager *sa_manager);
+#define drm_suballoc_manager_fini kcl_drm_suballoc_manager_fini
 
 struct drm_suballoc *
-drm_suballoc_new(struct drm_suballoc_manager *sa_manager, size_t size,
+kcl_drm_suballoc_new(struct drm_suballoc_manager *sa_manager, size_t size,
 		 gfp_t gfp, bool intr, size_t align);
+#define drm_suballoc_new kcl_drm_suballoc_new
 
-void drm_suballoc_free(struct drm_suballoc *sa, struct dma_fence *fence);
+void kcl_drm_suballoc_free(struct drm_suballoc *sa, struct dma_fence *fence);
+#define drm_suballoc_free kcl_drm_suballoc_free
 
 /**
  * drm_suballoc_soffset - Range start.
@@ -69,10 +74,11 @@ void drm_suballoc_free(struct drm_suballoc *sa, struct dma_fence *fence);
  *
  * Return: The start of the allocated range.
  */
-static inline size_t drm_suballoc_soffset(struct drm_suballoc *sa)
+static inline size_t kcl_drm_suballoc_soffset(struct drm_suballoc *sa)
 {
 	return sa->soffset;
 }
+#define drm_suballoc_soffset kcl_drm_suballoc_soffset
 
 /**
  * drm_suballoc_eoffset - Range end.
@@ -80,10 +86,11 @@ static inline size_t drm_suballoc_soffset(struct drm_suballoc *sa)
  *
  * Return: The end of the allocated range + 1.
  */
-static inline size_t drm_suballoc_eoffset(struct drm_suballoc *sa)
+static inline size_t kcl_drm_suballoc_eoffset(struct drm_suballoc *sa)
 {
 	return sa->eoffset;
 }
+#define drm_suballoc_eoffset kcl_drm_suballoc_eoffset
 
 /**
  * drm_suballoc_size - Range size.
@@ -91,23 +98,27 @@ static inline size_t drm_suballoc_eoffset(struct drm_suballoc *sa)
  *
  * Return: The size of the allocated range.
  */
-static inline size_t drm_suballoc_size(struct drm_suballoc *sa)
+static inline size_t kcl_drm_suballoc_size(struct drm_suballoc *sa)
 {
 	return sa->eoffset - sa->soffset;
 }
+#define drm_suballoc_size kcl_drm_suballoc_size
 
 #ifdef CONFIG_DEBUG_FS
-void drm_suballoc_dump_debug_info(struct drm_suballoc_manager *sa_manager,
+void kcl_drm_suballoc_dump_debug_info(struct drm_suballoc_manager *sa_manager,
 				  struct drm_printer *p,
 				  unsigned long long suballoc_base);
 #else
 static inline void
-drm_suballoc_dump_debug_info(struct drm_suballoc_manager *sa_manager,
+kcl_drm_suballoc_dump_debug_info(struct drm_suballoc_manager *sa_manager,
 			     struct drm_printer *p,
 			     unsigned long long suballoc_base)
 { }
 
 #endif
+#define drm_suballoc_dump_debug_info kcl_drm_suballoc_dump_debug_info
+#endif /* HAVE_DRM_SUBALLOC_MANAGER_INIT */
+
 #endif /*HAVE_DRM_DRM_SUBALLOC_H*/
 
 #endif /* _KCL_DRM_SUBALLOC_H_ */
