b0f1942810a0db935f4b26d31e0bd6726b58ea3d
Timur Kristóf <timur.kristof@gmail.com>
2025-09-26 20:02:02 +0200

    drm/amd/display: Don't add freesync modes to analog displays (v2)
    VRR is not supported on analog signals.
    Don't add freesync modes to analog displays or when
    VRR is unsupported by DC.
    
    Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -8913,6 +8913,10 @@ static void amdgpu_dm_connector_add_freesync_modes(struct drm_connector *connect
 #endif
 		return;
 
+	if (!amdgpu_dm_connector->dc_sink || amdgpu_dm_connector->dc_sink->edid_caps.analog ||
+		!dc_supports_vrr(amdgpu_dm_connector->dc_sink->ctx->dce_version))
+		return;
+
 	if (amdgpu_dm_connector->max_vfreq - amdgpu_dm_connector->min_vfreq > 10)
 		amdgpu_dm_connector->num_modes +=
 			add_fs_modes(amdgpu_dm_connector);
