908c797573d6e04b2680edb49a71794cbe40da0c
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
2025-11-04 10:42:45 +0100

    drm/amdgpu: jump to the correct label on failure
    drm_sched_entity_init wasn't called yet, so the only thing to
    do is to release allocated memory.
    This doesn't fix any bug since entity is zero allocated and
    drm_sched_entity_fini does nothing in this case.
    
    Signed-off-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
    Acked-by: Christian König <christian.koenig@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ctx.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ctx.c
@@ -238,7 +238,7 @@ static int amdgpu_ctx_init_entity(struct amdgpu_ctx *ctx, u32 hw_ip,
 		r = amdgpu_xcp_select_scheds(adev, hw_ip, hw_prio, fpriv,
 						&num_scheds, &scheds);
 		if (r)
-			goto cleanup_entity;
+			goto error_free_entity;
 	}
 
 	/* disable load balance if the hw engine retains context among dependent jobs */
