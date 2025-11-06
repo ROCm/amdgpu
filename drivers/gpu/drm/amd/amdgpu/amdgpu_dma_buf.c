17f1f16c53c514a4aaad6090ed79668e1f6e28f3
Vitaly Prosyak <vitaly.prosyak@amd.com>
2025-11-06 10:03:20 -0500

    drm/amdgpu: disable peer-to-peer access for DCC-enabled GC12 VRAM surfaces
    Certain multi-GPU configurations (especially GFX12) may hit
    data corruption when a DCC-compressed VRAM surface is shared across GPUs
    using peer-to-peer (P2P) DMA transfers.
    
    Such surfaces rely on device-local metadata and cannot be safely accessed
    through a remote GPU’s page tables. Attempting to import a DCC-enabled
    surface through P2P leads to incorrect rendering or GPU faults.
    
    This change disables P2P for DCC-enabled VRAM buffers that are contiguous
    and allocated on GFX12+ hardware.  In these cases, the importer falls back
    to the standard system-memory path, avoiding invalid access to compressed
    surfaces.
    
    Future work could consider optional migration (VRAM→System→VRAM) if a
    performance regression is observed when `attach->peer2peer = false`.
    
    Tested on:
     - Dual RX 9700 XT (Navi4x) setup
     - GNOME and Wayland compositor scenarios
     - Confirmed no corruption after disabling P2P under these conditions
    
    Suggested-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Vitaly Prosyak <vitaly.prosyak@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Change-Id: Ia0dd9eb39ab7a8c2226717307f6cc81083ce8773

--- drivers/gpu/drm/amd/amdgpu/amdgpu_dma_buf.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_dma_buf.c
@@ -260,11 +260,24 @@ static int amdgpu_dma_buf_attach(struct dma_buf *dmabuf,
 	struct amdgpu_device *adev = amdgpu_ttm_adev(bo->tbo.bdev);
 
 #ifdef HAVE_STRUCT_DMA_BUF_ATTACH_OPS_ALLOW_PEER2PEER
+	/*
+	 * Disable peer-to-peer access for DCC-enabled VRAM surfaces on GFX12+.
+	 * Such buffers cannot be safely accessed over P2P due to device-local
+	 * compression metadata. Fallback to system-memory path instead.
+	 * Device supports GFX12 (GC 12.x or newer)
+	 * BO was created with the AMDGPU_GEM_CREATE_GFX12_DCC flag
+	 *
+	 */
+	if ((adev->ip_versions[GC_HWIP][0] >= IP_VERSION(12, 0, 0)) &&
+		bo->flags & AMDGPU_GEM_CREATE_GFX12_DCC) {
+		attach->peer2peer = false;
+		goto update_vm;
+	}
 	if (!amdgpu_dmabuf_is_xgmi_accessible(attach_adev, bo) &&
 	    pci_p2pdma_distance(adev->pdev, attach->dev, false) < 0)
 		attach->peer2peer = false;
 #endif
-
+update_vm:
 	amdgpu_vm_bo_update_shared(bo);
 
 	return 0;
