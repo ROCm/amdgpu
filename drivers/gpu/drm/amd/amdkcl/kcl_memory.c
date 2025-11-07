302f82ffc2a868e02cdc07e2ad201ac6b17691e3
Bob Zhou <bob.zhou@amd.com>
2025-11-07 11:10:10 +0800

    drm/amdkcl: cleanup vmf_insert_*()
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>

--- drivers/gpu/drm/amd/amdkcl/kcl_memory.c
+++ drivers/gpu/drm/amd/amdkcl/kcl_memory.c
@@ -31,34 +31,6 @@
 #include <kcl/kcl_mm.h>
 #include <linux/vmalloc.h>
 
-#ifndef HAVE_VMF_INSERT_PFN_PROT
-#ifndef HAVE_VM_INSERT_PFN_PROT
-int vm_insert_pfn_prot(struct vm_area_struct *vma, unsigned long addr,
-                        unsigned long pfn, pgprot_t pgprot)
-{
-	struct vm_area_struct cvma = *vma;
-
-	cvma.vm_page_prot = pgprot;
-
-	return vm_insert_pfn(&cvma, addr, pfn);
-}
-#endif
-
-vm_fault_t _kcl_vmf_insert_pfn_prot(struct vm_area_struct *vma, unsigned long addr,
-		unsigned long pfn, pgprot_t pgprot)
-{
-	int err = vm_insert_pfn_prot(vma, addr, pfn, pgprot);
-
-	if (err == -ENOMEM)
-		return VM_FAULT_OOM;
-	if (err < 0 && err != -EBUSY)
-		return VM_FAULT_SIGBUS;
-
-	return VM_FAULT_NOPAGE;
-}
-EXPORT_SYMBOL(_kcl_vmf_insert_pfn_prot);
-#endif
-
 /* Copied from mm/util.c and modified for KCL */
 #ifdef HAVE_NO_KVREALLOC
 void *kvrealloc(const void *p, size_t oldsize, size_t newsize, gfp_t flags)
