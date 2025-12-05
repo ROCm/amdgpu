bbb0e16e9d61003e37198ab489612de966bdcc71
Chengjun Yao <Chengjun.Yao@amd.com>
2025-12-05 16:48:21 +0800

    drm/amdkcl: kcl-cleanup HAVE_BACKLIGHT_DEVICE_SET_BRIGHTNESS
    Signed-off-by: Chengjun Yao <Chengjun.Yao@amd.com>
    Reviewed-by: Bob Zhou <Bob.Zhou@amd.com>

--- include/kcl/kcl_backlight.h
+++ include/kcl/kcl_backlight.h
@@ -9,10 +9,6 @@
 #define AMDKCL_BACKLIGHT_H
 
 #include <linux/backlight.h>
-#ifndef HAVE_BACKLIGHT_DEVICE_SET_BRIGHTNESS
-int backlight_device_set_brightness(struct backlight_device *bd,
-				    unsigned long brightness);
-#endif /* HAVE_BACKLIGHT_DEVICE_SET_BRIGHTNESS */
 
 #ifndef BACKLIGHT_POWER_ON
 #define BACKLIGHT_POWER_ON              (0)
