e1e685eac76d9fca62e01470ef0c2351ca03c4a0
Victor Zhao <Victor.Zhao@amd.com>
2025-12-18 18:41:52 +0800

    drm/amdgpu: always get PTL state from PSP under sriov
    In SR-IOV mode, always query the latest PTL status from host via
    psp_performance_monitor_hw() to ensure the sysfs shows real-time
    hardware state.
    
    Signed-off-by: Victor Zhao <Victor.Zhao@amd.com>
    Reviewed-by: Lijo Lazar <lijo.lazar@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_psp.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_psp.c
@@ -1383,6 +1383,15 @@ static ssize_t ptl_enable_show(struct device *dev, struct device_attribute *attr
 	struct drm_device *ddev = dev_get_drvdata(dev);
 	struct amdgpu_device *adev = drm_to_adev(ddev);
 	struct psp_context *psp = &adev->psp;
+	uint32_t ptl_state, fmt1, fmt2;
+	int ret;
+
+	if (amdgpu_sriov_vf(adev)) {
+		ret = psp_performance_monitor_hw(psp, PSP_PTL_PERF_MON_QUERY,
+						 &ptl_state, &fmt1, &fmt2);
+		if (ret)
+			return ret;
+	}
 
 	return sysfs_emit(buf, "%s\n", psp->ptl_enabled ? "enabled" : "disabled");
 }
@@ -1439,6 +1448,15 @@ static ssize_t ptl_format_show(struct device *dev, struct device_attribute *attr
 	struct drm_device *ddev = dev_get_drvdata(dev);
 	struct amdgpu_device *adev = drm_to_adev(ddev);
 	struct psp_context *psp = &adev->psp;
+	uint32_t ptl_state, fmt1, fmt2;
+	int ret;
+
+	if (amdgpu_sriov_vf(adev)) {
+		ret = psp_performance_monitor_hw(psp, PSP_PTL_PERF_MON_QUERY,
+						 &ptl_state, &fmt1, &fmt2);
+		if (ret)
+			return ret;
+	}
 
 	return sysfs_emit(buf, "%s,%s\n",
 			amdgpu_ptl_fmt_str[psp->ptl_fmt1],
