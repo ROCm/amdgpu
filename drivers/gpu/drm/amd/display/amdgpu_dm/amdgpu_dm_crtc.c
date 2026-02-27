torvalds/linux

commit 18a4127e9315645d30c6b93a820efa0cba299c20

drm/amd/display: Disable CRTC degamma when color pipeline is enabled
The degamma is to be handled by Color pipeline API.

Reviewed-by: Harry Wentland <harry.wentland@amd.com>
Signed-off-by: Alex Hung <alex.hung@amd.com>
Signed-off-by: Simon Ser <contact@emersion.fr>
Link: https://patch.msgid.link/20251115000237.3561250-50-alex.hung@amd.com

drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_crtc.c
@@ -736,7 +736,7 @@ int amdgpu_dm_crtc_init(struct amdgpu_display_manager *dm,
 {
 	struct amdgpu_crtc *acrtc = NULL;
 	struct drm_plane *cursor_plane;
-	bool is_dcn;
+	bool has_degamma;
 	int res = -ENOMEM;
 
 	cursor_plane = kzalloc(sizeof(*cursor_plane), GFP_KERNEL);
@@ -775,20 +775,18 @@ int amdgpu_dm_crtc_init(struct amdgpu_display_manager *dm,
 
 	dm->adev->mode_info.crtcs[crtc_index] = acrtc;
 
-	/* Don't enable DRM CRTC degamma property for DCE since it doesn't
-	 * support programmable degamma anywhere.
+	/* Don't enable DRM CRTC degamma property for
+	 * 1. Degamma is replaced by color pipeline.
+	 * 2. DCE since it doesn't support programmable degamma anywhere.
+	 * 3. DCN401 since pre-blending degamma LUT doesn't apply to cursor.
 	 */
-	is_dcn = dm->adev->dm.dc->caps.color.dpp.dcn_arch;
-	/* Dont't enable DRM CRTC degamma property for DCN401 since the
-	 * pre-blending degamma LUT doesn't apply to cursor, and therefore
-	 * can't work similar to a post-blending degamma LUT as in other hw
-	 * versions.
-	 * TODO: revisit it once KMS plane color API is merged.
-	 */
-	drm_crtc_enable_color_mgmt(&acrtc->base,
-				   (is_dcn &&
-				    dm->adev->dm.dc->ctx->dce_version != DCN_VERSION_4_01) ?
-				     MAX_COLOR_LUT_ENTRIES : 0,
+	if (plane->color_pipeline_property)
+		has_degamma = false;
+	else
+		has_degamma = dm->adev->dm.dc->caps.color.dpp.dcn_arch &&
+			      dm->adev->dm.dc->ctx->dce_version != DCN_VERSION_4_01;
+
+	drm_crtc_enable_color_mgmt(&acrtc->base, has_degamma ? MAX_COLOR_LUT_ENTRIES : 0,
 				   true, MAX_COLOR_LUT_ENTRIES);
 
 	drm_mode_crtc_set_gamma_size(&acrtc->base, MAX_COLOR_LEGACY_LUT_ENTRIES);

ROCm/amdgpu

commit ca8aa75900462b384b78be0f2c4b9220efacdfe5

drm/amdkcl: test if drm_plane->color_pipeline_property is available
It's caused by 18a4127e9315645d30c6b93a820efa0cba299c20
"drm/amd/display: Disable CRTC degamma when color pipeline is enabled"

Signed-off-by: Bob Zhou <bob.zhou@amd.com>

drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_crtc.c
@@ -789,9 +789,11 @@ int amdgpu_dm_crtc_init(struct amdgpu_display_manager *dm,
 	 * 2. DCE since it doesn't support programmable degamma anywhere.
 	 * 3. DCN401 since pre-blending degamma LUT doesn't apply to cursor.
 	 */
+#ifdef HAVE_DRM_PLANE_COLOR_PIPELINE_PROPERTY
 	if (plane->color_pipeline_property)
 		has_degamma = false;
 	else
+#endif
 		has_degamma = dm->adev->dm.dc->caps.color.dpp.dcn_arch &&
 			      dm->adev->dm.dc->ctx->dce_version != DCN_VERSION_4_01;
 

drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_plane.c
@@ -2008,9 +2008,11 @@ int amdgpu_dm_plane_init(struct amdgpu_display_manager *dm,
 #ifdef AMD_PRIVATE_COLOR
 	dm_atomic_plane_attach_color_mgmt_properties(dm, plane);
 #else
+#ifdef HAVE_DRM_PLANE_COLOR_PIPELINE_PROPERTY
 	res = dm_plane_init_colorops(plane);
 	if (res)
 		return res;
+#endif
 #endif
 
 	/* Create (reset) the plane state */

drivers/gpu/drm/amd/dkms/m4/kernel.m4
@@ -79,6 +79,7 @@ AC_DEFUN([AC_CONFIG_KERNEL], [
 	AC_AMDGPU_DRM_DP_MST_TOPOLOGY_CBS
 	AC_AMDGPU_DRM_ATOMIC_PLANE_ENABLING
 	AC_AMDGPU_STRUCT_DRM_PLANE_HELPER_FUNCS
+	AC_AMDGPU_STRUCT_DRM_PLANE
 	AC_AMDGPU_DRM_DP_MST_ATOMIC_CHECK
 	AC_AMDGPU_DRM_DP_MST_ATOMIC_ENABLE_DSC
 	AC_AMDGPU_DRM_CONNECTOR_HELPER_FUNCS

drivers/gpu/drm/amd/dkms/m4/struct_drm_plane.m4
@@ -1,0 +1,21 @@
+dnl #
+dnl # commit v6.8-rc1-123-gd35b4e13b8b8
+dnl # drm/plane: Add color pipeline property
+dnl #
+AC_DEFUN([AC_AMDGPU_DRM_PLANE_COLOR_PIPELINE_PROPERTY], [
+	AC_KERNEL_DO_BACKGROUND([
+		AC_KERNEL_TRY_COMPILE([
+			#include <drm/drm_plane.h>
+		],[
+			struct drm_plane *plane = NULL;
+			plane->color_pipeline_property = NULL;
+		],[
+			AC_DEFINE(HAVE_DRM_PLANE_COLOR_PIPELINE_PROPERTY, 1,
+				[drm_plane->color_pipeline_property is available])
+		])
+	])
+])
+
+AC_DEFUN([AC_AMDGPU_STRUCT_DRM_PLANE], [
+	AC_AMDGPU_DRM_PLANE_COLOR_PIPELINE_PROPERTY
+])