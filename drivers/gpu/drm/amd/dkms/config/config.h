25b90de267e194eb8033661f98c29bc3eb622ab8
Chengjun Yao <Chengjun.Yao@amd.com>
2026-03-06 10:53:40 +0800

    drm/amdkcl: fake drm_fb_helper_gem_is_fb()
    It's cauesd by the commit: 2fa95f4681
    "drm/amdgpu: Move test for fbdev GEM object into generic helper"
    
    Signed-off-by: Chengjun Yao <Chengjun.Yao@amd.com>
    Reviewed-by: Bob Zhou <Bob.Zhou@amd.com>

--- drivers/gpu/drm/amd/dkms/config/config.h
+++ drivers/gpu/drm/amd/dkms/config/config.h
@@ -601,6 +601,9 @@
 /* drm_fb_helper_fill_info() is available */
 #define HAVE_DRM_FB_HELPER_FILL_INFO 1
 
+/* drm_fb_helper_gem_is_fb is available */
+#define HAVE_DRM_FB_HELPER_GEM_IS_FB 1
+
 /* drm_fb_helper_init() has 2 args */
 #define HAVE_DRM_FB_HELPER_INIT_2ARGS 1
 
