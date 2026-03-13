ROCm/amdgpu

commit 4cac08c8aa2e05cbfcba49189f7d74604e5cae01

drm/amdkcl: test if drm_gem_dmabuf_vmap() has struct iosys_map arg
Signed-off-by: Bob Zhou <bob.zhou@amd.com>

drivers/gpu/drm/amd/dkms/m4/drm_prime.m4
@@ -34,3 +34,27 @@ AC_DEFUN([AC_AMDGPU_DRM_GEM_PRIME_HANDLE_TO_FD], [
                 ])
         ])
 ])
+
+dnl #
+dnl # commit 6619ccf1bb1d
+dnl # dma-buf: Use struct dma_buf_map in dma_buf_vmap() interfaces
+dnl #
+dnl # commit 20e76f1a7059
+dnl # dma-buf: Use struct dma_buf_map in dma_buf_vunmap() interfaces
+dnl #
+dnl # commit c67e62790f5c
+dnl # drm/prime: split array import functions v4
+dnl #
+AC_DEFUN([AC_AMDGPU_DRM_GEM_DMABUF_VMAP_HAS_IOSYS_MAP_ARG], [
+	AC_KERNEL_DO_BACKGROUND([
+		AC_KERNEL_TRY_COMPILE_SYMBOL([
+			#include <drm/drm_prime.h>
+		],[
+			struct iosys_map map;
+			drm_gem_dmabuf_vmap(NULL, &map);
+		],[drm_gem_dmabuf_vmap], [drivers/gpu/drm/drm_prime.c], [
+			AC_DEFINE(HAVE_DRM_GEM_DMABUF_VMAP_HAS_IOSYS_MAP_ARG, 1,
+				[drm_gem_dmabuf_vmap() has struct iosys_map arg])
+		])
+	])
+])

drivers/gpu/drm/amd/amdgpu/amdgpu_dma_buf.c
@@ -511,6 +511,7 @@ static int amdgpu_dma_buf_begin_cpu_access(struct dma_buf *dma_buf,
 	return ret;
 }
 
+#ifdef HAVE_DRM_GEM_DMABUF_VMAP_HAS_IOSYS_MAP_ARG
 static int amdgpu_dma_buf_vmap(struct dma_buf *dma_buf, struct iosys_map *map)
 {
 	struct drm_gem_object *obj = dma_buf->priv;
@@ -540,6 +541,7 @@ static void amdgpu_dma_buf_vunmap(struct dma_buf *dma_buf, struct iosys_map *map
 	drm_gem_dmabuf_vunmap(dma_buf, map);
 	amdgpu_bo_unpin(bo);
 }
+#endif
 
 const struct dma_buf_ops amdgpu_dmabuf_ops = {
 #if defined(HAVE_DMA_BUF_OPS_LEGACY)
@@ -562,8 +564,13 @@ const struct dma_buf_ops amdgpu_dmabuf_ops = {
 	.release = drm_gem_dmabuf_release,
 	.begin_cpu_access = amdgpu_dma_buf_begin_cpu_access,
 	.mmap = drm_gem_dmabuf_mmap,
+#ifdef HAVE_DRM_GEM_DMABUF_VMAP_HAS_IOSYS_MAP_ARG
 	.vmap = amdgpu_dma_buf_vmap,
 	.vunmap = amdgpu_dma_buf_vunmap,
+#else
+	.vmap = drm_gem_dmabuf_vmap,
+	.vunmap = drm_gem_dmabuf_vunmap,
+#endif
 };
 
 /**

drivers/gpu/drm/amd/dkms/m4/kernel.m4
@@ -104,6 +104,7 @@ AC_DEFUN([AC_CONFIG_KERNEL], [
 	AC_AMDGPU_DRM_DP_SUBCONNECTOR
 	AC_AMDGPU_DRM_PRIME_SG_TO_DMA_ADDR_ARRAY
 	AC_AMDGPU_DRM_PRIME_PAGES_TO_SG
+	AC_AMDGPU_DRM_GEM_DMABUF_VMAP_HAS_IOSYS_MAP_ARG
 	AC_AMDGPU_DRM_CRTC_HELPER_FUNCS
 	AC_AMDGPU_DRM_DRIVER_GEM_OPEN_OBJECT
 	AC_AMDGPU_FS_RECLAIM_ACQUIRE

commit d6e28f15af9cc3867930224a506eea0ebb8d3108

drm/amdkcl: fix miss include headers in m4
It's caused by the commit: 4cac08c8a
"drm/amdkcl: test if drm_gem_dmabuf_vmap() has struct iosys_map arg"

Signed-off-by: Chengjun Yao <Chengjun.Yao@amd.com>
Reviewed-by: Bob Zhou <Bob.Zhou@amd.com>

drivers/gpu/drm/amd/dkms/m4/drm_prime.m4
@@ -49,6 +49,7 @@ AC_DEFUN([AC_AMDGPU_DRM_GEM_DMABUF_VMAP_HAS_IOSYS_MAP_ARG], [
 	AC_KERNEL_DO_BACKGROUND([
 		AC_KERNEL_TRY_COMPILE_SYMBOL([
 			#include <drm/drm_prime.h>
+			#include <linux/iosys-map.h>
 		],[
 			struct iosys_map map;
 			drm_gem_dmabuf_vmap(NULL, &map);