torvalds/linux

commit 590d356aa433074ece2b0d02faa5f959b26d54d6

mm: update shmem_[kernel]_file_*() functions to use vma_flags_t
In order to be able to use only vma_flags_t in vm_area_desc we must adjust
shmem file setup functions to operate in terms of vma_flags_t rather than
vm_flags_t.

This patch makes this change and updates all callers to use the new
functions.

No functional changes intended.

[akpm@linux-foundation.org: comment fixes, per Baolin]
Link: https://lkml.kernel.org/r/736febd280eb484d79cef5cf55b8a6f79ad832d2.1769097829.git.lorenzo.stoakes@oracle.com
Signed-off-by: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
Reviewed-by: Jarkko Sakkinen <jarkko@kernel.org>
Reviewed-by: Liam R. Howlett <Liam.Howlett@oracle.com>
Cc: Barry Song <baohua@kernel.org>
Cc: David Hildenbrand <david@kernel.org>
Cc: Dev Jain <dev.jain@arm.com>
Cc: Jason Gunthorpe <jgg@nvidia.com>
Cc: Suren Baghdasaryan <surenb@google.com>
Cc: Vlastimil Babka <vbabka@suse.cz>
Cc: Zi Yan <ziy@nvidia.com>
Cc: "Darrick J. Wong" <djwong@kernel.org>
Cc: Damien Le Moal <dlemoal@kernel.org>
Cc: Yury Norov <ynorov@nvidia.com>
Cc: Chris Mason <clm@fb.com>
Cc: Pedro Falcato <pfalcato@suse.de>
Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

drivers/gpu/drm/ttm/ttm_backup.c
@@ -178,5 +178,6 @@ EXPORT_SYMBOL_GPL(ttm_backup_bytes_avail);
  */
 struct file *ttm_backup_shmem_create(loff_t size)
 {
-	return shmem_file_setup("ttm shmem backup", size, 0);
+	return shmem_file_setup("ttm shmem backup", size,
+				EMPTY_VMA_FLAGS);
 }

drivers/gpu/drm/ttm/ttm_tt.c
@@ -330,7 +330,7 @@ int ttm_tt_swapout(struct ttm_device *bdev, struct ttm_tt *ttm,
 	struct page *to_page;
 	int i, ret;
 
-	swap_storage = shmem_file_setup("ttm swap", size, 0);
+	swap_storage = shmem_file_setup("ttm swap", size, EMPTY_VMA_FLAGS);
 	if (IS_ERR(swap_storage)) {
 		pr_err("Failed allocating swap storage\n");
 		return PTR_ERR(swap_storage);

arch/x86/kernel/cpu/sgx/ioctl.c
@@ -83,7 +83,7 @@ static int sgx_encl_create(struct sgx_encl *encl, struct sgx_secs *secs)
 	encl_size = secs->size + PAGE_SIZE;
 
 	backing = shmem_file_setup("SGX backing", encl_size + (encl_size >> 5),
-				   VM_NORESERVE);
+				   mk_vma_flags(VMA_NORESERVE_BIT));
 	if (IS_ERR(backing)) {
 		ret = PTR_ERR(backing);
 		goto err_out_shrink;

drivers/gpu/drm/drm_gem.c
@@ -130,14 +130,15 @@ int drm_gem_object_init_with_mnt(struct drm_device *dev,
 				 struct vfsmount *gemfs)
 {
 	struct file *filp;
+	const vma_flags_t flags = mk_vma_flags(VMA_NORESERVE_BIT);
 
 	drm_gem_private_object_init(dev, obj, size);
 
 	if (gemfs)
 		filp = shmem_file_setup_with_mnt(gemfs, "drm mm object", size,
-						 VM_NORESERVE);
+						 flags);
 	else
-		filp = shmem_file_setup("drm mm object", size, VM_NORESERVE);
+		filp = shmem_file_setup("drm mm object", size, flags);
 
 	if (IS_ERR(filp))
 		return PTR_ERR(filp);

drivers/gpu/drm/i915/gem/i915_gem_shmem.c
@@ -496,7 +496,7 @@ static int __create_shmem(struct drm_i915_private *i915,
 			  struct drm_gem_object *obj,
 			  resource_size_t size)
 {
-	unsigned long flags = VM_NORESERVE;
+	const vma_flags_t flags = mk_vma_flags(VMA_NORESERVE_BIT);
 	struct file *filp;
 
 	drm_gem_private_object_init(&i915->drm, obj, size);

drivers/gpu/drm/i915/gem/i915_gem_ttm.c
@@ -200,7 +200,8 @@ static int i915_ttm_tt_shmem_populate(struct ttm_device *bdev,
 		struct address_space *mapping;
 		gfp_t mask;
 
-		filp = shmem_file_setup("i915-shmem-tt", size, VM_NORESERVE);
+		filp = shmem_file_setup("i915-shmem-tt", size,
+					mk_vma_flags(VMA_NORESERVE_BIT));
 		if (IS_ERR(filp))
 			return PTR_ERR(filp);
 

drivers/gpu/drm/i915/gt/shmem_utils.c
@@ -19,7 +19,8 @@ struct file *shmem_create_from_data(const char *name, void *data, size_t len)
 	struct file *file;
 	int err;
 
-	file = shmem_file_setup(name, PAGE_ALIGN(len), VM_NORESERVE);
+	file = shmem_file_setup(name, PAGE_ALIGN(len),
+				mk_vma_flags(VMA_NORESERVE_BIT));
 	if (IS_ERR(file))
 		return file;
 

drivers/gpu/drm/ttm/tests/ttm_tt_test.c
@@ -143,7 +143,7 @@ static void ttm_tt_fini_shmem(struct kunit *test)
 	err = ttm_tt_init(tt, bo, 0, caching, 0);
 	KUNIT_ASSERT_EQ(test, err, 0);
 
-	shmem = shmem_file_setup("ttm swap", BO_SIZE, 0);
+	shmem = shmem_file_setup("ttm swap", BO_SIZE, EMPTY_VMA_FLAGS);
 	tt->swap_storage = shmem;
 
 	ttm_tt_fini(tt);

ROCm/amdgpu

commit 84e621d9a255d2d4cda704d4b6bf42666bc8b439

drm/amdkcl: handle shmem_file_setup() vma_flags_t change in kernel 7.0
It's caused by the commit: 590d356aa433
"mm: update shmem_[kernel]_file_*() functions to use vma_flags_t"

Signed-off-by: Chengjun Yao <Chengjun.Yao@amd.com>
Reviewed-by: Bob Zhou <Bob.Zhou@amd.com>

drivers/gpu/drm/amd/dkms/config/config.h
@@ -1141,6 +1141,9 @@
 /* sg_alloc_table_from_pages_segment() is available */
 #define HAVE_SG_ALLOC_TABLE_FROM_PAGES_SEGMENT 1
 
+/* shmem_file_setup() takes vma_flags_t arg */
+/* #undef HAVE_SHMEM_FILE_SETUP_VMA_FLAGS */
+
 /* shmem_read_folio() is available */
 #define HAVE_SHMEM_READ_FOLIO 1
 

drivers/gpu/drm/amd/dkms/m4/kernel.m4
@@ -268,6 +268,7 @@ AC_DEFUN([AC_CONFIG_KERNEL], [
 	AC_AMDGPU_RATELIMIT_STATE_RESET_INTERVAL
 	AC_AMDGPU_TIMER_DELETE
 	AC_AMDGPU_SHMEM_WRITEOUT
+	AC_AMDGPU_SHMEM_FILE_SETUP_VMA_FLAGS
 	AC_AMDGPU_KMAP_LOCAL_PAGE_TRY_FROM_PANIC
 	AC_AMDGPU_PM_HIBERNATE_IS_RECOVERING
 	AC_AMDGPU_PM_HIBERNATION_MODE_IS_SUSPEND

drivers/gpu/drm/amd/dkms/m4/shmem-file-setup-vma-flags.m4
@@ -1,0 +1,17 @@
+dnl #
+dnl # v7.0-rc4-590d356aa433
+dnl # mm: update shmem_[kernel]_file_*() functions to use vma_flags_t
+dnl #
+AC_DEFUN([AC_AMDGPU_SHMEM_FILE_SETUP_VMA_FLAGS], [
+	AC_KERNEL_DO_BACKGROUND([
+		AC_KERNEL_TRY_COMPILE([
+			#include <linux/shmem_fs.h>
+			#include <linux/mm_types.h>
+		], [
+			shmem_file_setup("test", 0, EMPTY_VMA_FLAGS);
+		], [
+			AC_DEFINE(HAVE_SHMEM_FILE_SETUP_VMA_FLAGS, 1,
+				[shmem_file_setup() takes vma_flags_t arg])
+		])
+	])
+])

drivers/gpu/drm/ttm/ttm_backup.c
@@ -195,5 +195,9 @@ EXPORT_SYMBOL_GPL(ttm_backup_bytes_avail);
  */
 struct file *ttm_backup_shmem_create(loff_t size)
 {
+#ifdef HAVE_SHMEM_FILE_SETUP_VMA_FLAGS
+	return shmem_file_setup("ttm shmem backup", size, EMPTY_VMA_FLAGS);
+#else
 	return shmem_file_setup("ttm shmem backup", size, 0);
+#endif
 }

drivers/gpu/drm/ttm/ttm_tt.c
@@ -336,7 +336,11 @@ int ttm_tt_swapout(struct ttm_device *bdev, struct ttm_tt *ttm,
 	struct page *to_page;
 	int i, ret;
 
+#ifdef HAVE_SHMEM_FILE_SETUP_VMA_FLAGS
+	swap_storage = shmem_file_setup("ttm swap", size, EMPTY_VMA_FLAGS);
+#else
 	swap_storage = shmem_file_setup("ttm swap", size, 0);
+#endif
 	if (IS_ERR(swap_storage)) {
 		pr_err("Failed allocating swap storage\n");
 		return PTR_ERR(swap_storage);