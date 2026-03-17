ROCm/amdgpu

commit 67ef8eaad9765a811b01c49c53b5f53d15b693ec

drm/amdkcl: test if dma_fence_check_and_signal() is available
It's caused by 06bc18e0def7d926265480faa555bf0b67a35a90
"amd/amdkfd: Use dma_fence_check_and_signal()"

Signed-off-by: Bob Zhou <bob.zhou@amd.com>

drivers/gpu/drm/amd/dkms/m4/dma_fence_check_and_signal.m4
@@ -1,0 +1,16 @@
+dnl #
+dnl # v6.18-rc2-1390-gc891b99d25dd
+dnl # dma-buf/dma-fence: Add dma_fence_check_and_signal()
+dnl #
+AC_DEFUN([AC_AMDGPU_DMA_FENCE_CHECK_AND_SIGNAL], [
+	AC_KERNEL_DO_BACKGROUND([
+		AC_KERNEL_TRY_COMPILE_SYMBOL([
+			#include <linux/dma-fence.h>
+		], [
+			dma_fence_check_and_signal(NULL);
+		], [dma_fence_check_and_signal], [drivers/dma-buf/st-dma-fence.c],[
+			AC_DEFINE(HAVE_DMA_FENCE_CHECK_AND_SIGNAL, 1,
+				[dma_fence_check_and_signal() is available])
+		])
+	])
+])

drivers/gpu/drm/amd/amdkfd/kfd_process.c
@@ -2230,8 +2230,11 @@ static bool signal_eviction_fence(struct kfd_process *p)
 	rcu_read_unlock();
 	if (!ef)
 		return true;
-
+#ifdef HAVE_DMA_FENCE_CHECK_AND_SIGNAL
 	ret = dma_fence_check_and_signal(ef);
+#else
+	ret = dma_fence_signal(ef);
+#endif
 	dma_fence_put(ef);
 
 	return ret;

drivers/gpu/drm/amd/dkms/m4/kernel.m4
@@ -154,6 +154,7 @@ AC_DEFUN([AC_CONFIG_KERNEL], [
 	AC_AMDGPU_KREALLOC_ARRAY
 	AC_AMDGPU_VGA_CLIENT_REGISTER_NOT_PASS_COOKIE
 	AC_AMDGPU_VMA_LOOKUP
+	AC_AMDGPU_DMA_FENCE_CHECK_AND_SIGNAL
 	AC_AMDGPU_DMA_FENCE_CHAIN_ALLOC
 	AC_AMDGPU_DMA_FENCE_CHAIN_STRUCT
 	AC_AMDGPU_DMA_FENCE_OPS_USE_64BIT_SEQNO

commit ffdb06ac39f0c4718f6f1b3d88d57a126b13622a

drm/amdkcl: fix use wrong file name
Fixes: 67ef8eaad("drm/amdkcl: test if dma_fence_check_and_signal() is available")
Signed-off-by: Chengjun Yao <Chengjun.Yao@amd.com>
Reviewed-by: Bob Zhou <Bob.Zhou@amd.com>

drivers/gpu/drm/amd/dkms/m4/dma_fence_check_and_signal.m4
@@ -8,7 +8,7 @@ AC_DEFUN([AC_AMDGPU_DMA_FENCE_CHECK_AND_SIGNAL], [
 			#include <linux/dma-fence.h>
 		], [
 			dma_fence_check_and_signal(NULL);
-		], [dma_fence_check_and_signal], [drivers/dma-buf/st-dma-fence.c],[
+		], [dma_fence_check_and_signal], [drivers/dma-buf/dma-fence.c],[
 			AC_DEFINE(HAVE_DMA_FENCE_CHECK_AND_SIGNAL, 1,
 				[dma_fence_check_and_signal() is available])
 		])