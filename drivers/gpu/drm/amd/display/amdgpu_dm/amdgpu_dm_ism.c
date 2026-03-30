3599ec1be1b817d684e43540fd13039da3a790e0
Chengjun Yao <Chengjun.Yao@amd.com>
2026-03-30 14:26:17 +0800

    drm/amdkcl: fake disable_delayed_work_sync()
    It's caused by the commit: 0b572a6bf0
    "drm/amd/display: Add Idle state manager(ISM)"
    
    Signed-off-by: Chengjun Yao <Chengjun.Yao@amd.com>
    Reviewed-by: Bob Zhou <Bob.Zhou@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_ism.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_ism.c
@@ -564,8 +564,14 @@ void amdgpu_dm_ism_disable(struct amdgpu_display_manager *dm)
 		acrtc = to_amdgpu_crtc(crtc);
 		ism = &acrtc->ism;
 
+#ifdef HAVE_DISABLE_DELAYED_WORK_SYNC
+		/* Cancel and disable any pending work */
 		disable_delayed_work_sync(&ism->delayed_work);
 		disable_delayed_work_sync(&ism->sso_delayed_work);
+#else
+		cancel_delayed_work_sync(&ism->delayed_work);
+		cancel_delayed_work_sync(&ism->sso_delayed_work);
+#endif
 	}
 }
 
@@ -622,8 +628,10 @@ void amdgpu_dm_ism_enable(struct amdgpu_display_manager *dm)
 		acrtc = to_amdgpu_crtc(crtc);
 		ism = &acrtc->ism;
 
+#ifdef HAVE_DISABLE_DELAYED_WORK_SYNC
 		enable_delayed_work(&ism->delayed_work);
 		enable_delayed_work(&ism->sso_delayed_work);
+#endif
 	}
 }
 
