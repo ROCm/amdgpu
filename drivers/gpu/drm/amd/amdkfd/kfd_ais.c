ROCm/amdgpu

commit 472dafb71704bc9d223d138f0d3d6ef476ece26d

drm/amdkfd: Use filesystem DIO alignment for AIS
Query direct I/O alignment via STATX_DIOALIGN instead of
requiring 4KB page alignment. Falls back to PAGE_SIZE if unsupported.

Reviewed-by: Amir Shetaia <Amir.Shetaia@amd.com>
Signed-off-by: Alysa Liu <Alysa.Liu@amd.com>

drivers/gpu/drm/amd/amdkfd/kfd_ais.c
@@ -29,6 +29,7 @@
 #include <linux/mmzone.h>
 #include <linux/dma-direct.h>
 #include <linux/mount.h>
+#include <linux/stat.h>
 /* Each VRAM page uses sizeof(struct page) on system memory */
 #define AIS_P2P_PAGE_STRUCT_SIZE(size) ((size)/PAGE_SIZE * sizeof(struct page))
 
@@ -250,6 +251,25 @@ out:
 }
 #endif /* HAVE_STRUCT_XARRAY */
 
+static int kfd_ais_get_dio_align(struct file *filep, unsigned int *offset_align,
+			unsigned int *mem_align)
+{
+	struct kstat st;
+
+	if (!vfs_getattr(&filep->f_path, &st, STATX_DIOALIGN, 0) &&
+		(st.result_mask & STATX_DIOALIGN)) {
+		if (!st.dio_offset_align || !st.dio_mem_align)
+			return -EINVAL;
+		*offset_align = st.dio_offset_align;
+		*mem_align = st.dio_mem_align;
+		return 0;
+	}
+
+	*offset_align = PAGE_SIZE;
+	*mem_align = PAGE_SIZE;
+	return 0;
+}
+
 int kfd_ais_rw_file(struct amdgpu_device *adev, struct amdgpu_bo *bo,
 		    struct kfd_ais_in_args *in, struct kfd_process_device *pdd,
 		    uint64_t *size_copied)
@@ -264,16 +284,25 @@ int kfd_ais_rw_file(struct amdgpu_device *adev, struct amdgpu_bo *bo,
 	loff_t cur_pos;
 	int ret = 0;
 	bool is_read = (in->op == KFD_IOC_AIS_READ);
-
-	/* For now support only page-aligned offsets and sizes. It could be
-	 * improved to fs block size in the future
-	 */
-	if (!PAGE_ALIGNED(in->file_offset) || !PAGE_ALIGNED(in->size))
-		return -EINVAL;
+	unsigned int dio_offset_align, dio_mem_align;
 
 	filep = fget((unsigned int)in->fd);
 	if (!filep)
 		return -EBADF;
+
+	if (filep->f_flags & O_DIRECT) {
+		ret = kfd_ais_get_dio_align(filep, &dio_offset_align, &dio_mem_align);
+		if (ret) {
+			dev_err(adev->dev, "AIS: file does not support direct I/O\n");
+			goto out;
+		}
+		if (!IS_ALIGNED(in->file_offset, dio_offset_align) ||
+			!IS_ALIGNED(in->size, dio_offset_align) ||
+			!IS_ALIGNED(in->handle_offset, dio_mem_align)) {
+			ret = -EINVAL;
+			goto out;
+		}
+	}
 
 	pdev = get_pci_dev_from_file(filep);
 	if (!pdev) {

commit 9087192ac424de1c299f4996c59fa023fbdfb80f

drm/amdkcl: Wrap code under HAVE_STATX_DIOALIGN
it's caused by 472dafb71704bc9d223d138
"drm/amdkfd: Use filesystem DIO alignment for AIS"

Signed-off-by: Yang Su <Yang.Su2@amd.com>

drivers/gpu/drm/amd/amdkfd/kfd_ais.c
@@ -333,6 +333,7 @@ static bool kfd_ais_check_p2p_cached(struct kfd_process_device *pdd,
 }
 #endif /* HAVE_STRUCT_XARRAY */
 
+#ifdef HAVE_STATX_DIOALIGN
 static int kfd_ais_get_dio_align(struct file *filep, unsigned int *offset_align,
 			unsigned int *mem_align)
 {
@@ -351,6 +352,7 @@ static int kfd_ais_get_dio_align(struct file *filep, unsigned int *offset_align,
 	*mem_align = PAGE_SIZE;
 	return 0;
 }
+#endif
 
 int kfd_ais_rw_file(struct amdgpu_device *adev, struct amdgpu_bo *bo,
 		    struct kfd_ais_in_args *in, struct kfd_process_device *pdd,
@@ -366,12 +368,20 @@ int kfd_ais_rw_file(struct amdgpu_device *adev, struct amdgpu_bo *bo,
 	loff_t cur_pos;
 	int ret = 0;
 	bool is_read = (in->op == KFD_IOC_AIS_READ);
+#ifdef HAVE_STATX_DIOALIGN
 	unsigned int dio_offset_align, dio_mem_align;
+#else
+	/* For now support only page-aligned offsets and sizes. It could be
+	 * improved to fs block size in the future
+	 */
+	if (!PAGE_ALIGNED(in->file_offset) || !PAGE_ALIGNED(in->size))
+		return -EINVAL;
+#endif
 
 	filep = fget((unsigned int)in->fd);
 	if (!filep)
 		return -EBADF;
-
+#ifdef HAVE_STATX_DIOALIGN
 	if (filep->f_flags & O_DIRECT) {
 		ret = kfd_ais_get_dio_align(filep, &dio_offset_align, &dio_mem_align);
 		if (ret) {
@@ -385,6 +395,7 @@ int kfd_ais_rw_file(struct amdgpu_device *adev, struct amdgpu_bo *bo,
 			goto out;
 		}
 	}
+#endif
 
 	pdev = get_pci_dev_from_file(filep);
 	if (!pdev) {

drivers/gpu/drm/amd/dkms/config/config.h
@@ -1174,6 +1174,9 @@
 /* enum SMCA_UMC_V2 is available */
 #define HAVE_SMCA_UMC_V2 1
 
+/* STATX_DIOALIGN and kstat dio_offset_align/dio_mem_align are available */
+#define HAVE_STATX_DIOALIGN 1
+
 /* strscpy() allows two arguments */
 #define HAVE_STRSCPY_ALLOW_TWO_ARGUMENTS 1
 

drivers/gpu/drm/amd/dkms/m4/kernel.m4
@@ -287,6 +287,7 @@ AC_DEFUN([AC_CONFIG_KERNEL], [
 	AC_AMDGPU_DRM_FB_CREATE_FORMAT_INFO
 	AC_AMDGPU_DRM_GET_FORMAT_INFO_2_ARGS
 	AC_AMDGPU_VFS_IOCB_ITER_READ
+	AC_AMDGPU_STATX_DIOALIGN
 	AC_AMDGPU_LIST_IS_HEAD
 	AC_AMDGPU_DEVM_I2C_ADD_ADAPTER
 	AC_AMDGPU_MEMPOOL_ALLOC_PREALLOCATED

drivers/gpu/drm/amd/dkms/m4/statx-dioalign.m4
@@ -1,0 +1,20 @@
+dnl #
+dnl # v6.0-rc2-1-g825cf206ed51
+dnl # statx: add direct I/O alignment information
+dnl #
+AC_DEFUN([AC_AMDGPU_STATX_DIOALIGN], [
+	AC_KERNEL_DO_BACKGROUND([
+		AC_KERNEL_TRY_COMPILE([
+			#include <linux/stat.h>
+			#include <linux/fs.h>
+		],[
+			struct kstat st;
+			st.dio_offset_align = 0;
+			st.dio_mem_align = 0;
+			(void)STATX_DIOALIGN;
+		],[
+			AC_DEFINE(HAVE_STATX_DIOALIGN, 1,
+				[STATX_DIOALIGN and kstat dio_offset_align/dio_mem_align are available])
+		])
+	])
+])