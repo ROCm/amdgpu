c511c2db00847b1ec5872efe10fd90c19c83611f
Yang Su <Yang.Su2@amd.com>
2026-04-02 13:45:22 +0800

    drm/amdkcl: wrap code under STATX_DIOALIGN
    Simply use STATX_DIOALIGN to bypass and cover all modifications;
    there is no need to define HAVE_STATX_DIOALIGN. Minimize the
    creation of m4 files.
    
    Signed-off-by: Yang Su <Yang.Su2@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_ais.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_ais.c
@@ -325,7 +325,7 @@ static bool kfd_ais_check_p2p_cached(struct kfd_process_device *pdd,
 	return xa_load(&pdd->ais_counters_xa, kfd_ais_make_key(pdev)) != NULL;
 }
 
-#ifdef HAVE_STATX_DIOALIGN
+#ifdef STATX_DIOALIGN
 static int kfd_ais_get_dio_align(struct file *filep, unsigned int *offset_align,
 			unsigned int *mem_align)
 {
@@ -360,7 +360,7 @@ int kfd_ais_rw_file(struct amdgpu_device *adev, struct amdgpu_bo *bo,
 	loff_t cur_pos;
 	int ret = 0;
 	bool is_read = (in->op == KFD_IOC_AIS_READ);
-#ifdef HAVE_STATX_DIOALIGN
+#ifdef STATX_DIOALIGN
 	unsigned int dio_offset_align, dio_mem_align;
 #else
 	/* For now support only page-aligned offsets and sizes. It could be
@@ -373,7 +373,7 @@ int kfd_ais_rw_file(struct amdgpu_device *adev, struct amdgpu_bo *bo,
 	filep = fget((unsigned int)in->fd);
 	if (!filep)
 		return -EBADF;
-#ifdef HAVE_STATX_DIOALIGN
+#ifdef STATX_DIOALIGN
 	if (filep->f_flags & O_DIRECT) {
 		ret = kfd_ais_get_dio_align(filep, &dio_offset_align, &dio_mem_align);
 		if (ret) {
