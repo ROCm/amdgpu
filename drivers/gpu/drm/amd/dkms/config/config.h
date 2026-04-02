8ff5a954b20806d237f57431e6d4c591005a8169
Yang Su <Yang.Su2@amd.com>
2026-04-02 13:47:53 +0800

    drm/amdkcl: clean up macro HAVE_STATX_DIOALIGN
    HAVE_STATX_DIOALIGN is a useless macro, STATX_DIOALIGN alone
    can achieve the bypass, so clean it.
    
    Signed-off-by: Yang Su <Yang.Su2@amd.com>

--- drivers/gpu/drm/amd/dkms/config/config.h
+++ drivers/gpu/drm/amd/dkms/config/config.h
@@ -1171,9 +1171,6 @@
 /* enum SMCA_UMC_V2 is available */
 #define HAVE_SMCA_UMC_V2 1
 
-/* STATX_DIOALIGN and kstat dio_offset_align/dio_mem_align are available */
-#define HAVE_STATX_DIOALIGN 1
-
 /* strscpy() allows two arguments */
 #define HAVE_STRSCPY_ALLOW_TWO_ARGUMENTS 1
 
