cde41d1d9be7c12a43cdf74cf8125b4b47affab1
Yang Su <Yang.Su2@amd.com>
2026-04-13 11:21:10 +0800

    drm/amdkcl: wrap code under HAVE_DRM_DRM_COLOROP_H
    it's cuase by 1fcdb4fd2948918b8b
    "drm/amd/display: Use overlay cursor when color pipeline is active"
    
    Signed-off-by: Yang Su <Yang.Su2@amd.com>
    Reviewed-by: Chengjun Yao <Chengjun.Yao@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -12703,6 +12703,7 @@ static int add_affected_mst_dsc_crtcs(struct drm_atomic_state *state, struct drm
  * available.
  */
 
+#ifdef HAVE_DRM_DRM_COLOROP_H
 /**
  * dm_plane_color_pipeline_active() - Check if a plane's color pipeline active.
  * @state: DRM atomic state
@@ -12734,6 +12735,7 @@ static bool dm_plane_color_pipeline_active(struct drm_atomic_state *state,
 	}
 	return false;
 }
+#endif
 
 /**
  * dm_crtc_get_cursor_mode() - Determine the required cursor mode on crtc
@@ -12811,11 +12813,13 @@ static int dm_crtc_get_cursor_mode(struct amdgpu_device *adev,
 			break;
 		}
 
+#ifdef HAVE_DRM_DRM_COLOROP_H
 		if (dm_plane_color_pipeline_active(state, plane, true) !=
 		    dm_plane_color_pipeline_active(state, plane, false)) {
 			consider_mode_change = true;
 			break;
 		}
+#endif
 	}
 
 	if (!consider_mode_change && !crtc_state->zpos_changed)
@@ -12856,11 +12860,13 @@ static int dm_crtc_get_cursor_mode(struct amdgpu_device *adev,
 			return 0;
 		}
 
+#ifdef HAVE_DRM_DRM_COLOROP_H
 		/* Underlying plane has an active color pipeline - cursor would be transformed */
 		if (dm_plane_color_pipeline_active(state, plane, false)) {
 			*cursor_mode = DM_CURSOR_OVERLAY_MODE;
 			return 0;
 		}
+#endif
 
 		dm_get_plane_scale(plane_state,
 				   &underlying_scale_w, &underlying_scale_h);
