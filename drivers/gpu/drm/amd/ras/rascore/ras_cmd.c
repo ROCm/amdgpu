adf7ce703ca8caa0317ec03a61bea38a1ce5c1be
Candice Li <candice.li@amd.com>
2026-05-13 10:46:01 +0800

    drm/amd/ras: bound CPER record fetch buffer size
    Bound CPER record fetch allocation by buffer size.
    
    v2: Drop redundant cap on cper_num and raise
        GET_CPER_RECORD max buffer size.
    
    Suggested-by: YiPeng Chai <YiPeng.Chai@amd.com>
    Signed-off-by: Candice Li <candice.li@amd.com>
    Reviewed-by: Tao Zhou <tao.zhou1@amd.com>

--- drivers/gpu/drm/amd/ras/rascore/ras_cmd.c
+++ drivers/gpu/drm/amd/ras/rascore/ras_cmd.c
@@ -214,7 +214,8 @@ static int ras_cmd_get_cper_records(struct ras_core_context *ras_core,
 		(cmd->output_buf_size < sizeof(*rsp)))
 		return RAS_CMD__ERROR_INVALID_INPUT_SIZE;
 
-	if (!req->buf_size || !req->buf_ptr || !req->cper_num)
+	if (!req->buf_size || !req->buf_ptr || !req->cper_num ||
+	    req->buf_size > RAS_CMD_MAX_CPER_BUF_SZ)
 		return RAS_CMD__ERROR_INVALID_INPUT_DATA;
 
 	buffer = kzalloc(req->buf_size, GFP_KERNEL);
