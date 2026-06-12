ec87c059282fb1633ab62db8430c2d6ec468f95c
Yang Su <Yang.Su2@amd.com>
2026-06-12 13:38:55 +0800

    drm/amdkcl: add KCL header wrapper for drm/drm_vblank_helper.h
    Add the AC_KERNEL_CHECK_HEADERS detection in drm-headers.m4 and a KCL
    wrapper header that conditionally includes the real header only when
    HAVE_DRM_DRM_VBLANK_HELPER_H is defined.
    
    Signed-off-by: Yang Su <Yang.Su2@amd.com>
    Reviewed-by: Chengjun Yao <Chengjun.Yao@amd.com>

---
+++ include/kcl/header/drm/drm_vblank_helper.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef _KCL_HEADER_DRM_VBLANK_HELPER_H_
+#define _KCL_HEADER_DRM_VBLANK_HELPER_H_
+
+#ifdef HAVE_DRM_DRM_VBLANK_HELPER_H
+#include_next <drm/drm_vblank_helper.h>
+#endif
+
+#endif
