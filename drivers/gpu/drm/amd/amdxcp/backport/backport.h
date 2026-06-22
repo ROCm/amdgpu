ROCm/ROCK-Kernel-Driver

drm/amdkcl: amdxcp module add kcl support
It's caused by 7dd353a7623ec9a10f9bfde53b19982f760995a2
"drm/amdxcp: add platform device driver for amdxcp"

Signed-off-by: Bob Zhou <bob.zhou@amd.com>
Reviewed-by: Flora Cui <flora.cui@amd.com>

drivers/gpu/drm/amd/amdxcp/backport/backport.h
@@ -1,0 +1,1 @@
+#include "kcl/kcl_drm_drv.h"

ROCm/amdgpu

drivers/gpu/drm/amd/amdxcp/backport/backport.h
@@ -1,0 +1,1 @@
+#include "kcl/kcl_amdgpu_drm_drv.h"