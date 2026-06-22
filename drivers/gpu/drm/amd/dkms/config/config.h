torvalds/linux

commit ae80122f3896c88884841520c983a6e8551da448

drm/ttm: use gpu mm stats to track gpu memory allocations. (v4)
This uses the newly introduced per-node gpu tracking stats,
to track GPU memory allocated via TTM and reclaimable memory in
the TTM page pools.

These stats will be useful later for system information and
later when mem cgroups are integrated.

Cc: Christian Koenig <christian.koenig@amd.com>
Cc: Matthew Brost <matthew.brost@intel.com>
Cc: Johannes Weiner <hannes@cmpxchg.org>
Cc: linux-mm@kvack.org
Cc: Andrew Morton <akpm@linux-foundation.org>
Acked-by: Christian König <christian.koenig@amd.com>
Signed-off-by: Dave Airlie <airlied@redhat.com>

drivers/gpu/drm/ttm/ttm_pool.c
@@ -159,8 +159,10 @@ static struct page *ttm_pool_alloc_page(struct ttm_pool *pool, gfp_t gfp_flags,
 
 	if (!ttm_pool_uses_dma_alloc(pool)) {
 		p = alloc_pages_node(pool->nid, gfp_flags, order);
-		if (p)
+		if (p) {
 			p->private = order;
+			mod_lruvec_page_state(p, NR_GPU_ACTIVE, 1 << order);
+		}
 		return p;
 	}
 
@@ -195,7 +197,7 @@ error_free:
 
 /* Reset the caching and pages of size 1 << order */
 static void ttm_pool_free_page(struct ttm_pool *pool, enum ttm_caching caching,
-			       unsigned int order, struct page *p)
+			       unsigned int order, struct page *p, bool reclaim)
 {
 	unsigned long attr = DMA_ATTR_FORCE_CONTIGUOUS;
 	struct ttm_pool_dma *dma;
@@ -210,6 +212,8 @@ static void ttm_pool_free_page(struct ttm_pool *pool, enum ttm_caching caching,
 #endif
 
 	if (!pool || !ttm_pool_uses_dma_alloc(pool)) {
+		mod_lruvec_page_state(p, reclaim ? NR_GPU_RECLAIM : NR_GPU_ACTIVE,
+				      -(1 << order));
 		__free_pages(p, order);
 		return;
 	}
@@ -297,6 +301,9 @@ static void ttm_pool_type_give(struct ttm_pool_type *pt, struct page *p)
 	list_add(&p->lru, &pt->pages);
 	spin_unlock(&pt->lock);
 	atomic_long_add(1 << pt->order, &allocated_pages);
+
+	mod_lruvec_page_state(p, NR_GPU_ACTIVE, -num_pages);
+	mod_lruvec_page_state(p, NR_GPU_RECLAIM, num_pages);
 }
 
 /* Take pages from a specific pool_type, return NULL when nothing available */
@@ -308,6 +315,8 @@ static struct page *ttm_pool_type_take(struct ttm_pool_type *pt)
 	p = list_first_entry_or_null(&pt->pages, typeof(*p), lru);
 	if (p) {
 		atomic_long_sub(1 << pt->order, &allocated_pages);
+		mod_lruvec_page_state(p, NR_GPU_ACTIVE, (1 << pt->order));
+		mod_lruvec_page_state(p, NR_GPU_RECLAIM, -(1 << pt->order));
 		list_del(&p->lru);
 	}
 	spin_unlock(&pt->lock);
@@ -340,7 +349,7 @@ static void ttm_pool_type_fini(struct ttm_pool_type *pt)
 	spin_unlock(&shrinker_lock);
 
 	while ((p = ttm_pool_type_take(pt)))
-		ttm_pool_free_page(pt->pool, pt->caching, pt->order, p);
+		ttm_pool_free_page(pt->pool, pt->caching, pt->order, p, true);
 }
 
 /* Return the pool_type to use for the given caching and order */
@@ -392,7 +401,7 @@ static unsigned int ttm_pool_shrink(void)
 
 	p = ttm_pool_type_take(pt);
 	if (p) {
-		ttm_pool_free_page(pt->pool, pt->caching, pt->order, p);
+		ttm_pool_free_page(pt->pool, pt->caching, pt->order, p, true);
 		num_pages = 1 << pt->order;
 	} else {
 		num_pages = 0;
@@ -484,7 +493,7 @@ static pgoff_t ttm_pool_unmap_and_free(struct ttm_pool *pool, struct page *page,
 	if (pt)
 		ttm_pool_type_give(pt, page);
 	else
-		ttm_pool_free_page(pool, caching, order, page);
+		ttm_pool_free_page(pool, caching, order, page, false);
 
 	return nr;
 }
@@ -792,7 +801,7 @@ static int __ttm_pool_alloc(struct ttm_pool *pool, struct ttm_tt *tt,
 	return 0;
 
 error_free_page:
-	ttm_pool_free_page(pool, page_caching, order, p);
+	ttm_pool_free_page(pool, page_caching, order, p, false);
 
 error_free_all:
 	if (tt->restore)

ROCm/amdgpu

commit 857215f5ba4abe3dc4dfcce01c575d6801bf6b9f

drm/amdkcl: test if NR_GPU_ACTIVE is available
It's caused by ae80122f3896c
"drm/ttm: use gpu mm stats to track gpu memory allocations. (v4)"

Wrap mod_lruvec_page_state() calls with #ifdef on old kernels.

Signed-off-by: Chengjun Yao <Chengjun.Yao@amd.com>

drivers/gpu/drm/amd/dkms/config/config.h
@@ -1078,6 +1078,9 @@
 /* kvrealloc() isn't available */
 /* #undef HAVE_NO_KVREALLOC */
 
+/* NR_GPU_ACTIVE is available */
+#define HAVE_NR_GPU_ACTIVE 1
+
 /* class_create has one argument */
 #define HAVE_ONE_ARGUMENT_OF_CLASS_CREATE 1
 

drivers/gpu/drm/amd/dkms/m4/kernel.m4
@@ -294,6 +294,7 @@ AC_DEFUN([AC_CONFIG_KERNEL], [
 	AC_AMDGPU_MEMPOOL_ALLOC_PREALLOCATED
 	AC_AMDGPU_KFD_PEERDIRECT_SUPPORT
 	AC_AMDGPU_SYSTEM_PERCPU_WQ
+	AC_AMDGPU_NR_GPU_ACTIVE
 	AC_AMDGPU_PCI_DEV_IS_DISCONNECTED
 	AC_DRM_EDID_CONNECTOR_ADD_MODES
 	AC_DRM_EDID_CONNECTOR_UPDATE

drivers/gpu/drm/amd/dkms/m4/nr-gpu-active.m4
@@ -1,0 +1,16 @@
+dnl #
+dnl # v7.0-rc7-1767-g2232ba9c7931d
+dnl # mm: add gpu active/reclaim per-node stat counters
+dnl #
+AC_DEFUN([AC_AMDGPU_NR_GPU_ACTIVE], [
+	AC_KERNEL_DO_BACKGROUND([
+		AC_KERNEL_TRY_COMPILE([
+			#include <linux/mmzone.h>
+		], [
+			enum node_stat_item item = NR_GPU_ACTIVE;
+			(void)item;
+		], [
+			AC_DEFINE(HAVE_NR_GPU_ACTIVE, 1, [NR_GPU_ACTIVE is available])
+		])
+	])
+])

drivers/gpu/drm/ttm/ttm_pool.c
@@ -177,7 +177,9 @@ static struct page *ttm_pool_alloc_page(struct ttm_pool *pool, gfp_t gfp_flags,
 		p = alloc_pages_node(pool->nid, gfp_flags, order);
 		if (p) {
 			p->private = order;
+#ifdef HAVE_NR_GPU_ACTIVE
 			mod_lruvec_page_state(p, NR_GPU_ACTIVE, 1 << order);
+#endif
 		}
 		return p;
 	}
@@ -214,8 +216,10 @@ error_free:
 static void __free_pages_gpu_account(struct page *p, unsigned int order,
 				     bool reclaim)
 {
+#ifdef HAVE_NR_GPU_ACTIVE
 	mod_lruvec_page_state(p, reclaim ? NR_GPU_RECLAIM : NR_GPU_ACTIVE,
 			      -(1 << order));
+#endif
 	__free_pages(p, order);
 }
 
@@ -326,8 +330,10 @@ static void ttm_pool_type_give(struct ttm_pool_type *pt, struct page *p)
 	rcu_read_unlock();
 
 	atomic_long_add(num_pages, &allocated_pages[nid]);
+#ifdef HAVE_NR_GPU_ACTIVE
 	mod_lruvec_page_state(p, NR_GPU_ACTIVE, -num_pages);
 	mod_lruvec_page_state(p, NR_GPU_RECLAIM, num_pages);
+#endif
 }
 
 static enum lru_status take_one_from_lru(struct list_head *item,
@@ -352,8 +358,10 @@ static struct page *ttm_pool_type_take(struct ttm_pool_type *pt, int nid)
 	ret = list_lru_walk_node(&pt->pages, nid, take_one_from_lru, (void *)&p, &nr_to_walk);
 	if (ret == 1 && p) {
 		atomic_long_sub(1 << pt->order, &allocated_pages[nid]);
+#ifdef HAVE_NR_GPU_ACTIVE
 		mod_lruvec_page_state(p, NR_GPU_ACTIVE, (1 << pt->order));
 		mod_lruvec_page_state(p, NR_GPU_RECLAIM, -(1 << pt->order));
+#endif
 	}
 	return p;
 }