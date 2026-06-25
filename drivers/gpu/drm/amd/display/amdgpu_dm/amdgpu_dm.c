torvalds/linux

commit 3c668e8c652a9d6d226b72f1e90b5a611b748f4a

drm/amdgpu: Switch private_obj initialization to atomic_create_state
The amdgpu driver relies on a drm_private_obj, that is initialized by
allocating and initializing a state, and then passing it to
drm_private_obj_init.

Since we're gradually moving away from that pattern to the more
established one relying on a atomic_create_state implementation, let's
migrate this instance to the new pattern.

Reviewed-by: Thomas Zimmermann <tzimmermann@suse.de>
Reviewed-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
Link: https://patch.msgid.link/20260224-drm-private-obj-reset-v5-1-5a72f8ec9934@kernel.org
Signed-off-by: Maxime Ripard <mripard@kernel.org>

drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -4844,14 +4844,37 @@ static void dm_atomic_destroy_state(struct drm_private_obj *obj,
 	kfree(dm_state);
 }
 
+static struct drm_private_state *
+dm_atomic_create_state(struct drm_private_obj *obj)
+{
+	struct amdgpu_device *adev = drm_to_adev(obj->dev);
+	struct dm_atomic_state *dm_state;
+	struct dc_state *context;
+
+	dm_state = kzalloc_obj(*dm_state);
+	if (!dm_state)
+		return ERR_PTR(-ENOMEM);
+
+	context = dc_state_create_current_copy(adev->dm.dc);
+	if (!context) {
+		kfree(dm_state);
+		return ERR_PTR(-ENOMEM);
+	}
+
+	__drm_atomic_helper_private_obj_create_state(obj, &dm_state->base);
+	dm_state->context = context;
+
+	return &dm_state->base;
+}
+
 static struct drm_private_state_funcs dm_atomic_state_funcs = {
+	.atomic_create_state = dm_atomic_create_state,
 	.atomic_duplicate_state = dm_atomic_duplicate_state,
 	.atomic_destroy_state = dm_atomic_destroy_state,
 };
 
 static int amdgpu_dm_mode_config_init(struct amdgpu_device *adev)
 {
-	struct dm_atomic_state *state;
 	int r;
 
 	adev->mode_info.mode_config_initialized = true;
@@ -4871,42 +4894,23 @@ static int amdgpu_dm_mode_config_init(struct amdgpu_device *adev)
 	/* indicates support for immediate flip */
 	adev_to_drm(adev)->mode_config.async_page_flip = true;
 
-	state = kzalloc_obj(*state);
-	if (!state)
-		return -ENOMEM;
-
-	state->context = dc_state_create_current_copy(adev->dm.dc);
-	if (!state->context) {
-		kfree(state);
-		return -ENOMEM;
-	}
-
 	drm_atomic_private_obj_init(adev_to_drm(adev),
 				    &adev->dm.atomic_obj,
-				    &state->base,
+				    NULL,
 				    &dm_atomic_state_funcs);
 
 	r = amdgpu_display_modeset_create_props(adev);
-	if (r) {
-		dc_state_release(state->context);
-		kfree(state);
+	if (r)
 		return r;
-	}
 
 #ifdef AMD_PRIVATE_COLOR
-	if (amdgpu_dm_create_color_properties(adev)) {
-		dc_state_release(state->context);
-		kfree(state);
+	if (amdgpu_dm_create_color_properties(adev))
 		return -ENOMEM;
-	}
 #endif
 
 	r = amdgpu_dm_audio_init(adev);
-	if (r) {
-		dc_state_release(state->context);
-		kfree(state);
+	if (r)
 		return r;
-	}
 
 	return 0;
 }

ROCm/amdgpu

commit 526d8905ddeaf8bc4d8421c18e20bfb2a15f68f6

drm/amdkcl: test if drm_private_state_funcs has atomic_create_state
It's caused by 3c668e8c652a9
"drm/amdgpu: Switch private_obj initialization to atomic_create_state"

Fall back to 4-arg drm_atomic_private_obj_init() on old kernels.

Signed-off-by: Chengjun Yao <Chengjun.Yao@amd.com>

drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -5218,6 +5218,7 @@ static void dm_atomic_destroy_state(struct drm_private_obj *obj,
 	kfree(dm_state);
 }
 
+#ifdef HAVE_DRM_PRIVATE_STATE_FUNCS_ATOMIC_CREATE_STATE
 static struct drm_private_state *
 dm_atomic_create_state(struct drm_private_obj *obj)
 {
@@ -5240,15 +5241,21 @@ dm_atomic_create_state(struct drm_private_obj *obj)
 
 	return &dm_state->base;
 }
+#endif
 
 static struct drm_private_state_funcs dm_atomic_state_funcs = {
+#ifdef HAVE_DRM_PRIVATE_STATE_FUNCS_ATOMIC_CREATE_STATE
 	.atomic_create_state = dm_atomic_create_state,
+#endif
 	.atomic_duplicate_state = dm_atomic_duplicate_state,
 	.atomic_destroy_state = dm_atomic_destroy_state,
 };
 
 static int amdgpu_dm_mode_config_init(struct amdgpu_device *adev)
 {
+#ifndef HAVE_DRM_PRIVATE_STATE_FUNCS_ATOMIC_CREATE_STATE
+	struct dm_atomic_state *state;
+#endif
 	int r;
 
 	adev->mode_info.mode_config_initialized = true;
@@ -5268,22 +5275,54 @@ static int amdgpu_dm_mode_config_init(struct amdgpu_device *adev)
 	/* indicates support for immediate flip */
 	adev_to_drm(adev)->mode_config.async_page_flip = true;
 
+#ifndef HAVE_DRM_PRIVATE_STATE_FUNCS_ATOMIC_CREATE_STATE
+	state = kzalloc_obj(*state);
+	if (!state)
+		return -ENOMEM;
+
+	state->context = dc_state_create_current_copy(adev->dm.dc);
+	if (!state->context) {
+		kfree(state);
+		return -ENOMEM;
+	}
+
+	drm_atomic_private_obj_init(adev_to_drm(adev),
+				    &adev->dm.atomic_obj,
+				    &state->base,
+				    &dm_atomic_state_funcs);
+#else
 	drm_atomic_private_obj_init(adev_to_drm(adev),
 				    &adev->dm.atomic_obj,
 				    &dm_atomic_state_funcs);
+#endif
 
 	r = amdgpu_display_modeset_create_props(adev);
-	if (r)
+	if (r) {
+#ifndef HAVE_DRM_PRIVATE_STATE_FUNCS_ATOMIC_CREATE_STATE
+		dc_state_release(state->context);
+		kfree(state);
+#endif
 		return r;
+	}
 
 #ifdef AMD_PRIVATE_COLOR
-	if (amdgpu_dm_create_color_properties(adev))
+	if (amdgpu_dm_create_color_properties(adev)) {
+#ifndef HAVE_DRM_PRIVATE_STATE_FUNCS_ATOMIC_CREATE_STATE
+		dc_state_release(state->context);
+		kfree(state);
+#endif
 		return -ENOMEM;
+	}
 #endif
 
 	r = amdgpu_dm_audio_init(adev);
-	if (r)
+	if (r) {
+#ifndef HAVE_DRM_PRIVATE_STATE_FUNCS_ATOMIC_CREATE_STATE
+		dc_state_release(state->context);
+		kfree(state);
+#endif
 		return r;
+	}
 
 	return 0;
 }

drivers/gpu/drm/amd/dkms/config/config.h
@@ -775,6 +775,9 @@
 /* struct drm_private_obj.lock is available */
 #define HAVE_DRM_PRIVATE_OBJ_LOCK 1
 
+/* struct drm_private_state_funcs has atomic_create_state */
+#define HAVE_DRM_PRIVATE_STATE_FUNCS_ATOMIC_CREATE_STATE 1
+
 /* drm_show_fdinfo() is available */
 #define HAVE_DRM_SHOW_FDINFO 1
 

drivers/gpu/drm/amd/dkms/m4/drm_private_state_funcs_atomic_create_state.m4
@@ -1,0 +1,17 @@
+dnl #
+dnl # v6.19-rc7-1691-g47b5ac7daa46e
+dnl # drm/atomic: Add new atomic_create_state callback to drm_private_obj
+dnl #
+AC_DEFUN([AC_AMDGPU_DRM_PRIVATE_STATE_FUNCS_ATOMIC_CREATE_STATE], [
+	AC_KERNEL_DO_BACKGROUND([
+		AC_KERNEL_TRY_COMPILE([
+			#include <drm/drm_atomic.h>
+		], [
+			struct drm_private_state_funcs *funcs = NULL;
+			(void)funcs->atomic_create_state;
+		], [
+			AC_DEFINE(HAVE_DRM_PRIVATE_STATE_FUNCS_ATOMIC_CREATE_STATE, 1,
+				[struct drm_private_state_funcs has atomic_create_state])
+		])
+	])
+])

drivers/gpu/drm/amd/dkms/m4/kernel.m4
@@ -175,6 +175,7 @@ AC_DEFUN([AC_CONFIG_KERNEL], [
 	AC_AMDGPU_DMA_FENCE_CHAIN_CONTAINED
 	AC_AMDGPU_DRM_GEM_OBJECT_FUNCS_VMAP_HAS_IOSYS_MAP_ARG
 	AC_AMDGPU_DRM_PRIVATE_OBJ_LOCK
+	AC_AMDGPU_DRM_PRIVATE_STATE_FUNCS_ATOMIC_CREATE_STATE
 	AC_AMDGPU_DRM_DSC_CONFIG_SIMPLE_422
 	AC_AMDGPU_DRM_DP_READ_DPCD_CAPS
 	AC_AMDGPU_DRM_DP_REMOVE_RAYLOAD_PART