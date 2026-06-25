ROCm/amdgpu

commit 87d8d919269dacdde6a451acb35e278486922b4c

drm/amdgpu/amdgpu_connectors: use struct drm_edid instead of struct edid
Some amdgpu code is still using deprecated edid functions. Switch to
the newer functions and update the amdgpu_connector struct's edid type
to the drm_edid type.

At the same time, use the raw EDID when we need to for speaker
allocations and for determining if the input is digital.

Signed-off-by: Joshua Peisach <jpeisach@ubuntu.com>
Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

drivers/gpu/drm/amd/amdgpu/amdgpu_connectors.c
@@ -280,10 +280,10 @@ amdgpu_connector_find_encoder(struct drm_connector *connector,
 	return NULL;
 }
 
-static struct edid *
+static const struct drm_edid *
 amdgpu_connector_get_hardcoded_edid(struct amdgpu_device *adev)
 {
-	return drm_edid_duplicate(drm_edid_raw(adev->mode_info.bios_hardcoded_edid));
+	return drm_edid_dup(adev->mode_info.bios_hardcoded_edid);
 }
 
 static void amdgpu_connector_get_edid(struct drm_connector *connector)
@@ -302,8 +302,8 @@ static void amdgpu_connector_get_edid(struct drm_connector *connector)
 	if ((amdgpu_connector_encoder_get_dp_bridge_encoder_id(connector) !=
 	     ENCODER_OBJECT_ID_NONE) &&
 	    amdgpu_connector->ddc_bus->has_aux) {
-		amdgpu_connector->edid = drm_get_edid(connector,
-						      &amdgpu_connector->ddc_bus->aux.ddc);
+		amdgpu_connector->edid = drm_edid_read_ddc(connector,
+							  &amdgpu_connector->ddc_bus->aux.ddc);
 	} else if ((connector->connector_type == DRM_MODE_CONNECTOR_DisplayPort) ||
 		   (connector->connector_type == DRM_MODE_CONNECTOR_eDP)) {
 		struct amdgpu_connector_atom_dig *dig = amdgpu_connector->con_priv;
@@ -311,14 +311,14 @@ static void amdgpu_connector_get_edid(struct drm_connector *connector)
 		if ((dig->dp_sink_type == CONNECTOR_OBJECT_ID_DISPLAYPORT ||
 		     dig->dp_sink_type == CONNECTOR_OBJECT_ID_eDP) &&
 		    amdgpu_connector->ddc_bus->has_aux)
-			amdgpu_connector->edid = drm_get_edid(connector,
-							      &amdgpu_connector->ddc_bus->aux.ddc);
+			amdgpu_connector->edid = drm_edid_read_ddc(connector,
+								  &amdgpu_connector->ddc_bus->aux.ddc);
 		else if (amdgpu_connector->ddc_bus)
-			amdgpu_connector->edid = drm_get_edid(connector,
-							      &amdgpu_connector->ddc_bus->adapter);
+			amdgpu_connector->edid = drm_edid_read_ddc(connector,
+								  &amdgpu_connector->ddc_bus->adapter);
 	} else if (amdgpu_connector->ddc_bus) {
-		amdgpu_connector->edid = drm_get_edid(connector,
-						      &amdgpu_connector->ddc_bus->adapter);
+		amdgpu_connector->edid = drm_edid_read_ddc(connector,
+							  &amdgpu_connector->ddc_bus->adapter);
 	}
 
 	if (!amdgpu_connector->edid) {
@@ -326,7 +326,7 @@ static void amdgpu_connector_get_edid(struct drm_connector *connector)
 		if (((connector->connector_type == DRM_MODE_CONNECTOR_LVDS) ||
 		     (connector->connector_type == DRM_MODE_CONNECTOR_eDP))) {
 			amdgpu_connector->edid = amdgpu_connector_get_hardcoded_edid(adev);
-			drm_connector_update_edid_property(connector, amdgpu_connector->edid);
+			drm_edid_connector_update(connector, amdgpu_connector->edid);
 		}
 	}
 }
@@ -345,11 +345,11 @@ static int amdgpu_connector_ddc_get_modes(struct drm_connector *connector)
 	int ret;
 
 	if (amdgpu_connector->edid) {
-		drm_connector_update_edid_property(connector, amdgpu_connector->edid);
-		ret = drm_add_edid_modes(connector, amdgpu_connector->edid);
+		drm_edid_connector_update(connector, amdgpu_connector->edid);
+		ret = drm_edid_connector_add_modes(connector);
 		return ret;
 	}
-	drm_connector_update_edid_property(connector, NULL);
+	drm_edid_connector_update(connector, NULL);
 	return 0;
 }
 
@@ -932,7 +932,7 @@ amdgpu_connector_vga_detect(struct drm_connector *connector, bool force)
 			ret = connector_status_connected;
 		} else {
 			amdgpu_connector->use_digital =
-				!!(amdgpu_connector->edid->input & DRM_EDID_INPUT_DIGITAL);
+				drm_edid_is_digital(amdgpu_connector->edid);
 
 			/* some oems have boards with separate digital and analog connectors
 			 * with a shared ddc line (often vga + hdmi)
@@ -1112,7 +1112,7 @@ amdgpu_connector_dvi_detect(struct drm_connector *connector, bool force)
 			broken_edid = true; /* defer use_digital to later */
 		} else {
 			amdgpu_connector->use_digital =
-				!!(amdgpu_connector->edid->input & DRM_EDID_INPUT_DIGITAL);
+				drm_edid_is_digital(amdgpu_connector->edid);
 
 			/* some oems have boards with separate digital and analog connectors
 			 * with a shared ddc line (often vga + hdmi)

drivers/gpu/drm/amd/amdgpu/amdgpu_mode.h
@@ -624,7 +624,7 @@ struct amdgpu_connector {
 	bool use_digital;
 	/* we need to mind the EDID between detect
 	   and get modes due to analog/digital/tvencoder */
-	struct edid *edid;
+	const struct drm_edid *edid;
 	void *con_priv;
 	bool dac_load_detect;
 	bool detected_by_load; /* if the connection status was determined by load */

drivers/gpu/drm/amd/amdgpu/dce_v10_0.c
@@ -1298,7 +1298,7 @@ static void dce_v10_0_audio_write_speaker_allocation(struct drm_encoder *encoder
 		return;
 	}
 
-	sad_count = drm_edid_to_speaker_allocation(amdgpu_connector->edid, &sadb);
+	sad_count = drm_edid_to_speaker_allocation(drm_edid_raw(amdgpu_connector->edid), &sadb);
 	if (sad_count < 0) {
 		DRM_ERROR("Couldn't read Speaker Allocation Data Block: %d\n", sad_count);
 		sad_count = 0;
@@ -1368,7 +1368,7 @@ static void dce_v10_0_audio_write_sad_regs(struct drm_encoder *encoder)
 		return;
 	}
 
-	sad_count = drm_edid_to_sad(amdgpu_connector->edid, &sads);
+	sad_count = drm_edid_to_sad(drm_edid_raw(amdgpu_connector->edid), &sads);
 	if (sad_count < 0)
 		DRM_ERROR("Couldn't read SADs: %d\n", sad_count);
 	if (sad_count <= 0)

drivers/gpu/drm/amd/amdgpu/dce_v6_0.c
@@ -1265,7 +1265,7 @@ static void dce_v6_0_audio_write_speaker_allocation(struct drm_encoder *encoder)
 		return;
 	}
 
-	sad_count = drm_edid_to_speaker_allocation(amdgpu_connector->edid, &sadb);
+	sad_count = drm_edid_to_speaker_allocation(drm_edid_raw(amdgpu_connector->edid), &sadb);
 	if (sad_count < 0) {
 		DRM_ERROR("Couldn't read Speaker Allocation Data Block: %d\n", sad_count);
 		sad_count = 0;
@@ -1346,7 +1346,7 @@ static void dce_v6_0_audio_write_sad_regs(struct drm_encoder *encoder)
 		return;
 	}
 
-	sad_count = drm_edid_to_sad(amdgpu_connector->edid, &sads);
+	sad_count = drm_edid_to_sad(drm_edid_raw(amdgpu_connector->edid), &sads);
 	if (sad_count < 0)
 		DRM_ERROR("Couldn't read SADs: %d\n", sad_count);
 	if (sad_count <= 0)

drivers/gpu/drm/amd/amdgpu/dce_v8_0.c
@@ -1271,7 +1271,7 @@ static void dce_v8_0_audio_write_speaker_allocation(struct drm_encoder *encoder)
 		return;
 	}
 
-	sad_count = drm_edid_to_speaker_allocation(amdgpu_connector->edid, &sadb);
+	sad_count = drm_edid_to_speaker_allocation(drm_edid_raw(amdgpu_connector->edid), &sadb);
 	if (sad_count < 0) {
 		DRM_ERROR("Couldn't read Speaker Allocation Data Block: %d\n", sad_count);
 		sad_count = 0;
@@ -1339,7 +1339,7 @@ static void dce_v8_0_audio_write_sad_regs(struct drm_encoder *encoder)
 		return;
 	}
 
-	sad_count = drm_edid_to_sad(amdgpu_connector->edid, &sads);
+	sad_count = drm_edid_to_sad(drm_edid_raw(amdgpu_connector->edid), &sads);
 	if (sad_count < 0)
 		DRM_ERROR("Couldn't read SADs: %d\n", sad_count);
 	if (sad_count <= 0)

commit 9c4d736ddeb488033bc8cfd8b697657fbeb02262

drm/amdkcl: backport drm_edid API for older kernels
It's caused by 87d8d919269da
"drm/amdgpu/amdgpu_connectors: use struct drm_edid instead of struct edid"

Provide KCL wrappers for drm_edid_dup, drm_edid_read_ddc,
drm_edid_connector_update, drm_edid_connector_add_modes,
drm_edid_is_digital on old kernels.

Signed-off-by: Chengjun Yao <Chengjun.Yao@amd.com>

drivers/gpu/drm/amd/amdgpu/amdgpu_connectors.c
@@ -112,7 +112,7 @@ int amdgpu_connector_get_monitor_bpc(struct drm_connector *connector)
 #if defined(HAVE_DRM_DISPLAY_INFO_IS_HDMI)
 			if (connector->display_info.is_hdmi) {
 #else
-			if (drm_detect_hdmi_monitor(drm_edid_raw(amdgpu_connector->edid))) {
+			if (drm_detect_hdmi_monitor((struct edid *)drm_edid_raw(amdgpu_connector->edid))) {
 #endif
 				if (connector->display_info.bpc)
 					bpc = connector->display_info.bpc;
@@ -124,7 +124,7 @@ int amdgpu_connector_get_monitor_bpc(struct drm_connector *connector)
 #if defined(HAVE_DRM_DISPLAY_INFO_IS_HDMI)
 		if (connector->display_info.is_hdmi) {
 #else
-		if (drm_detect_hdmi_monitor(drm_edid_raw(amdgpu_connector->edid))) {
+		if (drm_detect_hdmi_monitor((struct edid *)drm_edid_raw(amdgpu_connector->edid))) {
 #endif
 			if (connector->display_info.bpc)
 				bpc = connector->display_info.bpc;
@@ -137,7 +137,7 @@ int amdgpu_connector_get_monitor_bpc(struct drm_connector *connector)
 #if defined(HAVE_DRM_DISPLAY_INFO_IS_HDMI)
 		    connector->display_info.is_hdmi) {
 #else
-		    drm_detect_hdmi_monitor(drm_edid_raw(amdgpu_connector->edid))) {
+		    drm_detect_hdmi_monitor((struct edid *)drm_edid_raw(amdgpu_connector->edid))) {
 #endif
 			if (connector->display_info.bpc)
 				bpc = connector->display_info.bpc;
@@ -165,7 +165,7 @@ int amdgpu_connector_get_monitor_bpc(struct drm_connector *connector)
 #if defined(HAVE_DRM_DISPLAY_INFO_IS_HDMI)
 	if (connector->display_info.is_hdmi) {
 #else
-	if (drm_detect_hdmi_monitor(drm_edid_raw(amdgpu_connector->edid))) {
+	if (drm_detect_hdmi_monitor((struct edid *)drm_edid_raw(amdgpu_connector->edid))) {
 #endif
 		/*
 		 * Pre DCE-8 hw can't handle > 12 bpc, and more than 12 bpc doesn't make

drivers/gpu/drm/amd/amdkcl/kcl_drm_edid.c
@@ -124,28 +124,46 @@ const struct edid *_kcl_drm_edid_raw(const struct drm_edid *drm_edid)
 EXPORT_SYMBOL(_kcl_drm_edid_raw);
 #endif
 
-#ifndef HAVE_DRM_EDID_CONNECTOR_UPDATE
-int kcl_drm_edid_connector_update(struct drm_connector *connector,
-      const struct drm_edid *drm_edid)
+#ifndef HAVE_DRM_EDID_READ_DDC
+const struct drm_edid *_kcl_drm_edid_read_ddc(struct drm_connector *connector,
+					       struct i2c_adapter *adapter)
 {
-	const struct edid *edid = _kcl_drm_edid_raw(drm_edid);
-	return drm_connector_update_edid_property(connector, edid);
+	struct edid *edid;
+	const struct drm_edid *drm_edid;
+
+	edid = drm_get_edid(connector, adapter);
+	if (!edid)
+		return NULL;
+
+	drm_edid = drm_edid_alloc(edid, (edid->extensions + 1) * EDID_LENGTH);
+	kfree(edid);
+	return drm_edid;
 }
-EXPORT_SYMBOL(kcl_drm_edid_connector_update);
+EXPORT_SYMBOL(_kcl_drm_edid_read_ddc);
+#endif
+
+#ifndef HAVE_DRM_EDID_CONNECTOR_UPDATE
+int _kcl_drm_edid_connector_update(struct drm_connector *connector,
+				    const struct drm_edid *drm_edid)
+{
+	return drm_connector_update_edid_property(connector,
+		drm_edid ? (struct edid *)drm_edid_raw(drm_edid) : NULL);
+}
+EXPORT_SYMBOL(_kcl_drm_edid_connector_update);
 #endif
 
 #ifndef HAVE_DRM_EDID_CONNECTOR_ADD_MODES
-int kcl_drm_edid_connector_add_modes(struct drm_connector *connector)
+int _kcl_drm_edid_connector_add_modes(struct drm_connector *connector)
 {
 	struct edid *edid = NULL;
 
 	if (connector->edid_blob_ptr)
 		edid = (struct edid *)connector->edid_blob_ptr->data;
 
-	if (edid)
-		return drm_add_edid_modes(connector, edid);
+	if (!edid)
+		return 0;
 
-	return 0;
+	return drm_add_edid_modes(connector, edid);
 }
-EXPORT_SYMBOL(kcl_drm_edid_connector_add_modes);
+EXPORT_SYMBOL(_kcl_drm_edid_connector_add_modes);
 #endif

drivers/gpu/drm/amd/dkms/config/config.h
@@ -610,19 +610,25 @@
 /* drm_edid_connector_update is available */
 #define HAVE_DRM_EDID_CONNECTOR_UPDATE 1
 
+/* drm_edid_dup() is available and exported */
+#define HAVE_DRM_EDID_DUP 1
+
 /* drm_edid_is_digital is available */
 #define HAVE_DRM_EDID_IS_DIGITAL 1
 
-/* drm_edid_alloc() is available */
+/* drm_edid_alloc() is available and exported */
 #define HAVE_DRM_EDID_MALLOC 1
 
 /* drm_edid_override_connector_update() is available */
 #define HAVE_DRM_EDID_OVERRIDE_CONNECTOR_UPDATE 1
 
-/* drm_edid_raw() is available */
+/* drm_edid_raw() is available and exported */
 #define HAVE_DRM_EDID_RAW 1
 
-/* drm_edid_valid() is available */
+/* drm_edid_read_ddc() is available and exported */
+#define HAVE_DRM_EDID_READ_DDC 1
+
+/* drm_edid_valid() is available and exported */
 #define HAVE_DRM_EDID_VALID 1
 
 /* fb_create has const struct drm_format_info * parameter */

drivers/gpu/drm/amd/dkms/m4/drm_edid_alloc.m4
@@ -4,13 +4,13 @@ dnl # drm/edid: add new interfaces around struct drm_edid
 dnl #
 AC_DEFUN([AC_AMDGPU_DRM_EDID_MALLOC], [
 	AC_KERNEL_DO_BACKGROUND([
-		AC_KERNEL_TRY_COMPILE([
+		AC_KERNEL_TRY_COMPILE_SYMBOL([
 			#include <drm/drm_edid.h>
-		],[
+		], [
 			drm_edid_alloc(NULL, 0);
-		],[
+		], [drm_edid_alloc], [drivers/gpu/drm/drm_edid.c], [
 			AC_DEFINE(HAVE_DRM_EDID_MALLOC, 1,
-				[drm_edid_alloc() is available])
+				[drm_edid_alloc() is available and exported])
 		])
 	])
 ])
@@ -21,13 +21,13 @@ dnl # drm/edid: add drm_edid_raw() to access the raw EDID data
 dnl #
 AC_DEFUN([AC_AMDGPU_DRM_EDID_RAW], [
 	AC_KERNEL_DO_BACKGROUND([
-		AC_KERNEL_TRY_COMPILE([
+		AC_KERNEL_TRY_COMPILE_SYMBOL([
 			#include <drm/drm_edid.h>
-		],[
+		], [
 			drm_edid_raw(NULL);
-		],[
+		], [drm_edid_raw], [drivers/gpu/drm/drm_edid.c], [
 			AC_DEFINE(HAVE_DRM_EDID_RAW, 1,
-				[drm_edid_raw() is available])
+				[drm_edid_raw() is available and exported])
 		])
 	])
 ])
@@ -38,20 +38,110 @@ dnl # drm/edid: add function for checking drm_edid validity
 dnl #
 AC_DEFUN([AC_AMDGPU_DRM_EDID_VALID], [
 	AC_KERNEL_DO_BACKGROUND([
-		AC_KERNEL_TRY_COMPILE([
+		AC_KERNEL_TRY_COMPILE_SYMBOL([
 			#include <drm/drm_edid.h>
-		],[
+		], [
 			drm_edid_valid(NULL);
-		],[
+		], [drm_edid_valid], [drivers/gpu/drm/drm_edid.c], [
 			AC_DEFINE(HAVE_DRM_EDID_VALID, 1,
-				[drm_edid_valid() is available])
+				[drm_edid_valid() is available and exported])
 		])
 	])
 ])
 
 
+dnl #
+dnl # commit v6.3-rc2-329-gf1e4c916f97f
+dnl # drm/edid: add drm_edid_dup()
+dnl #
+AC_DEFUN([AC_AMDGPU_DRM_EDID_DUP], [
+	AC_KERNEL_DO_BACKGROUND([
+		AC_KERNEL_TRY_COMPILE_SYMBOL([
+			#include <drm/drm_edid.h>
+		], [
+			drm_edid_dup(NULL);
+		], [drm_edid_dup], [drivers/gpu/drm/drm_edid.c], [
+			AC_DEFINE(HAVE_DRM_EDID_DUP, 1,
+				[drm_edid_dup() is available and exported])
+		])
+	])
+])
+
+dnl #
+dnl # commit v6.3-rc2-336-gce6526ab2364
+dnl # drm/edid: add drm_edid_read_ddc()
+dnl #
+AC_DEFUN([AC_AMDGPU_DRM_EDID_READ_DDC], [
+	AC_KERNEL_DO_BACKGROUND([
+		AC_KERNEL_TRY_COMPILE_SYMBOL([
+			#include <drm/drm_edid.h>
+		], [
+			drm_edid_read_ddc(NULL, NULL);
+		], [drm_edid_read_ddc], [drivers/gpu/drm/drm_edid.c], [
+			AC_DEFINE(HAVE_DRM_EDID_READ_DDC, 1,
+				[drm_edid_read_ddc() is available and exported])
+		])
+	])
+])
+
+dnl #
+dnl # commit v5.20-rc1-474-g5ceb7058b5fe
+dnl # drm/edid: add drm_edid_connector_update()
+dnl #
+AC_DEFUN([AC_AMDGPU_DRM_EDID_CONNECTOR_UPDATE], [
+	AC_KERNEL_DO_BACKGROUND([
+		AC_KERNEL_TRY_COMPILE_SYMBOL([
+			#include <drm/drm_edid.h>
+		], [
+			drm_edid_connector_update(NULL, NULL);
+		], [drm_edid_connector_update], [drivers/gpu/drm/drm_edid.c], [
+			AC_DEFINE(HAVE_DRM_EDID_CONNECTOR_UPDATE, 1,
+				[drm_edid_connector_update() is available and exported])
+		])
+	])
+])
+
+dnl #
+dnl # commit v5.20-rc1-479-gedd1f90a8403
+dnl # drm/edid: add drm_edid_connector_add_modes()
+dnl #
+AC_DEFUN([AC_AMDGPU_DRM_EDID_CONNECTOR_ADD_MODES], [
+	AC_KERNEL_DO_BACKGROUND([
+		AC_KERNEL_TRY_COMPILE_SYMBOL([
+			#include <drm/drm_edid.h>
+		], [
+			drm_edid_connector_add_modes(NULL);
+		], [drm_edid_connector_add_modes], [drivers/gpu/drm/drm_edid.c], [
+			AC_DEFINE(HAVE_DRM_EDID_CONNECTOR_ADD_MODES, 1,
+				[drm_edid_connector_add_modes() is available and exported])
+		])
+	])
+])
+
+dnl #
+dnl # commit v6.3-rc2-327-g02364dd57e33
+dnl # drm/edid: add drm_edid_is_digital()
+dnl #
+AC_DEFUN([AC_AMDGPU_DRM_EDID_IS_DIGITAL], [
+	AC_KERNEL_DO_BACKGROUND([
+		AC_KERNEL_TRY_COMPILE_SYMBOL([
+			#include <drm/drm_edid.h>
+		], [
+			drm_edid_is_digital(NULL);
+		], [drm_edid_is_digital], [drivers/gpu/drm/drm_edid.c], [
+			AC_DEFINE(HAVE_DRM_EDID_IS_DIGITAL, 1,
+				[drm_edid_is_digital() is available and exported])
+		])
+	])
+])
+
 AC_DEFUN([AC_AMDGPU_STRUCT_DRM_EDID], [
 	AC_AMDGPU_DRM_EDID_MALLOC
 	AC_AMDGPU_DRM_EDID_RAW
 	AC_AMDGPU_DRM_EDID_VALID
+	AC_AMDGPU_DRM_EDID_DUP
+	AC_AMDGPU_DRM_EDID_READ_DDC
+	AC_AMDGPU_DRM_EDID_CONNECTOR_UPDATE
+	AC_AMDGPU_DRM_EDID_CONNECTOR_ADD_MODES
+	AC_AMDGPU_DRM_EDID_IS_DIGITAL
 ])

include/kcl/kcl_drm_edid.h
@@ -36,13 +36,8 @@ struct drm_edid {
 
 #ifndef HAVE_DRM_EDID_MALLOC
 const struct drm_edid *_kcl_drm_edid_alloc(const void *edid, size_t size);
-const struct drm_edid *kcl_drm_edid_dup(const struct drm_edid *drm_edid);
-const struct drm_edid *kcl_drm_edid_read_ddc(struct drm_connector *connector,
- struct i2c_adapter *adapter);
 void _kcl_drm_edid_free(const struct drm_edid *drm_edid);
 #define  drm_edid_alloc _kcl_drm_edid_alloc
-#define  drm_edid_dup kcl_drm_edid_dup
-#define  drm_edid_read_ddc kcl_drm_edid_read_ddc
 #define  drm_edid_free _kcl_drm_edid_free
 #endif
 
@@ -62,26 +57,58 @@ static inline bool _kcl_drm_edid_valid(const struct drm_edid *drm_edid)
 #define  drm_edid_valid _kcl_drm_edid_valid
 #endif
 
-#ifndef HAVE_DRM_EDID_IS_DIGITAL
-static inline bool kcl_drm_edid_is_digital(const struct drm_edid *drm_edid)
+/*	commit v6.3-rc2-329-gf1e4c916f97f
+	drm/edid: add drm_edid_dup()	*/
+#ifndef HAVE_DRM_EDID_DUP
+static inline const struct drm_edid *_kcl_drm_edid_dup(const struct drm_edid *drm_edid)
 {
-	if (!drm_edid || !drm_edid->edid)
-		return false;
-
-	return drm_edid->edid->input & DRM_EDID_INPUT_DIGITAL;
+	if (!drm_edid)
+		return NULL;
+	return drm_edid_alloc(drm_edid_raw(drm_edid), drm_edid->size);
 }
-#define drm_edid_is_digital kcl_drm_edid_is_digital
+#define drm_edid_dup _kcl_drm_edid_dup
 #endif
 
-#ifndef HAVE_DRM_EDID_CONNECTOR_ADD_MODES
-int kcl_drm_edid_connector_add_modes(struct drm_connector *connector);
-#define drm_edid_connector_add_modes kcl_drm_edid_connector_add_modes
+/*	commit v6.3-rc2-336-gce6526ab2364
+	drm/edid: add drm_edid_read_ddc()	*/
+#ifndef HAVE_DRM_EDID_READ_DDC
+struct drm_connector;
+struct i2c_adapter;
+const struct drm_edid *_kcl_drm_edid_read_ddc(struct drm_connector *connector,
+					       struct i2c_adapter *adapter);
+#define drm_edid_read_ddc _kcl_drm_edid_read_ddc
 #endif
 
+/*	commit v5.20-rc1-474-g5ceb7058b5fe
+	drm/edid: add drm_edid_connector_update()	*/
 #ifndef HAVE_DRM_EDID_CONNECTOR_UPDATE
-int kcl_drm_edid_connector_update(struct drm_connector *connector,
-      const struct drm_edid *drm_edid);
-#define drm_edid_connector_update kcl_drm_edid_connector_update
+struct drm_connector;
+int _kcl_drm_edid_connector_update(struct drm_connector *connector,
+				    const struct drm_edid *drm_edid);
+#define drm_edid_connector_update _kcl_drm_edid_connector_update
+#endif
+
+/*	commit v5.20-rc1-479-gedd1f90a8403
+	drm/edid: add drm_edid_connector_add_modes()	*/
+#ifndef HAVE_DRM_EDID_CONNECTOR_ADD_MODES
+struct drm_connector;
+int _kcl_drm_edid_connector_add_modes(struct drm_connector *connector);
+#define drm_edid_connector_add_modes _kcl_drm_edid_connector_add_modes
+#endif
+
+/*	commit v6.3-rc2-327-g02364dd57e33
+	drm/edid: add drm_edid_is_digital()	*/
+#ifndef HAVE_DRM_EDID_IS_DIGITAL
+static inline bool _kcl_drm_edid_is_digital(const struct drm_edid *drm_edid)
+{
+	const struct edid *edid;
+
+	if (!drm_edid)
+		return false;
+	edid = drm_edid_raw(drm_edid);
+	return edid && (edid->input & DRM_EDID_INPUT_DIGITAL);
+}
+#define drm_edid_is_digital _kcl_drm_edid_is_digital
 #endif
 
 #endif