ddead0e111bdb2dd0c1ec29a58eaf3f6e1c9fb8f
Chengjun Yao <Chengjun.Yao@amd.com>
2026-07-22 18:37:05 +0800

    Revert "drm/amdgpu: drop debug_enable_ras_aca debug mask flag"
    This reverts commit 575de78068757a85660fa468de33ec5ce15a6be6.
    
    Signed-off-by: Chengjun Yao <Chengjun.Yao@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu.h
@@ -934,6 +934,7 @@ struct amdgpu_device {
 	bool                            debug_largebar;
 	bool                            debug_disable_soft_recovery;
 	bool                            debug_use_vram_fw_buf;
+	bool                            debug_enable_ras_aca;
 	bool                            debug_exp_resets;
 	bool                            debug_disable_gpu_ring_reset;
 	bool                            debug_vm_userptr;
